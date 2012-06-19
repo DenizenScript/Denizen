807f95996e71b28fdb5fd8080f8d9c75fd205ba9
Jeremy Schroeder <jeremy@ssdbg.com>
2012-06-19 16:38:25 -0400

    ...

--- src/net/aufdemrand/denizen/commands/sampleCommand.java
+++ src/net/aufdemrand/denizen/commands/sampleCommand.java
@@ -1,5 +1,23 @@
 package net.aufdemrand.denizen.commands;
 
-public class sampleCommand {
+import net.citizensnpcs.api.npc.NPC;
+
+import org.bukkit.entity.Player;
+
+public class sampleCommand implements Command {
+
+	
+	@Override
+	public boolean execute(Player thePlayer, NPC theDenizen, String[] arguments) {
+
+		/* 
+		 * TODO: command code
+		 */
+		
+		return false;
+	}
+
+
+
 
 }
