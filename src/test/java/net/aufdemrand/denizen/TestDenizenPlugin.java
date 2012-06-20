99773150e124f129cc19d4a8e0cbbb475b12a7ab
Chris DeLashmutt <grog@grogscave.net>
2012-06-20 16:57:13 -0400

    Moving Tests into appropriate package.

---
+++ src/test/java/net/aufdemrand/denizen/TestDenizenPlugin.java
@@ -0,0 +1,20 @@
+package net.aufdemrand.denizen;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.integration.junit4.JUnit4Mockery;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Test the basic plugin functionality
+ * @author grog
+ */
+@RunWith(JMock.class)
+public class TestDenizenPlugin {
+	Mockery context = new JUnit4Mockery();
+	
+	@Test
+	public void testNoArgCommand(){
+		//TODO: Need to finish test.
+	}
+}
