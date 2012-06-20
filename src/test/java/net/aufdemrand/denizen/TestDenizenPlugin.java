3d207d381785aa5b0f3cf856a1ce3fe9facaf73c
Chris DeLashmutt <grog@grogscave.net>
2012-06-20 18:03:25 -0400

    Reving POM version, adding code to retrieve the plugin version from the maven info inside the jar, and adding a test to validate it.

--- src/test/java/net/aufdemrand/denizen/TestDenizenPlugin.java
+++ src/test/java/net/aufdemrand/denizen/TestDenizenPlugin.java
@@ -1,6 +1,10 @@
 package net.aufdemrand.denizen;
 
+import static org.hamcrest.Matchers.*;
+
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
@@ -16,10 +20,10 @@ import org.junit.runner.RunWith;
 public class TestDenizenPlugin {
 	Mockery context = new JUnit4Mockery();
 	
-	@Test
 	/**
 	 * Test the plugin handing no arguments passed with an invocation of "/denizen"
 	 */
+	@Test
 	public void testNoArgCommand() {
 		Denizen denizen = new Denizen();
 		final CommandSender sender = context.mock(CommandSender.class);
@@ -29,5 +33,20 @@ public class TestDenizenPlugin {
 		}});
 		
 		denizen.onCommand(sender, null, null, new String[0]);
+	}
+
+	/**
+	 * Test the plugin handing no arguments passed with an invocation of "/denizen"
+	 */
+	@Test
+	public void testVersion() {
+		Denizen denizen = new Denizen();
+		final Player sender = context.mock(Player.class);
+		
+		context.checking(new Expectations() {{
+			oneOf(sender).sendMessage(with(allOf(aNonNull(String.class), startsWith(ChatColor.GREEN +"Denizen version: "))));
+		}});
+		
+		denizen.onCommand(sender, null, null, new String[]{"version"});
 	}
 }
