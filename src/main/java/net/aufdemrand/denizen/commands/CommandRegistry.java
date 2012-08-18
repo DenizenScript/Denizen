4d897fdbd50fe6d8cd27ec259d837eb25755193d
jrbudda <jrbudda@gmail.com>
2012-08-18 00:46:35 -0400

    CAST Command

--- src/main/java/net/aufdemrand/denizen/commands/CommandRegistry.java
+++ src/main/java/net/aufdemrand/denizen/commands/CommandRegistry.java
@@ -101,6 +101,7 @@ public class CommandRegistry {
 		DropCommand dropCommand = new DropCommand();
 		CooldownCommand cooldownCommand = new CooldownCommand();
 		HintCommand hintCommand = new HintCommand();
+		net.aufdemrand.denizen.commands.core.CastCommand castCommand = new net.aufdemrand.denizen.commands.core.CastCommand();
 		
 		try {
 			hintCommand.activateAs("HINT");
@@ -132,6 +133,7 @@ public class CommandRegistry {
 			resetCommand.activateAs("RESET");
 			flagCommand.activateAs("FLAG");	
 			strikeCommand.activateAs("STRIKE");
+			castCommand.activateAs("CAST");
 			
 		} catch (ActivationException e) {
 			plugin.getLogger().log(Level.SEVERE, "Oh no! Denizen has run into a problem registering the core commands!");
