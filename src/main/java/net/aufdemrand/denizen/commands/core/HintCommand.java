883a878296ff81cd40b71c31a636beaf26dadc76
Jeremy Schroeder <jeremy@ssdbg.com>
2012-08-19 17:10:07 -0400

    Use echoDebug in HINT.

--- src/main/java/net/aufdemrand/denizen/commands/core/HintCommand.java
+++ src/main/java/net/aufdemrand/denizen/commands/core/HintCommand.java
@@ -55,7 +55,7 @@ public class HintCommand extends AbstractCommand {
 
 			if (plugin.getScripts().contains(scriptPath + x + ".Trigger")) {
 				chatTriggers.add(plugin.getScripts().getString(scriptPath + x + ".Trigger"));
-				if (plugin.debugMode) plugin.getLogger().info("found " + chatTriggers.get(x-1));
+				aH.echoDebug("...found '" + chatTriggers.get(x-1) + "'");
 			}
 			else thisTriggerExists = false;
 			x++;
