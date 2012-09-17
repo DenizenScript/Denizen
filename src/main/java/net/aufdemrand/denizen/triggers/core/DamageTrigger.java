61ed4e79b7430a7f1e92a4d164acce71fe94ce54
Jeremy Schroeder <jeremy@ssdbg.com>
2012-09-17 15:06:20 -0400

    Sentrizen fix for Damage Trigger?

--- src/main/java/net/aufdemrand/denizen/triggers/core/DamageTrigger.java
+++ src/main/java/net/aufdemrand/denizen/triggers/core/DamageTrigger.java
@@ -10,8 +10,11 @@ import net.aufdemrand.denizen.scripts.ScriptHelper;
 import net.aufdemrand.denizen.scripts.ScriptEngine.QueueType;
 import net.aufdemrand.denizen.triggers.AbstractTrigger;
 
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.event.NPCDamageEvent;
 import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
+import net.citizensnpcs.api.npc.NPC;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -21,44 +24,53 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 
 public class DamageTrigger extends AbstractTrigger implements Listener {
 
 	/* Damage Trigger event. Virtually identical to the Click Trigger, for comments, see ClickTrigger.java */
 
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void damageTrigger(NPCLeftClickEvent event) {
+	public void damageTrigger(EntityDamageByEntityEvent event) {
 
-		ScriptHelper sE = plugin.getScriptEngine().helper;
-		if (plugin.getDenizenNPCRegistry().isDenizenNPC(event.getNPC())) {
-			DenizenNPC denizenNPC = plugin.getDenizenNPCRegistry().getDenizen(event.getNPC());
+		if (CitizensAPI.getNPCRegistry().isNPC(event.getEntity()) && event.getDamager() instanceof Player) {
+			NPC theNPC = CitizensAPI.getNPCRegistry().getNPC(event.getEntity());
 
-			/* Check if trigger is enabled, because if not, send click trigger (if enabled in settings) */
-			if (denizenNPC.getCitizensEntity().getTrait(DenizenTrait.class).triggerIsEnabled(triggerName.toUpperCase())) {
+			if (plugin.getDenizenNPCRegistry().isDenizenNPC(theNPC)) {
 
-				if (denizenNPC.isInteractable(triggerName, event.getClicker())) {
-					sE.setCooldown(denizenNPC, DamageTrigger.class, plugin.settings.DefaultDamageCooldown());
-					if (!parseDamageTrigger(denizenNPC, event.getClicker())) {
-						denizenNPC.talk(TalkType.CHAT, event.getClicker(), Reason.NoMatchingDamageTrigger);
+				ScriptHelper sE = plugin.getScriptEngine().helper;
+
+					DenizenNPC denizenNPC = plugin.getDenizenNPCRegistry().getDenizen(theNPC);
+
+					/* Check if trigger is enabled, because if not, send click trigger (if enabled in settings) */
+					if (denizenNPC.getCitizensEntity().getTrait(DenizenTrait.class).triggerIsEnabled(triggerName.toUpperCase())) {
+
+						if (denizenNPC.isInteractable(triggerName, (Player) event.getDamager())) {
+							sE.setCooldown(denizenNPC, DamageTrigger.class, plugin.settings.DefaultDamageCooldown());
+							if (!parseDamageTrigger(denizenNPC, (Player) event.getDamager())) {
+								denizenNPC.talk(TalkType.CHAT, (Player) event.getDamager(), Reason.NoMatchingDamageTrigger);
+							}
+						}
+					}
+
+					else if (plugin.settings.DisabledDamageTriggerInsteadTriggersClick()) {
+						plugin.getTriggerRegistry().getTrigger(ClickTrigger.class).clickTrigger(new NPCRightClickEvent(theNPC, (Player) event.getDamager()));
 					}
 				}
 			}
-
-			else if (plugin.settings.DisabledDamageTriggerInsteadTriggersClick()) {
-				plugin.getTriggerRegistry().getTrigger(ClickTrigger.class).clickTrigger(new NPCRightClickEvent(event.getNPC(), event.getClicker()));
-			}
 		}
-	}
+	
 
 
 	/* Parses the Damage Trigger */
 	CommandSender cs;
-	
+
 	public boolean parseDamageTrigger(DenizenNPC theDenizen, Player thePlayer) {
 
 		ScriptHelper sE = plugin.getScriptEngine().helper;
 		if (cs == null) cs = Bukkit.getConsoleSender();
-		
+
 		// Play the HURT effect.
 		theDenizen.getEntity().playEffect(EntityEffect.HURT);
 
@@ -66,12 +78,12 @@ public class DamageTrigger extends AbstractTrigger implements Listener {
 		String theScriptName = theDenizen.getInteractScript(thePlayer, this.getClass());
 
 		if (theScriptName == null) {
-			
+
 			// Check for Quick Script
 			if (plugin.getAssignments().contains("Denizens." + theDenizen.getName() + ".Quick Scripts.Damage Trigger.Script")) {
 
 				if (plugin.debugMode) cs.sendMessage(ChatColor.LIGHT_PURPLE + "+- Parsing QUICK DAMAGE script: " + theDenizen.getName() + "/" + thePlayer.getName() + " -+");
-				
+
 				/* Get the contents of the Script. */
 				List<String> theScript = plugin.getAssignments().getStringList("Denizens." + theDenizen.getName() + ".Quick Scripts.Damage Trigger.Script");
 
@@ -79,19 +91,19 @@ public class DamageTrigger extends AbstractTrigger implements Listener {
 
 				/* Build scriptEntries from theScript and add it into the queue */
 				sE.queueScriptEntries(thePlayer, sE.buildScriptEntries(thePlayer, theDenizen, theScript, theDenizen.getName() + " Quick Damage", 1), QueueType.TASK);
-				
+
 				return true;
 			}
 		}
 
 		if (plugin.debugMode) cs.sendMessage(ChatColor.LIGHT_PURPLE + "+- Parsing damage trigger: " + theDenizen.getName() + "/" + thePlayer.getName() + " -+");
 		if (plugin.debugMode) cs.sendMessage(ChatColor.LIGHT_PURPLE + "| " + ChatColor.WHITE + "Getting current step:");
-		
+
 		Integer theStep = sE.getCurrentStep(thePlayer, theScriptName);
 		List<String> theScript = sE.getScript(sE.getTriggerPath(theScriptName, theStep, triggerName) + sE.scriptString);
-		
+
 		if (theScript.isEmpty()) return false;
-		
+
 		sE.queueScriptEntries(thePlayer, sE.buildScriptEntries(thePlayer, theDenizen, theScript, theScriptName, theStep), QueueType.TRIGGER);
 
 		return true;
