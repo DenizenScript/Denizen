6fc5b35b1b7fe266cb7ee831585e07ce2ecda4da
Jeremy Schroeder <jeremy@ssdbg.com>
2012-12-01 22:31:38 -0500

    Add matchesToggle(arg) and getBooleanFrom(arg) to aH.
    Also clean up on some existing methods.

--- src/main/java/net/aufdemrand/denizen/scripts/helpers/ArgumentHelper.java
+++ src/main/java/net/aufdemrand/denizen/scripts/helpers/ArgumentHelper.java
@@ -25,20 +25,20 @@ import net.aufdemrand.denizen.utilities.debugging.Debugger;
 
 public class ArgumentHelper {
 
-	Debugger dB;
-	Denizen denizen;
+    Debugger dB;
+    Denizen denizen;
 
-	public enum ArgumentType {
-		String, Word, Integer, Double, Float, Custom
-	}
-	
-	public ArgumentHelper(Denizen denizen) {
-		this.denizen = denizen;
-		dB = denizen.getDebugger();
-	}
-	
-	/**
-	 *   dScript Argument         JAVA Parse Method      JAVA Get Method       Returns       Pattern Matcher (Case_Insensitive)
+    public enum ArgumentType {
+        String, Word, Integer, Double, Float, Custom
+    }
+
+    public ArgumentHelper(Denizen denizen) {
+        this.denizen = denizen;
+        dB = denizen.getDebugger();
+    }
+
+    /**
+     *   dScript Argument         JAVA Parse Method      JAVA Get Method       Returns       Pattern Matcher (Case_Insensitive)
      *  +------------------------+----------------------+---------------------+-------------+----------------------------------------------
      *   NPCID:#                  parsed in executer     done in executer      DenizenNPC    (matched against current NPCs)
      *   PLAYER:player_name       parsed in executer     done in executer      Player        (matched against online/offline Players)
@@ -55,29 +55,30 @@ public class ArgumentHelper {
      *   'NPCs rule!' (String)    matchesString(arg)     getStringFrom(arg)    String        \.+
      *   single_word (Word)       matchesWord(arg)       getStringFrom(arg)    String        \w+
      *   string|string2 (List)    ...                    getListFrom(arg)      List<String>  String.split("|")
-	 */
+     */
 
-	final Pattern durationPattern = Pattern.compile("duration:(\\d+)", Pattern.CASE_INSENSITIVE);
-	final Pattern scriptPattern = Pattern.compile("script:.+", Pattern.CASE_INSENSITIVE);
-	final Pattern locationPattern = Pattern.compile("location:\\d+,\\d+,\\d+,\\w+", Pattern.CASE_INSENSITIVE);
-	final Pattern queuetypePattern = Pattern.compile("(?:queue|queuetype):(?:player|player_task|npc)", Pattern.CASE_INSENSITIVE);
-	final Pattern quantityPattern = Pattern.compile("qty:\\d+", Pattern.CASE_INSENSITIVE);
+    final Pattern durationPattern = Pattern.compile("duration:(\\d+)", Pattern.CASE_INSENSITIVE);
+    final Pattern scriptPattern = Pattern.compile("script:.+", Pattern.CASE_INSENSITIVE);
+    final Pattern locationPattern = Pattern.compile("location:\\d+,\\d+,\\d+,\\w+", Pattern.CASE_INSENSITIVE);
+    final Pattern queuetypePattern = Pattern.compile("(?:queue|queuetype):(?:player|player_task|npc)", Pattern.CASE_INSENSITIVE);
+    final Pattern quantityPattern = Pattern.compile("qty:\\d+", Pattern.CASE_INSENSITIVE);
     final Pattern materialPattern = Pattern.compile("[a-zA-Z\\x5F]+", Pattern.CASE_INSENSITIVE);
     final Pattern materialDataPattern = Pattern.compile("[a-zA-Z]+?:\\d+", Pattern.CASE_INSENSITIVE);
-	final Pattern itemIdPattern = Pattern.compile("(?:(item:)|)\\d+");
-	final Pattern itemIdDataPattern = Pattern.compile("(?:(item:)|)(\\d+)(:)(\\d+)");
+    final Pattern itemIdPattern = Pattern.compile("(?:(item:)|)\\d+");
+    final Pattern itemIdDataPattern = Pattern.compile("(?:(item:)|)(\\d+)(:)(\\d+)");
     final Pattern integerPattern = Pattern.compile("\\d+");
     final Pattern doublePattern = Pattern.compile("\\d+\\.\\d+");
     final Pattern floatPattern = Pattern.compile("^[-+]?[0-9]+[.]?[0-9]*([eE][-+]?[0-9]+)?$");
-	final Pattern stringPattern = Pattern.compile("\\.+", Pattern.CASE_INSENSITIVE);
-	final Pattern wordPattern = Pattern.compile("\\w+", Pattern.CASE_INSENSITIVE);
+    final Pattern stringPattern = Pattern.compile("\\.+", Pattern.CASE_INSENSITIVE);
+    final Pattern wordPattern = Pattern.compile("\\w+", Pattern.CASE_INSENSITIVE);
+    final Pattern togglePattern = Pattern.compile("toggle:(true|false)", Pattern.CASE_INSENSITIVE);
 
-	/*
-	 * Argument Matchers
-	 */
+    /*
+     * Argument Matchers
+     */
 
-	/**
-	 * In practice, the remaining standard arguments should be used whenever possible to keep things consistant across the entire 
+    /**
+     * In practice, the remaining standard arguments should be used whenever possible to keep things consistant across the entire 
      * Denizen experience. Should you need to use custom arguments, however, there is support for that as well. After all, while using 
      * standard arguments is nice, you should never reach. Arguments should make as much sense to the user as possible.
      * 
@@ -104,63 +105,63 @@ public class ArgumentHelper {
      * Valid ArgumentTypes: ArgumentType.String, ArgumentType.Word, ArgumentType.Integer, ArgumentType.Double, ArgumentType.Float,
      * and ArgumentType.Custom
      * 
-	 */
-	
-	public boolean matchesValueArg(String argumentName, String argument, ArgumentType type) {
-		if (argument == null) return false;
-		if (argument.split(":").length == 1) return false;
-		if (!argument.toUpperCase().contains(argumentName.toUpperCase() + ":")) return false;
-		argument = argument.split(":", 2)[1];
-		Matcher m;
-		
-		switch (type) {
-		case Word:
-			m = wordPattern.matcher(argument);
-			return m.matches();
+     */
 
-		case Integer:
-			m = integerPattern.matcher(argument);
-			return m.matches();
+    public boolean matchesValueArg(String argumentName, String argument, ArgumentType type) {
+        if (argument == null) return false;
+        if (argument.split(":").length == 1) return false;
+        if (!argument.toUpperCase().contains(argumentName.toUpperCase() + ":")) return false;
+        argument = argument.split(":", 2)[1];
+        Matcher m;
 
-		case Double:
-			m = doublePattern.matcher(argument);
-			return m.matches();
+        switch (type) {
+        case Word:
+            m = wordPattern.matcher(argument);
+            return m.matches();
 
-		case Float:
-			m = floatPattern.matcher(argument);
-			return m.matches();
+        case Integer:
+            m = integerPattern.matcher(argument);
+            return m.matches();
 
-		default:
-			return true;
-		}
-	}
+        case Double:
+            m = doublePattern.matcher(argument);
+            return m.matches();
 
-	public boolean matchesArg(String name, String argument) {
-		if (argument.toUpperCase().equals(name.toUpperCase())) return true;
-		return false;
-	}
+        case Float:
+            m = floatPattern.matcher(argument);
+            return m.matches();
 
-	public boolean matchesInteger(String argument) {
-		Matcher m = integerPattern.matcher(argument);
-		return m.matches();
-	}
-	
-	public boolean matchesDouble(String argument) {
-		Matcher m = doublePattern.matcher(argument);
-		return m.matches();
-	}
-	
-	public boolean matchesLocation(String argument) {
+        default:
+            return true;
+        }
+    }
+
+    public boolean matchesArg(String name, String argument) {
+        if (argument.toUpperCase().equals(name.toUpperCase())) return true;
+        return false;
+    }
+
+    public boolean matchesInteger(String argument) {
+        Matcher m = integerPattern.matcher(argument);
+        return m.matches();
+    }
+
+    public boolean matchesDouble(String argument) {
+        Matcher m = doublePattern.matcher(argument);
+        return m.matches();
+    }
+
+    public boolean matchesLocation(String argument) {
         Matcher m = locationPattern.matcher(argument);
         return m.matches();
     }
 
-	public boolean matchesItem(String argument) {
-	    Matcher m;
-		m = itemIdPattern.matcher(argument);
-		if (m.matches())
-			return true;
-		m = itemIdDataPattern.matcher(argument);
+    public boolean matchesItem(String argument) {
+        Matcher m;
+        m = itemIdPattern.matcher(argument);
+        if (m.matches())
+            return true;
+        m = itemIdDataPattern.matcher(argument);
         if (m.matches())
             return true;
         // Strip ITEM: to check against a valid Material
@@ -172,7 +173,7 @@ public class ArgumentHelper {
             for (Material mat : Material.values())
                 if (mat.name().equalsIgnoreCase(argument))
                     return true;
-            }
+        }
         m = materialDataPattern.matcher(argument);
         if (m.matches()) {
             if (argument.split(":").length == 2)
@@ -182,112 +183,123 @@ public class ArgumentHelper {
                     return true;
         }
         // No match!
-		return false;
-	}
+        return false;
+    }
 
-	public boolean matchesDuration(String regex) {
-		Matcher m = durationPattern.matcher(regex.toLowerCase());
-		return m.matches();
-	}
+    public boolean matchesDuration(String regex) {
+        Matcher m = durationPattern.matcher(regex);
+        return m.matches();
+    }
 
-	public boolean matchesQueueType(String regex) {
-		Matcher m = queuetypePattern.matcher(regex.toLowerCase());
-		return m.matches();
-	}
+    public boolean matchesToggle(String regex) {
+        Matcher m = togglePattern.matcher(regex);
+        return m.matches();
+    }
 
-	public boolean matchesScript(String regex) {
-		Matcher m = scriptPattern.matcher(regex.toLowerCase());
-		// Check if script exists by looking for  Script Name:
-		//                                          Type: ...
-		if (m.matches() && denizen.getScripts().contains(regex.toUpperCase() + ".TYPE"))
-		    return true;
-		return false;
-	}
+    public boolean matchesQueueType(String regex) {
+        Matcher m = queuetypePattern.matcher(regex);
+        return m.matches();
+    }
 
-	public boolean matchesQuantity(String regex) {
-		Matcher m = quantityPattern.matcher(regex.toLowerCase());
-		return m.matches();
-	}
+    public boolean matchesScript(String regex) {
+        Matcher m = scriptPattern.matcher(regex);
+        // Check if script exists by looking for  Script Name:
+        //                                          Type: ...
+        if (m.matches() && denizen.getScripts().contains(regex.toUpperCase() + ".TYPE"))
+            return true;
+        return false;
+    }
 
-	/*
-	 * Argument Extractors
-	 */
+    public boolean matchesQuantity(String regex) {
+        Matcher m = quantityPattern.matcher(regex);
+        return m.matches();
+    }
 
-	public String getStringFrom(String argument) {
-		if (argument.split(":").length >= 2)
-			return argument.split(":")[1];
-		else return argument;
-	}
+    /*
+     * Argument Extractors
+     */
 
-	public Player getPlayerFrom(String argument) {
-		if (argument.split(":").length >= 2)
-			return denizen.getServer().getPlayer(argument.split(":")[1]);
-		else return denizen.getServer().getPlayer(argument);
-	}
+    public boolean getBooleanFrom(String argument) {
+        if (argument.split(":").length >= 2)
+            return Boolean.valueOf(argument.split(":")[1]).booleanValue();
+        else return false;
+    }
 
-	public Location getLocationFrom(String argument) {
-	    argument = argument.split(":", 2)[1];
-	    String[] num = argument.split(",");
-	    Location location = null;
-	    try {
-	        location = new Location(Bukkit.getWorld(num[3]), Double.valueOf(num[1]), Double.valueOf(num[2]), Double.valueOf(num[0]));
-	    } catch (Exception e) { dB.echoError("Invalid Location!"); return null; }
-	    return location;
-	}
-	
-	public List<String> getListFrom(String argument) {
-	    return Arrays.asList(argument.split("\\|"));
-	}
-	
-	public QueueType getQueueFrom(String argument) {
-		try { if (argument.split(":").length >= 2)
-			return QueueType.valueOf(argument.split(":")[1].toUpperCase());
-		else return QueueType.valueOf(argument.toUpperCase());
-		} catch (Exception e) { dB.echoError("Invalid Queuetype!"); }
-		return null;
-	}
+    public String getStringFrom(String argument) {
+        if (argument.split(":").length >= 2)
+            return argument.split(":")[1];
+        else return argument;
+    }
 
-	public Integer getIntegerFrom(String argument) {
-		try { if (argument.split(":").length >= 2)
-			return Integer.valueOf(argument.split(":")[1]);
-		else return Integer.valueOf(argument);
-		} catch (Exception e) { return 0; }
-	}
+    public Player getPlayerFrom(String argument) {
+        if (argument.split(":").length >= 2)
+            return denizen.getServer().getPlayer(argument.split(":")[1]);
+        else return denizen.getServer().getPlayer(argument);
+    }
 
-	public Float getFloatFrom(String argument) {
-		try { if (argument.split(":").length >= 2)
-			return Float.valueOf(argument.split(":")[1]);
-		else return Float.valueOf(argument);
-		} catch (Exception e) { return 0f; }
-	}
+    public Location getLocationFrom(String argument) {
+        argument = argument.split(":", 2)[1];
+        String[] num = argument.split(",");
+        Location location = null;
+        try {
+            location = new Location(Bukkit.getWorld(num[3]), Double.valueOf(num[1]), Double.valueOf(num[2]), Double.valueOf(num[0]));
+        } catch (Exception e) { dB.echoError("Invalid Location!"); return null; }
+        return location;
+    }
 
-	public Double getDoubleFrom(String argument) {
-		try { if (argument.split(":").length >= 2)
-			return Double.valueOf(argument.split(":")[1]);
-		else return Double.valueOf(argument);
-		} catch (Exception e) { return 0.00; }
-	}
+    public List<String> getListFrom(String argument) {
+        return Arrays.asList(argument.split("\\|"));
+    }
 
-	public ItemStack getItemFrom(String thisArg) {
-		Matcher m = itemIdPattern.matcher(thisArg);
-		Matcher m2 = itemIdDataPattern.matcher(thisArg);
-		Matcher m3 = materialPattern.matcher(thisArg);
-		Matcher m4 = materialDataPattern.matcher(thisArg);
-		ItemStack stack = null;
-		try {
-			if (m.matches())
-				stack = new ItemStack(Integer.valueOf(thisArg));
-			else if (m2.matches()) {
-				stack = new ItemStack(Integer.valueOf(thisArg.split(":")[0]));
-				stack.setDurability(Short.valueOf(thisArg.split(":")[1]));
-			} else if (m3.matches()) {
-				stack = new ItemStack(Material.valueOf(thisArg.toUpperCase()));
-			} else if (m4.matches()) {
-				stack = new ItemStack(Material.valueOf(thisArg.split(":")[0].toUpperCase()));
-				stack.setDurability(Short.valueOf(thisArg.split(":")[1]));
-			}
-		} catch (Exception e) { dB.echoError("Invalid item!"); if (denizen.getDebugger().showStackTraces) e.printStackTrace(); }
-		return stack;
-	}
+    public QueueType getQueueFrom(String argument) {
+        try { if (argument.split(":").length >= 2)
+            return QueueType.valueOf(argument.split(":")[1].toUpperCase());
+        else return QueueType.valueOf(argument.toUpperCase());
+        } catch (Exception e) { dB.echoError("Invalid Queuetype!"); }
+        return null;
+    }
+
+    public Integer getIntegerFrom(String argument) {
+        try { if (argument.split(":").length >= 2)
+            return Integer.valueOf(argument.split(":")[1]);
+        else return Integer.valueOf(argument);
+        } catch (Exception e) { return 0; }
+    }
+
+    public Float getFloatFrom(String argument) {
+        try { if (argument.split(":").length >= 2)
+            return Float.valueOf(argument.split(":")[1]);
+        else return Float.valueOf(argument);
+        } catch (Exception e) { return 0f; }
+    }
+
+    public Double getDoubleFrom(String argument) {
+        try { if (argument.split(":").length >= 2)
+            return Double.valueOf(argument.split(":")[1]);
+        else return Double.valueOf(argument);
+        } catch (Exception e) { return 0.00; }
+    }
+
+    public ItemStack getItemFrom(String thisArg) {
+        Matcher m = itemIdPattern.matcher(thisArg);
+        Matcher m2 = itemIdDataPattern.matcher(thisArg);
+        Matcher m3 = materialPattern.matcher(thisArg);
+        Matcher m4 = materialDataPattern.matcher(thisArg);
+        ItemStack stack = null;
+        try {
+            if (m.matches())
+                stack = new ItemStack(Integer.valueOf(thisArg));
+            else if (m2.matches()) {
+                stack = new ItemStack(Integer.valueOf(thisArg.split(":")[0]));
+                stack.setDurability(Short.valueOf(thisArg.split(":")[1]));
+            } else if (m3.matches()) {
+                stack = new ItemStack(Material.valueOf(thisArg.toUpperCase()));
+            } else if (m4.matches()) {
+                stack = new ItemStack(Material.valueOf(thisArg.split(":")[0].toUpperCase()));
+                stack.setDurability(Short.valueOf(thisArg.split(":")[1]));
+            }
+        } catch (Exception e) { dB.echoError("Invalid item!"); if (denizen.getDebugger().showStackTraces) e.printStackTrace(); }
+        return stack;
+    }
 
 }
