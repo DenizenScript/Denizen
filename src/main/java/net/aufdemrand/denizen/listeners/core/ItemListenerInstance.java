02508c56e336ba77715e6606c7be44e96a28bb1e
Mason Adkins <zappalives0@gmail.com>
2012-12-13 01:16:17 -0500

    Ported LISTEN ITEM to 0.8
    Code originally created by man_cub, will add to author documentation
    when added in. This is currently a rough port, will need testing and
    load() finished. Plans to add ENCHANT as an ItemType.

---
+++ src/main/java/net/aufdemrand/denizen/listeners/core/ItemListenerInstance.java
@@ -0,0 +1,182 @@
+package net.aufdemrand.denizen.listeners.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import net.aufdemrand.denizen.listeners.AbstractListener;
+import net.aufdemrand.denizen.listeners.core.ItemListenerType.ItemType;
+import net.aufdemrand.denizen.listeners.core.KillListenerType.KillType;
+import net.aufdemrand.denizen.scripts.helpers.ArgumentHelper.ArgumentType;
+import net.aufdemrand.denizen.utilities.debugging.Debugger.Messages;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.FurnaceSmeltEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerFishEvent;
+
+public class ItemListenerInstance extends AbstractListener implements Listener {
+
+	ItemType type;
+	List<String> items;
+	int quantity = 0;
+	String listenerId;
+	
+	int currentItems = 0;
+	
+	@Override
+	public void onBuild(List<String> args) {
+		
+		for (String arg : args) {
+			if (aH.matchesValueArg("TYPE", arg, ArgumentType.Custom)) {
+				try { 
+					this.type = ItemType.valueOf(arg); 
+					dB.echoDebug(Messages.DEBUG_SET_TYPE, this.type.name());
+				} catch (Exception e) { }
+
+			} else if (aH.matchesQuantity(arg)) {
+				this.quantity = aH.getIntegerFrom(arg);
+				dB.echoDebug(Messages.DEBUG_SET_QUANTITY, String.valueOf(quantity));
+
+			} else if (aH.matchesValueArg("ITEMS", arg, ArgumentType.Custom)) {
+				items = aH.getListFrom(arg);
+				dB.echoDebug("...set ITEMS.");
+			}
+
+			if (items.isEmpty()) {
+				dB.echoError("Missing ITEMS argument!");
+				cancel();
+			}
+			
+			if (type == null) {
+				dB.echoError("Missing TYPE argument! Valid: CRAFT, SMELT, FISH");
+				cancel();
+			}
+			
+			denizen.getServer().getPluginManager().registerEvents((Listener) this, denizen);
+		}
+	}
+
+	List<Integer> itemsCrafted = new ArrayList<Integer>();
+	@EventHandler
+	public void listenCraft(CraftItemEvent event) {
+		if (type == ItemType.CRAFT) {
+			if (event.getWhoClicked() == player) {
+				if (items.contains(event.getCurrentItem().getType().toString()) 
+						|| items.contains(String.valueOf(event.getCurrentItem().getTypeId()))) {
+					if (itemsCrafted.contains(event.getCurrentItem().getTypeId()))
+						return;
+					else itemsCrafted.add(event.getCurrentItem().getTypeId());
+
+					currentItems++;
+					dB.echoDebug(ChatColor.YELLOW + "// " + player.getName() + " crafted a " + event.getCurrentItem().getType().toString() + ".");
+					complete(false);
+				}
+			}
+		}
+	}
+
+	List<Integer> itemsSmelted = new ArrayList<Integer>();
+	@EventHandler
+	public void listenSmelt(FurnaceSmeltEvent event) {
+		if (type == ItemType.SMELT) {
+			InventoryClickEvent e = (InventoryClickEvent) player;
+			if (event.getBlock() == e.getCurrentItem()) {
+				if (items.contains(event.getBlock().getType().toString()) 
+						|| items.contains(String.valueOf(event.getBlock().getTypeId()))) {
+					if (itemsSmelted.contains(event.getBlock().getTypeId()))
+						return;
+					else itemsSmelted.add(event.getBlock().getTypeId());
+
+					currentItems++;
+					dB.echoDebug(ChatColor.YELLOW + "// " + player.getName() + " smelted a " + event.getBlock().getType().toString() + ".");
+					complete(false);
+				}
+			}
+		}
+	}
+
+	List<EntityType> itemsFished = new ArrayList<EntityType>();
+	@EventHandler
+	public void listenFish(PlayerFishEvent event) {
+		if (type == ItemType.FISH) {
+			if (event.getPlayer() == player) {
+				if (items.contains(event.getCaught().getType().toString())) {
+					if (itemsFished.contains(event.getCaught().getType()))
+						return;
+					else itemsFished.add(event.getCaught().getType());
+
+					currentItems++;
+					dB.echoDebug(ChatColor.YELLOW + "// " + player.getName() + " fished a " + event.getCaught().getType().toString() + ".");
+					complete(false);
+				}
+			}
+		}
+	}
+	
+	@Override
+	public void onSave() {
+		try {
+			store("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Listen Type", "ITEM");
+			store("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Script", this.scriptName);
+			store("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Type", this.type.toString());
+			store("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Items", this.items);
+			store("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Quantity", this.quantity);
+			store("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Current Items", this.currentItems);
+		} catch (Exception e) {
+			dB.echoError("Unable to save ITEM listener for '%s'!", player.getName());
+		}
+	}
+
+	@Override
+	public void onLoad() {
+		// NOT ENTIRELY SURE HOW THE LOADING PART WORKS
+		/*
+		try {
+			this.type = ItemType.valueOf(plugin.getSaves().getString("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Type"));
+			this.items = plugin.getSaves().getStringList("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Items");
+			this.quantity = plugin.getSaves().getInt("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Quantity");
+			this.currentItems = plugin.getSaves().getInt("Players." + player.getName() + ".Listeners.Saves." + listenerId + ".Current Items");
+		} catch (Exception e) { 
+			dB.echoError("Unable to load ITEM listener for '%s'!", player.getName());
+			cancel();
+		}
+		 */
+	}
+
+	@Override
+	public void onFinish() {
+
+	}
+
+	public void complete(boolean forceable) {
+		if (quantity >= currentItems || forceable) {
+			CraftItemEvent.getHandlerList().unregister(this);
+			FurnaceSmeltEvent.getHandlerList().unregister(this);
+			InventoryClickEvent.getHandlerList().unregister(this);
+			PlayerFishEvent.getHandlerList().unregister(this);
+
+			finish();
+		}
+
+	}
+	
+	@Override
+	public void onCancel() {
+		CraftItemEvent.getHandlerList().unregister(this);
+		FurnaceSmeltEvent.getHandlerList().unregister(this);
+		InventoryClickEvent.getHandlerList().unregister(this);
+	}
+
+	@Override
+	public String report() {
+		return player.getName() + " current has quest listener '" + listenerId 
+				+ "' active and must " + type.name() + " " + Arrays.toString(items.toArray())
+				+ " '(s). Current progress '" + currentItems + "/" + quantity + "'.";
+	}
+
+}
---
+++ src/main/java/net/aufdemrand/denizen/listeners/core/ItemListenerType.java
@@ -0,0 +1,13 @@
+package net.aufdemrand.denizen.listeners.core;
+
+import net.aufdemrand.denizen.listeners.AbstractListenerType;
+
+public class ItemListenerType extends AbstractListenerType {
+
+	public enum ItemType {CRAFT, SMELT, FISH}
+	
+	@Override
+	public void onEnable() {
+		
+	}
+}
