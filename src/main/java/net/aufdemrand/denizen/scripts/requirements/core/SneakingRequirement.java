ba60a2909e90ee9023d13ff6b2d11526d4a33f7a
Mason Adkins <zappalives0@gmail.com>
2012-12-15 13:27:31 -0500

    Ported SNEAKIONG requirement

---
+++ src/main/java/net/aufdemrand/denizen/scripts/requirements/core/SneakingRequirement.java
@@ -0,0 +1,33 @@
+package net.aufdemrand.denizen.scripts.requirements.core;
+
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
+import net.aufdemrand.denizen.exceptions.RequirementCheckException;
+import net.aufdemrand.denizen.npc.DenizenNPC;
+import net.aufdemrand.denizen.scripts.requirements.AbstractRequirement;
+
+public class SneakingRequirement extends AbstractRequirement {
+
+	@Override
+	public void onEnable() {
+		// nothing to do here
+	}
+
+	@Override
+	public boolean check(Player player, DenizenNPC npc, String scriptName,
+			List<String> args) throws RequirementCheckException {
+		boolean outcome = false;
+
+		if (player.isSneaking()) {
+			outcome = true;
+		}
+
+		if (outcome == true) dB.echoDebug("...player is sneaking!");
+		else dB.echoDebug("...player is not sneaking!");
+
+		return outcome;
+	}
+
+}
