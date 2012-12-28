0330d1bd5e25c2f4414062bf1d1367fc0b6a53eb
Mason Adkins <zappalives0@gmail.com>
2012-12-27 23:25:21 -0500

    GIVE command added

--- src/main/java/net/aufdemrand/denizen/scripts/commands/CommandRegistry.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/CommandRegistry.java
@@ -19,6 +19,7 @@ import net.aufdemrand.denizen.scripts.commands.core.FailCommand;
 import net.aufdemrand.denizen.scripts.commands.core.FeedCommand;
 import net.aufdemrand.denizen.scripts.commands.core.FinishCommand;
 import net.aufdemrand.denizen.scripts.commands.core.FlagCommand;
+import net.aufdemrand.denizen.scripts.commands.core.GiveCommand;
 import net.aufdemrand.denizen.scripts.commands.core.HealCommand;
 import net.aufdemrand.denizen.scripts.commands.core.IfCommand;
 import net.aufdemrand.denizen.scripts.commands.core.ListenCommand;
@@ -81,6 +82,7 @@ public class CommandRegistry implements DenizenRegistry {
 		new FeedCommand().activate().as("FEED").withOptions("(AMT:#) (TARGET:NPC|PLAYER)", 0);
 		new FinishCommand().activate().as("FINISH").withOptions("(PLAYER:player_name)", 0);
 		new FlagCommand().activate().as("FLAG").withOptions("(DENIZEN|PLAYER|GLOBAL) [[NAME([#])]:[VALUE]|[NAME]:[FLAG_ACTION]:(VALUE)]", 1);
+		new GiveCommand().activate().as("FLAG").withOptions("[MONEY|#(:#)|MATERIAL_TYPE(:#)] (QTY:#)", 1);
 		new HealCommand().activate().as("HEAL").withOptions("(AMT:#) (TARGET:NPC|PLAYER)", 0);
 		new IfCommand().activate().as("IF").withOptions("(!)[COMPARABLE] (OPERATOR) (COMPARED_TO) (BRIDGE) (...) [COMMAND] (ELSE) (COMMAND) // see documentation.", 2);
 		new ListenCommand().activate().as("LISTEN").withOptions("[Listener_Type] [ID:ListenerID] [Listener Arguments] // see documentation.", 2);
---
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/GiveCommand.java
@@ -0,0 +1,141 @@
+package net.aufdemrand.denizen.scripts.commands.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.RegisteredServiceProvider;
+
+import net.aufdemrand.denizen.exceptions.CommandExecutionException;
+import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
+import net.aufdemrand.denizen.scripts.ScriptEntry;
+import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
+import net.aufdemrand.denizen.utilities.debugging.dB;
+import net.citizensnpcs.util.Messages;
+import net.citizensnpcs.util.Messaging;
+import net.milkbowl.vault.economy.Economy;
+
+/* GIVE [MONEY|#(:#)|MATERIAL_TYPE(:#)] (QTY:#) */
+
+/* 
+ * Arguments: [] - Required, () - Optional 
+ * [MONEY|[#](:#)|[MATERIAL_TYPE](:#)] specifies what to give.
+ *   [MONEY] gives money using your economy.
+ *   [#](:#) gives the item with the specified item ID. Optional
+ *     argument (:#) can specify a specific data value.
+ *   [MATERIAL_TYPE](:#) gives the item with the specified
+ *     bukkit MaterialType. Optional argument (:#) can specify
+ *     a specific data value.
+ * (QTY:#) specifies quantity. If not specified, assumed 'QTY:1'    
+ *  
+ */
+
+enum GiveType { ITEM, MONEY, EXP, HEROESEXP }
+
+public class GiveCommand  extends AbstractCommand {
+	Player player;
+	GiveType giveType;
+	int theAmount;
+	ItemStack theItem;
+
+	@Override
+	public void onEnable() {
+		//nothing to do here
+	}
+
+	@Override
+	public void parseArgs(ScriptEntry scriptEntry)
+			throws InvalidArgumentsException {
+		/* Initialize variables */ 
+	
+		if (scriptEntry.getArguments () == null) {
+			throw new InvalidArgumentsException ("...Usage: GIVE [MONEY|#(:#)|MATERIAL_TYPE(:#)] (QTY:#)");
+		}
+		
+		player = scriptEntry.getPlayer();
+		giveType = null;
+		theAmount = 1;
+		theItem = null;
+	
+		/* Match arguments to expected variables */
+		for (String thisArg : scriptEntry.getArguments()) {
+			if (aH.matchesQuantity(thisArg)) {
+				theAmount = aH.getIntegerFrom(thisArg);
+				dB.echoDebug ("...set quantity to '%s'.", thisArg);
+			} 
+			
+			else if (aH.matchesArg("MONEY", thisArg)) {
+				this.giveType = GiveType.MONEY;
+				dB.echoDebug ("...giving MONEY.");
+			} 
+			
+			else if (aH.matchesArg("HEROESEXP", thisArg)
+					|| aH.matchesArg("HEROES_EXP", thisArg)) {
+				this.giveType = GiveType.HEROESEXP;
+				dB.echoDebug("...giving Heroes Quest EXP.");
+			} 
+			
+			else if (aH.matchesArg("XP", thisArg)
+					|| aH.matchesArg("EXP", thisArg)) {
+				this.giveType = GiveType.EXP;
+				dB.echoDebug ("...giving EXP.");
+			} 
+			
+			else if (aH.matchesItem(thisArg)) {
+				theItem = aH.getItemFrom (thisArg);
+				this.giveType = GiveType.ITEM;
+				if (theItem != null) {
+					dB.echoDebug ("...set item to be given to '%s'.", thisArg);
+				}
+			}
+			
+			else throw new InvalidArgumentsException(dB.Messages.ERROR_UNKNOWN_ARGUMENT, thisArg);
+		}			
+	}
+
+	@SuppressWarnings("incomplete-switch")
+	@Override
+	public void execute(String commandName) throws CommandExecutionException {
+		dB.echoDebug ("execute (" + commandName + ")");
+		if (giveType != null) {
+			switch (giveType) {
+
+			case MONEY:
+				 try {
+					 	RegisteredServiceProvider<Economy> provider = Bukkit.getServicesManager().getRegistration (Economy.class);
+					 	if (provider != null && provider.getProvider() != null) {
+					 		Economy economy = provider.getProvider();
+							double doubleAmount = Double.valueOf(theAmount);
+							dB.echoDebug ("Giving player " + theAmount + " money.");
+							economy.depositPlayer(player.getName(), doubleAmount);
+					 	}
+				 	} catch (NoClassDefFoundError e) {
+						dB.echoError("No economy loaded! Have you installed Vault and a compatible economy plugin?");	
+						Messaging.logTr(Messages.ERROR_LOADING_ECONOMY);
+				 	}				
+
+				 break;
+
+			case EXP:
+				player.giveExp(theAmount);
+				dB.echoDebug("..giving player " + theAmount + " EXP");
+				break;
+
+			case ITEM:
+				theItem.setAmount(theAmount);
+				dB.echoDebug("..giving player " + theAmount + " of " + theItem);
+				HashMap<Integer, ItemStack> leftovers = player.getInventory().addItem(theItem);
+
+				if (!leftovers.isEmpty()) {
+					dB.echoDebug ("...Player did not have enough space in their inventory, the rest of the items have been placed on the floor.");
+					for (Map.Entry<Integer, ItemStack> leftoverItem : leftovers.entrySet()) {
+						player.getWorld().dropItem(player.getLocation(), leftoverItem.getValue());
+					}
+				}
+				break;
+			}
+		}
+	}
+}
