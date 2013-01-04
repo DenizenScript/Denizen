2b261a93ce2bda0ebea9c33810ce0f3f1c9cc2bc
dbixler <drbixler@gmail.com>
2013-01-04 18:40:55 -0500

    Just testing a Javadoc link into Bukkit's Javadocs.

--- src/main/java/net/aufdemrand/denizen/scripts/commands/core/EnchantCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/EnchantCommand.java
@@ -8,39 +8,27 @@ import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
 /**
  * Applies an enchantment on an ItemStack
  * 
+ * Arguments: [] - Required, () - Optional 
+ * 
+ * Example Usage: * 
+ * @see <a href="http://jd.bukkit.org/dev/apidocs/org/bukkit/inventory/ItemStack.html">org.bukkit.inventory.ItemStack</a>
+ * 
  * @author Jeremy Schroeder
  */
 
 public class EnchantCommand extends AbstractCommand {
-
 	@Override
 	public void onEnable() {
 
 	}
 
-	/* 
-	 * Arguments: [] - Required, () - Optional 
-	 * 
-	 * 
-	 * 
-	 * Example Usage:
-	 * 
-	 */
-	
 	@Override
 	public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-
 		for (String arg : scriptEntry.getArguments()) {
-
-		
 		}
-
 	}
 
 	@Override
 	public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
-
 	}
-	
-	
 }
