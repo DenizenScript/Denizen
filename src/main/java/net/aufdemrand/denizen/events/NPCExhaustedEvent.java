3f31721c8312fafbc8bde910442acf29efbf76f4
Jeremy Schroeder <jeremy@ssdbg.com>
2013-01-08 20:12:32 -0500

    Add Bukkit NPCExhaustedEvent and Denizen 'On Exhausted:' Action.

--- src/main/java/net/aufdemrand/denizen/events/NPCExhaustedEvent.java
+++ src/main/java/net/aufdemrand/denizen/events/NPCExhaustedEvent.java
@@ -1,20 +1,25 @@
 package net.aufdemrand.denizen.events;
 
-import org.bukkit.event.Event;
+import net.citizensnpcs.api.event.NPCEvent;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
- * Bukkit event for when Denizen dScripts are reloaded.
+ * Bukkit event for when an NPC is exhausted. Exhausted NPCs cannot move
+ * until their hunger level is below maxhunger, as tracked by the HungerTrait
+ * provided by Denizen.
  * 
  * @author Jeremy Schroeder
  *
  */
-public class NPCExhaustedEvent extends Event {
+public class NPCExhaustedEvent extends NPCEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
+    private boolean cancelled = false;
 
-    public NPCExhaustedEvent() {
-    	// Nothing to do here.
+    public NPCExhaustedEvent(NPC npc) {
+    	super(npc);
     }
     
     public HandlerList getHandlers() {
@@ -24,4 +29,15 @@ public class NPCExhaustedEvent extends Event {
     public static HandlerList getHandlerList() {
         return handlers;
     }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
 }
