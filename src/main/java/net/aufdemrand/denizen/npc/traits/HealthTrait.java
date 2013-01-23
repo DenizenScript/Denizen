9f3dd22e90b74651e7f3b5709ad4d0a62bbb8d86
Jeremy Schroeder <jeremy@ssdbg.com>
2013-01-23 18:18:37 -0500

    Add Death actions. Use '/npc health' to activate. Valid: On Death, On Death by Player, On Death by Monster, On Death by [ENTITY_TYPE]

--- src/main/java/net/aufdemrand/denizen/npc/traits/HealthTrait.java
+++ src/main/java/net/aufdemrand/denizen/npc/traits/HealthTrait.java
@@ -1,8 +1,13 @@
 package net.aufdemrand.denizen.npc.traits;
 
+import net.aufdemrand.denizen.utilities.DenizenAPI;
+import net.citizensnpcs.api.event.NPCDamageByEntityEvent;
 import net.citizensnpcs.api.persistence.Persist;
 import net.citizensnpcs.api.trait.Trait;
 import org.bukkit.craftbukkit.v1_4_R1.entity.CraftLivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class HealthTrait extends Trait implements Listener {
@@ -12,27 +17,27 @@ public class HealthTrait extends Trait implements Listener {
     private int maxhealth = 20;
     @Persist("current")
     private int currenthealth = 20;
-	
+
     /**
      * Listens for spawn of an NPC and updates its health with saved
      * information from this Trait. If a respawn from death, sets health to maxHealth.
      * If any other type of respawn, sets health to the last known currentHealth.
      *
      */
-	@Override public void onSpawn() {
-	    if (currenthealth > 0) setHealth(currenthealth);
-	    else setHealth();
-	}
+    @Override public void onSpawn() {
+        if (currenthealth > 0) setHealth(currenthealth);
+        else setHealth();
+    }
 
     /**
      * Listens for a despawn to note currentHealth as the time. Will be used
      * to reset health on a respawn.
      *
      */
-	@Override public void onDespawn() {
-	    if (getHealth() > 0) currenthealth = getHealth();
+    @Override public void onDespawn() {
+        if (getHealth() > 0) currenthealth = getHealth();
         else currenthealth = -1;
-	}
+    }
 
     public HealthTrait() {
         super("health");
@@ -54,9 +59,9 @@ public class HealthTrait extends Trait implements Listener {
      * @param newMax new maximum health
      *
      */
-	public void setMaxhealth(int newMax) {
-		this.maxhealth = newMax;
-	}
+    public void setMaxhealth(int newMax) {
+        this.maxhealth = newMax;
+    }
 
     /**
      * Gets the maximum health for this NPC.
@@ -72,9 +77,9 @@ public class HealthTrait extends Trait implements Listener {
      *
      * @param health number of health points to heal
      */
-	public void heal(int health) {
-	    setHealth(getHealth() + health);
-	}
+    public void heal(int health) {
+        setHealth(getHealth() + health);
+    }
 
     /**
      * Sets the NPCs health to maximum.
@@ -89,9 +94,31 @@ public class HealthTrait extends Trait implements Listener {
      *
      * @param health total health points
      */
-	public void setHealth(int health) {
-	    ((CraftLivingEntity) npc.getBukkitEntity()).getHandle().setHealth(health);
-	    currenthealth = getHealth();
-	}
+    public void setHealth(int health) {
+        ((CraftLivingEntity) npc.getBukkitEntity()).getHandle().setHealth(health);
+        currenthealth = getHealth();
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onDeath(NPCDamageByEntityEvent event) {
+        // Check if the event pertains to this NPC
+        if (event.getNPC() != npc) return;
+
+        // Make sure this is a killing blow
+        if (this.getHealth() - event.getDamage() > 0)
+            return;
+
+        // It does... is killer a Player?
+        if (event.getDamager() instanceof Player) {
+            // Yep.. pass player to the action
+            DenizenAPI.getDenizenNPC(npc).action("death", (Player) event.getDamager());
+            DenizenAPI.getDenizenNPC(npc).action("death by player", (Player) event.getDamager());
+        }   else {
+            // Nope.. killed by entity
+            DenizenAPI.getDenizenNPC(npc).action("death", null);
+            DenizenAPI.getDenizenNPC(npc).action("death by monster", null);
+            DenizenAPI.getDenizenNPC(npc).action("death by " + event.getDamager().getType().name(), null);
+        }
+    }
 
 }
