116d976cc06d228471dd1c64f013f02718042eb8
Jeremy Schroeder <jeremy@ssdbg.com>
2013-01-24 07:53:05 -0500

    Modify DROP command, allow XP to be dropped. Usage: - drop xp

---
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/BreakCommand.java
@@ -0,0 +1,113 @@
+package net.aufdemrand.denizen.scripts.commands.core;
+
+import net.aufdemrand.denizen.exceptions.CommandExecutionException;
+import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
+import net.aufdemrand.denizen.npc.traits.HealthTrait;
+import net.aufdemrand.denizen.scripts.ScriptEntry;
+import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
+import net.aufdemrand.denizen.utilities.arguments.aH;
+import net.aufdemrand.denizen.utilities.arguments.aH.ArgumentType;
+import net.aufdemrand.denizen.utilities.debugging.dB;
+import net.aufdemrand.denizen.utilities.debugging.dB.Messages;
+import net.citizensnpcs.api.CitizensAPI;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+/**
+ * Feeds a (Player) entity.
+ * 
+ * @author Jeremy Schroeder, Mason Adkins
+ */
+
+public class BreakCommand extends AbstractCommand {
+
+    @Override
+    public void onEnable() {
+        // nothing to do here
+    }
+
+    /* HEAL (AMT:#) (TARGET:NPC|PLAYER) */
+
+    /* 
+     * Arguments: [] - Required, () - Optional 
+     * (AMT:#) 1-20, usually.
+     * (TARGET:NPC|PLAYER) Specifies which object is the target of the feeding effects. 
+     *          Default: Player, unless not available
+     *   
+     * Example Usage:
+     * HEAL AMT:20 TARGET:NPC
+     * HEAL AMT:5
+     * HEAL
+     *
+     */
+
+    private enum TargetType { NPC, PLAYER } 
+
+    private int amount;
+    private LivingEntity target;
+    private TargetType targetType;
+
+    @Override
+    public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
+
+        // Must reset ALL private variables, else information left over from last time
+        // might be used.
+        targetType = TargetType.PLAYER;
+        amount = Integer.MAX_VALUE;
+        // Set target to Player by default, if available
+        if (scriptEntry.getPlayer() != null) target = (LivingEntity) scriptEntry.getPlayer();
+        else target = null;
+        
+        for (String arg : scriptEntry.getArguments()) {
+
+            if (aH.matchesQuantity(arg) || aH.matchesValueArg("amt", arg, ArgumentType.Integer)) {
+                amount = aH.getIntegerFrom(arg);
+                dB.echoDebug(Messages.DEBUG_SET_QUANTITY, String.valueOf(amount));
+                continue;
+
+            }   else if (aH.matchesValueArg("target", arg, ArgumentType.String)) {
+                try {
+                    targetType = TargetType.valueOf(aH.getStringFrom(arg));
+                    dB.echoDebug("TARGET to HEAL: " + targetType.name());
+                } catch (Exception e) { dB.echoError("Invalid TARGET! Valid: NPC, PLAYER"); }
+                continue;
+
+            }   else throw new InvalidArgumentsException(Messages.ERROR_UNKNOWN_ARGUMENT, arg);
+        }
+
+        // If TARGET is NPC/PLAYER and no NPC/PLAYER available, throw exception.
+        if (targetType == TargetType.PLAYER && scriptEntry.getPlayer() == null) throw new InvalidArgumentsException(Messages.ERROR_NO_PLAYER);
+        else if (targetType == TargetType.NPC && scriptEntry.getNPC() == null) throw new InvalidArgumentsException(Messages.ERROR_NO_NPCID);
+        // If TARGET is NPC, set entity.
+        else if (targetType == TargetType.NPC) target = scriptEntry.getNPC().getEntity();
+        
+        return;
+    }
+
+    
+    @Override
+    public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
+
+        // Target is a NPC
+        if (CitizensAPI.getNPCRegistry().isNPC(target)) {
+            NPC npc = CitizensAPI.getNPCRegistry().getNPC(target);
+            if (!npc.hasTrait(HealthTrait.class)) npc.addTrait(HealthTrait.class);
+            // Set health to max
+            if (amount == Integer.MAX_VALUE) npc.getTrait(HealthTrait.class).setHealth(npc.getTrait(HealthTrait.class).getMaxhealth());
+            // else, set Health
+            else npc.getTrait(HealthTrait.class).heal(amount);
+        
+        // Target is a Player
+        } else {
+           // Set to max food level
+           if (amount == Integer.MAX_VALUE) ((Player) target).setFoodLevel(20);
+           // else, increase food levels
+           else ((Player) target).setFoodLevel(((Player) target).getFoodLevel() + amount);
+        }
+    
+        return;
+    }
+    
+    
+}
--- src/main/java/net/aufdemrand/denizen/scripts/commands/core/DropCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/DropCommand.java
@@ -1,8 +1,5 @@
 package net.aufdemrand.denizen.scripts.commands.core;
 
-import org.bukkit.Location;
-import org.bukkit.inventory.ItemStack;
-
 import net.aufdemrand.denizen.exceptions.CommandExecutionException;
 import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
 import net.aufdemrand.denizen.scripts.ScriptEntry;
@@ -10,78 +7,80 @@ import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
 import net.aufdemrand.denizen.utilities.arguments.aH;
 import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.aufdemrand.denizen.utilities.debugging.dB.Messages;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ExperienceOrb;
+import org.bukkit.inventory.ItemStack;
 
 /**
- * Drops items in a location.
- * 
+ * Drops items or experience in a location.
+ *
  * @author Jeremy Schroeder
  */
 
 public class DropCommand extends AbstractCommand {
 
-	@Override
-	public void onEnable() {
+    @Override
+    public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
 
-	}
-
-	/* 
-	 * Arguments: [] - Required, () - Optional 
-	 * 
-	 * 
-	 * 
-	 * Example Usage:
-	 * 
-	 */
-	
-	ItemStack item;
-	Location location;
-	
-	@Override
-	public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-
-		// Initialize necessary fields
-        item = null;
-		Integer qty = null;
-		location = null;
+        // Initialize necessary fields
+        ItemStack item = null;
+        Integer qty = null;
+        Location location = null;
+        Boolean exp = false;
 
         // Set some defaults
+        if (scriptEntry.getPlayer() != null) location = scriptEntry.getPlayer().getLocation();
+        if (location == null && scriptEntry.getNPC() != null) location = scriptEntry.getNPC().getLocation();
 
+        for (String arg : scriptEntry.getArguments()) {
+            if (aH.matchesItem(arg)) {
+                item = aH.getItemFrom(arg);
 
-		for (String arg : scriptEntry.getArguments()) {
-			
-			if (aH.matchesItem(arg)) {
-				 item = aH.getItemFrom(arg);
-				 dB.echoDebug("...set ITEM: '%s'", aH.getStringFrom(arg));
-				 continue;
-				
-			} else if (aH.matchesQuantity(arg)) {
-				 qty = aH.getIntegerFrom(arg);
-				 dB.echoDebug(Messages.DEBUG_SET_QUANTITY, String.valueOf(qty));
-				 continue;
-			
-			} else if (aH.matchesLocation(arg)) {
-				 location = aH.getLocationFrom(arg);
-				 dB.echoDebug(Messages.DEBUG_SET_LOCATION, aH.getStringFrom(arg));
-				 continue;
-				 
-			} else {
-				
-			}
-		}
+            } else if (aH.matchesArg("XP, EXP", arg)) {
+                exp = true;
 
-		if (item == null) throw new InvalidArgumentsException(Messages.ERROR_INVALID_ITEM);
-		if (location == null) throw new InvalidArgumentsException(Messages.ERROR_MISSING_LOCATION);
-		
-		if (qty != null)
-			item.setAmount(qty);
-	}
+            } else if (aH.matchesQuantity(arg)) {
+                qty = aH.getIntegerFrom(arg);
 
-	@Override
-	public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
+            } else if (aH.matchesLocation(arg)) {
+                location = aH.getLocationFrom(arg);
 
-		location.getWorld().dropItemNaturally(location, item);
+            } else throw new InvalidArgumentsException(Messages.ERROR_UNKNOWN_ARGUMENT, arg);
+        }
 
-	}
-	
-	
+        if (item == null && !exp) throw new InvalidArgumentsException(Messages.ERROR_INVALID_ITEM);
+        if (location == null) throw new InvalidArgumentsException(Messages.ERROR_MISSING_LOCATION);
+
+        scriptEntry.addObject("location", location);
+        scriptEntry.addObject("item", item);
+        scriptEntry.addObject("exp", exp);
+        scriptEntry.addObject("qty", qty);
+    }
+
+    @Override
+    public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
+
+        Location location = (Location) scriptEntry.getObject("location");
+        Integer qty = (Integer) scriptEntry.getObject("qty");
+        Boolean exp = (Boolean) scriptEntry.getObject("exp");
+        ItemStack item = (ItemStack) scriptEntry.getObject("item");
+
+        if (qty != null && item != null)
+            item.setAmount(qty);
+        else qty = 1;
+
+        dB.echoApproval(ChatColor.DARK_GRAY + "Executing '" + ChatColor.YELLOW +  getName() + ChatColor.DARK_GRAY + "': "
+                + "Location='" + ChatColor.YELLOW + location.getBlockX() + "," + location.getBlockY()
+                + "," + location.getBlockZ() + "," + location.getWorld().getName() + ChatColor.GRAY + "', "
+                + (item != null ? "Item='" + ChatColor.YELLOW + item.getType().toString() + "(" + qty + ")" + ChatColor.DARK_GRAY + "'"
+                : "Exp=" + ChatColor.YELLOW + qty + ChatColor.DARK_GRAY + "'"));
+
+        if (exp)
+            ((ExperienceOrb) location.getWorld().spawnEntity(location, EntityType.EXPERIENCE_ORB)).setExperience(qty);
+        else location.getWorld().dropItemNaturally(location, item);
+
+    }
+
 }
