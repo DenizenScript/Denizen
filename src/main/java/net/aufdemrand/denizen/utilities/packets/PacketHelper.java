ad8769a55737e12fc88ea29687b783a2832a9f39
Jeremy Schroeder <jeremy@ssdbg.com>
2013-01-26 18:19:06 -0500

    ...

--- src/main/java/net/aufdemrand/denizen/utilities/packets/PacketHelper.java
+++ src/main/java/net/aufdemrand/denizen/utilities/packets/PacketHelper.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package net.aufdemrand.denizen.utilities.packets;
 
 import net.aufdemrand.denizen.Denizen;
@@ -20,6 +21,29 @@ public class PacketHelper {
 	}
 	
 }
+=======
+package net.aufdemrand.denizen.utilities.packets;
+
+import net.aufdemrand.denizen.Denizen;
+import net.aufdemrand.denizen.utilities.Depends;
+import net.aufdemrand.denizen.utilities.packets.handler.NameplateHandler;
+
+public class PacketHelper {
+
+	private final NameplateHandler npHandler;
+	
+	public PacketHelper(Denizen denizen) {
+		npHandler = new NameplateHandler(denizen);
+		
+		Depends.protocolManager.addPacketListener(getNameplateHandler());
+	}
+	
+	public final NameplateHandler getNameplateHandler() {
+		 return npHandler;
+	}
+	
+}
+>>>>>>> Rollback.
 =======
 package net.aufdemrand.denizen.utilities.packets;
 
