1c9c5aaae6ae0782b304985c231c76bd4882e704
SpaceEmotion <spaceemotion@gmail.com>
2013-01-26 12:44:34 +0100

    Use Depends in MONEY requirement and PlayerTags

--- src/main/java/net/aufdemrand/denizen/scripts/requirements/core/MoneyRequirement.java
+++ src/main/java/net/aufdemrand/denizen/scripts/requirements/core/MoneyRequirement.java
@@ -3,6 +3,7 @@ package net.aufdemrand.denizen.scripts.requirements.core;
 import net.aufdemrand.denizen.exceptions.RequirementCheckException;
 import net.aufdemrand.denizen.scripts.requirements.AbstractRequirement;
 import net.aufdemrand.denizen.scripts.requirements.RequirementsContext;
+import net.aufdemrand.denizen.utilities.Depends;
 import net.aufdemrand.denizen.utilities.arguments.aH;
 import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.aufdemrand.denizen.utilities.debugging.dB.Messages;
@@ -27,16 +28,14 @@ public class MoneyRequirement extends AbstractRequirement{
 			} else throw new RequirementCheckException(Messages.ERROR_UNKNOWN_ARGUMENT, arg);
 		}
 		
-		try {
-		 	RegisteredServiceProvider<Economy> provider = Bukkit.getServicesManager().getRegistration (Economy.class);
-		 	if (provider != null && provider.getProvider() != null) {
-		 		Economy economy = provider.getProvider();
-		 		balance = economy.getBalance(context.getPlayer().getName());
-				dB.echoDebug ("...player balance: " + balance);
-		 	}
-	 	} catch (NoClassDefFoundError e) {
+		if(Depends.economy != null) {
+			Economy economy = Depends.economy;
+			balance = economy.getBalance(context.getPlayer().getName());
+			dB.echoDebug ("...player balance: " + balance);
+		} else {
 			dB.echoError("No economy loaded! Have you installed Vault and a compatible economy plugin?");
-	 	}
+		}
+		
 		if (balance >= quantity) outcome = true;
 		else outcome = false;
 		
