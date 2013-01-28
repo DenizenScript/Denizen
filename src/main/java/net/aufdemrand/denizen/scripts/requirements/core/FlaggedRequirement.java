7c2e7db7a56b9c0a163efedf38a13ae058303097
SpaceEmotion <spaceemotion@gmail.com>
2013-01-28 20:32:15 +0100

    First FLAGGED requirement implementation test

--- src/main/java/net/aufdemrand/denizen/scripts/requirements/core/FlaggedRequirement.java
+++ src/main/java/net/aufdemrand/denizen/scripts/requirements/core/FlaggedRequirement.java
@@ -1,36 +1,76 @@
 package net.aufdemrand.denizen.scripts.requirements.core;
 
+import java.util.List;
+
 import net.aufdemrand.denizen.exceptions.RequirementCheckException;
 import net.aufdemrand.denizen.scripts.requirements.AbstractRequirement;
 import net.aufdemrand.denizen.scripts.requirements.RequirementsContext;
+import net.aufdemrand.denizen.flags.FlagManager;
+import net.aufdemrand.denizen.utilities.DenizenAPI;
 
-import java.util.List;
+import net.citizensnpcs.api.npc.NPC;
+
+import org.bukkit.entity.Player;
 
 public class FlaggedRequirement extends AbstractRequirement {
 
-	// TODO: Finish.
-
-	/* FLAGGED [FLAG_NAME]
-
-	/* Arguments: [] - Required, () - Optional
-	 *  
+	/* FLAGGED [TYPE:FLAG_NAME]
+	 * Example: FLAGGED PLAYER:heHazCookiez
+	 * 
+	 * Arguments: [] - Required, () - Optional
 	 */
     
-    private enum FlagType { GLOBAL, DENIZEN, PLAYER }
-
-	String flagName;
-	FlagType flagType;
+    private enum FlagType { GLOBAL, NPC, PLAYER }
 
     @Override
     public boolean check(RequirementsContext context, List<String> args) throws RequirementCheckException {
 
 	    boolean outcome = false;
+		
+		FlagType type = null;
+		String flag = "";
+		
+		for(String arg: args) {
+			String[] flagList = arg.split(":");
+			
+			if(flagList.length < 2) continue;
+			
+			String typeName = flagList[0].toUpperCase();
+			
+			if(typeName.startsWith("P")) // PLAYER flag
+				type = FlagType.PLAYER;
+			else if(typeName.startsWith("N")) // NPC flag
+				type = FlagType.NPC;
+			else if(typeName.startsWith("G")) // GLOBAL flag
+				type = FlagType.GLOBAL;
+			
+			flag = arg.substring(typeName.length() + 1, arg.length());
+		}
 
+		FlagManager flagMng = DenizenAPI.getCurrentInstance().flagManager();
+		
+		switch(type) {
+			case PLAYER:
+				Player player = context.getPlayer();
+				
+				if(player != null)
+					outcome = flagMng.getPlayerFlag(player.getName(), flag).size() > 0;
+				
+				break;
+				
+			case NPC:
+				NPC npc = context.getNPC();
+				
+				if(npc != null)
+					outcome = flagMng.getNPCFlag(npc.getId(), flag).size() > 0;
+					
+				break;
+				
+			case GLOBAL:
+				outcome = flagMng.getGlobalFlag(flag).size() > 0;
+		}
+		
     	return outcome;
 	}
 
-    @Override
-    public void onEnable() {
-        // TODO Auto-generated method stub
-    }
 }
