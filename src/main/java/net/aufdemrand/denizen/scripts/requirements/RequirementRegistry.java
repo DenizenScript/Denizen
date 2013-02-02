760e3c0f007284687c6213b421109d44db2923e9
Mason Adkins <zappalives0@gmail.com>
2013-02-02 00:23:37 -0500

    more RequirementRegistry cleanup

--- src/main/java/net/aufdemrand/denizen/scripts/requirements/RequirementRegistry.java
+++ src/main/java/net/aufdemrand/denizen/scripts/requirements/RequirementRegistry.java
@@ -60,52 +60,52 @@ public class RequirementRegistry implements DenizenRegistry {
 		String enchantedHint = "(-)enchanted (ITEMINHAND)";
         new EnchantedRequirement().activate().as("ENCHANTED").withOptions(enchantedHint, 1);
 		
-        String flaggedHint = "flagged T.B.D.";
+        String flaggedHint = "(-)flagged T.B.D.";
         new FlaggedRequirement().activate().as("FLAGGED").withOptions(flaggedHint, 0);
 		
-        String inGroupHint = "ingroup [GROUP]";
+        String inGroupHint = "(-)ingroup [GROUP]";
         new InGroupRequirement().activate().as("INGROUP").withOptions(inGroupHint, 1);
 		
-        String permissionHint = "permission [PERMISSION]";
+        String permissionHint = "(-)permission [PERMISSION]";
         new PermissionRequirement().activate().as("PERMISSION").withOptions(permissionHint, 1);
 		
-        String holdingHint = "holding [ITEMNAME]";
+        String holdingHint = "(-)holding [ITEMNAME]";
         new HoldingRequirement().activate().as("HOLDING").withOptions(holdingHint, 1);
 		
-        String liquidHint = "islquid [LOCATION:x,y,z,world]";
+        String liquidHint = "(-)islquid [LOCATION:x,y,z,world]";
         new LiquidRequirement().activate().as("ISLIQUID").withOptions(liquidHint, 1);
 		
-        String moneyHint = "[QTY:#]";
+        String moneyHint = "(-)money [QTY:#]";
         new MoneyRequirement().activate().as("MONEY").withOptions(moneyHint, 1);
 		
-        String ownerHint = "owner (no args)";
+        String ownerHint = "(-)owner (no args)";
         new OwnerRequirement().activate().as("OWNER").withOptions(ownerHint, 0);
 		
-        String opHint = "op (no args)";;
+        String opHint = "(-)op (no args)";;
         new OpRequirement().activate().as("OP").withOptions(opHint, 0);
 		
-        String poweredHint = "ispowered [LOCATION:x,y,z,world]";
+        String poweredHint = "(-)ispowered [LOCATION:x,y,z,world]";
         new PoweredRequirement().activate().as("ISPOWERED").withOptions(poweredHint, 1);
 		
-        String scriptHint = "script T.B.D.";
+        String scriptHint = "(-)script T.B.D.";
         new ScriptRequirement().activate().as("SCRIPT").withOptions(scriptHint, 0);
 		
-        String sneakingHint = "sneaking (no args)";
+        String sneakingHint = "(-)sneaking (no args)";
         new SneakingRequirement().activate().as("SNEAKING").withOptions(sneakingHint, 0);
 		
-        String stormHint = "storm (no args)";
+        String stormHint = "(-)storm (no args)";
         new StormRequirement().activate().as("STORMING").withOptions(stormHint, 0);
 		
-        String sunnyHint = "sunny (no args)";
+        String sunnyHint = "(-)sunny (no args)";
         new SunnyRequirement().activate().as("SUNNY").withOptions(sunnyHint, 0);
 		
-        String timeHint = "time [DAWN, DAY, DUSK, NIGHT]";
+        String timeHint = "(-)time [DAWN, DAY, DUSK, NIGHT]";
 		new TimeRequirement().activate().as("TIME").withOptions(timeHint, 1);
 		
-		String worldGuardRegionHint = "inregion [NAME:regionname]";
+		String worldGuardRegionHint = "(-)inregion [NAME:regionname]";
         new WorldGuardRegionRequirement().activate().as("INREGION").withOptions(worldGuardRegionHint, 1);
 		
-        String procedureHint = "procedure [SCRIPT:procedure_script]";
+        String procedureHint = "(-)procedure [SCRIPT:procedure_script]";
         new ProcedureRequirement().activate().as("PROCEDURE").withOptions(procedureHint, 1);
         
         dB.echoApproval("Loaded core requirements: " + instances.keySet().toString());
