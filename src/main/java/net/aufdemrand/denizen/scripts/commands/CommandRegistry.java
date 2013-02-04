6ea629e7abe52ad284ab67ca6f70983091e15e74
SpaceEmotion <spaceemotion@gmail.com>
2013-02-04 14:42:36 +0100

    More convenient requirement/command loading
    using registerCoreMember() function

--- src/main/java/net/aufdemrand/denizen/scripts/commands/CommandRegistry.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/CommandRegistry.java
@@ -46,129 +46,84 @@ public class CommandRegistry implements DenizenRegistry {
 
 	@Override
 	public void registerCoreMembers() {
-
-        String announceHint = "announce ['announcement text']";
-        new AnnounceCommand().activate().as("ANNOUNCE").withOptions(announceHint, 1);
-
-        String vulnerableHint = "vulnerable (toggle:{true}|false|toggle)";
-        new VulnerableCommand().activate().as("VULNERABLE").withOptions(vulnerableHint, 0);
-        
-        String scribeHint = "scribe [script:book_script] (give|{drop}|equip) (location:x,y,z,world) OR scribe [item:id.name] [script:book_script]";
-        new ScribeCommand().activate().as("SCRIBE").withOptions(scribeHint, 1);
-        
-        String runTaskCommand = "runtask (id:id_name{script_name}) [script:script_name] (instant|queue:queue_type{player_task}) (delay:#{0})";
-        new RuntaskCommand().activate().as("RUNTASK").withOptions(runTaskCommand, 1);
-        
-        String castHint = "cast [potion_effect_type] (duration:#{60s}) (power:#{1}) (target(s):npc|player|npc.#|player.player_name|entity_name)";
-        new CastCommand().activate().as("CAST").withOptions(castHint, 1);
-        
-        String chatHint = "chat ['message to chat'] (npcid:#) (target(s):npc.#|player.player_name{attached player})";
-        new ChatCommand().activate().as("CHAT").withOptions(chatHint, 1);
-        
-        String cooldownHint = "cooldown (duration:#{60s}) (global|{player:player_name}) (script:script_name{)";
-        new CooldownCommand().activate().as("COOLDOWN").withOptions(cooldownHint, 1);
-        
-        String waitHint = "wait (duration:#{5s}) (queue:queue_type) (player:player_name{attached}) (npcid:#{attached})";
-        new WaitCommand().activate().as("WAIT").withOptions(waitHint, 0);
-        
-        String strikeHint = "strike (no_damage) [location:x,y,z,world]";
-        new StrikeCommand().activate().as("STRIKE").withOptions(strikeHint, 1);
-        
-        String disengageHint = "disengage (NPCID:#)";
-        new DisengageCommand().activate().as("DISENGAGE").withOptions(disengageHint, 0);
-        
-        String dropHint = "drop [ITEM:item|EXP] (QTY:#) (LOCATION:location)";
-        new DropCommand().activate().as("DROP").withOptions(dropHint, 1);
-
-        String clearHint = "clear [QUEUE:queue_type] (...)";
-        new ClearCommand().activate().as("CLEAR").withOptions(clearHint, 1);
-
-        String engageHint = "engage (DURATION:#) (NPCID:#)";
-        new EngageCommand().activate().as("ENGAGE").withOptions(engageHint, 0);
-        
-        String executeHint = "execute [AS_PLAYER|AS_SERVER|AS_NPC|AS_OP] ['Bukkit Command']";
-        new ExecuteCommand().activate().as("EXECUTE").withOptions(executeHint, 2);
-
-        String failHint = "fail (PLAYER:player_name)";
-        new FailCommand().activate().as("FAIL").withOptions(failHint, 0);
-
-        String feedHint = "feed (AMT:#) (TARGET:NPC|PLAYER)";
-        new FeedCommand().activate().as("FEED").withOptions(feedHint, 0);
-
-        String finishHint = "finish (PLAYER:player_name)";
-        new FinishCommand().activate().as("FINISH").withOptions(finishHint, 0);
-
-        String flagHint = "flag (DENIZEN|PLAYER|GLOBAL) [[NAME([#])]:[VALUE]|[NAME]:[FLAG_ACTION]:(VALUE)]";
-        new FlagCommand().activate().as("FLAG").withOptions(flagHint, 1);
-
-        String giveHint = "give [MONEY|ITEM:#(:#)|ITEM:MATERIAL_TYPE(:#)] (QTY:#)";
-        new GiveCommand().activate().as("GIVE").withOptions(giveHint, 1);
-
-        String healHint = "heal (AMT:#) (TARGET:NPC|PLAYER)";
-        new HealCommand().activate().as("HEAL").withOptions(healHint, 0);
-
-        String ifHint = "if (!)[COMPARABLE] (OPERATOR) (COMPARED_TO) (BRIDGE) (...) [COMMAND] (ELSE) (COMMAND) // see documentation.";
-        new IfCommand().activate().as("IF").withOptions(ifHint, 2);
-
-        String listenHint = "listen [Listener_Type] [ID:ListenerID] [Listener Arguments] // see documentation.";
-        new ListenCommand().activate().as("LISTEN").withOptions(listenHint, 2);
-        
-        String lookHint = "look [LOCATION:x,y,z,world]";
-        new LookCommand().activate().as("LOOK").withOptions(lookHint, 1);
-        
-        String lookcloseHint = "lookclose [TOGGLE:TRUE|FALSE] (RANGE:#.#) (REALISTIC)";
-        new LookcloseCommand().activate().as("LOOKCLOSE").withOptions(lookcloseHint, 1);
-
-        String newHint = "new ITEMSTACK [ITEM:item] (QTY:qty)";
-        new NewCommand().activate().as("NEW").withOptions(newHint, 2);
-
-        String modifyBlockHint = "modifyblock [LOCATION:x,y,z,world] [MATERIAL:DATA VALUE] (RADIUS:##) (HEIGHT:##) (DEPTH:##)";
-        new ModifyBlockCommand().activate().as("MODIFYBLOCK").withOptions(modifyBlockHint, 2);
-
-        String narrateHint = "narrate (PLAYER:player_name) ['Text to narrate']";
-        new NarrateCommand().activate().as("NARRATE").withOptions(narrateHint, 1);
-
-        String playSoundHint = "playsound [LOCATION:x,y,z,world] [SOUND:NAME] (VOLUME:#) (PITCH:#)";
-        new PlaySoundCommand().activate().as("PLAYSOUND").withOptions(playSoundHint, 2);
-
-        String randomHint = "random [#]";
-        new RandomCommand().activate ().as("RANDOM").withOptions(randomHint, 1);
-
-        String shootHint = "shoot [ENTITY:entity] (RIDE) (BURN) (EXPLODE) (FIREWORKS) (LOCATION:location)";
-        new ShootCommand().activate().as("SHOOT").withOptions(shootHint, 1);
-
-        String switchHint = "switch [LOCATION:x,y,z,world] (STATE:ON|OFF|TOGGLE) (DURATION:#)";
-        new SwitchCommand().activate().as("SWITCH").withOptions(switchHint, 1);
-
-        String takeHint = "take [MONEY|ITEMINHAND|ITEM:#(:#)|ITEM:MATERIAL_TYPE(:#)] (QTY:#)";
-        new TakeCommand().activate ().as("TAKE").withOptions(takeHint, 1);
-
-        String teleportHint = "teleport (NPC) [LOCATION:x,y,z,world] (TARGETS:[NPC.#]|[PLAYER.PlayerName])";
-        new TeleportCommand().activate().as("TELEPORT").withOptions(teleportHint, 1);
-
-        String triggerHint = "trigger [NAME:Trigger_Name] [(TOGGLE:TRUE|FALSE)|(COOLDOWN:#.#)|(RADIUS:#)]";
-        new TriggerCommand().activate().as("TRIGGER").withOptions(triggerHint, 2);
-
-        String zapHint = "zap [#|STEP:step_name] (SCRIPT:script_name{current_script}) (DURATION:#{0})";
-        new ZapCommand().activate().as("ZAP").withOptions(zapHint, 0);
-
-        String followHint = "follow (STOP)";
-        new FollowCommand().activate().as("FOLLOW").withOptions(followHint, 0);
-
-        String attackHint = "attack (STOP)";
-        new AttackCommand().activate().as("ATTACK").withOptions(attackHint, 0);
-
-        String assignmentHint = "assignment [{SET}|REMOVE] (SCRIPT:assignment_script)";
-        new AssignmentCommand().activate().as("ASSIGNMENT").withOptions(assignmentHint, 1);
-
-        String determineHint = "determine [TRUE|FALSE]";
-        new DetermineCommand().activate().as("DETERMINE").withOptions(determineHint, 1);
+        registerCoreMember(AnnounceCommand.class, "ANNOUNCE", "announce ['announcement text']", 1);
+        registerCoreMember(AttackCommand.class, "ATTACK", "attack (STOP)", 0);
+        registerCoreMember(AssignmentCommand.class, "ASSIGNMENT", "assignment [{SET}|REMOVE] (SCRIPT:assignment_script)", 1);
 		
-		String nameplateHint = "nameplate [COLOR] (PLAYER)";
-		new NameplateCommand().activate().as("NAMEPLATE").withOptions(nameplateHint, 1);
+        registerCoreMember(CastCommand.class, "CAST",
+            "cast [potion_effect_type] (duration:#{60s}) (power:#{1}) (target(s):npc|player|npc.#|player.player_name|entity_name)", 1);
+        registerCoreMember(ChatCommand.class, "CHAT",
+            "chat ['message to chat'] (npcid:#) (target(s):npc.#|player.player_name{attached player})", 1);
+        registerCoreMember(ClearCommand.class, "CLEAR", "clear [QUEUE:queue_type] (...)", 1);
+        registerCoreMember(CooldownCommand.class, "COOLDOWN",
+            "cooldown (duration:#{60s}) (global|{player:player_name}) (script:script_name{)", 1);
+		
+        registerCoreMember(DetermineCommand.class, "DETERMINE", "determine [TRUE|FALSE]", 1);
+        registerCoreMember(DisengageCommand.class, "DISENGAGE", "disengage (NPCID:#)", 0);
+        registerCoreMember(DropCommand.class, "DROP", "drop [ITEM:item|EXP] (QTY:#) (LOCATION:location)", 1);
+		
+        registerCoreMember(EngageCommand.class, "ENGAGE", "engage (DURATION:#) (NPCID:#)", 0);
+        registerCoreMember(ExecuteCommand.class, "EXECUTE", "execute [AS_PLAYER|AS_SERVER|AS_NPC|AS_OP] ['Bukkit Command']", 2);
+		
+        registerCoreMember(FailCommand.class, "FAIL", "fail (PLAYER:player_name)", 0);
+        registerCoreMember(FeedCommand.class, "FEED", "feed (AMT:#) (TARGET:NPC|PLAYER)", 0);
+        registerCoreMember(FinishCommand.class, "FINISH", "finish (PLAYER:player_name)", 0);
+        registerCoreMember(FlagCommand.class, "FLAG", "flag (DENIZEN|PLAYER|GLOBAL) [[NAME([#])]:[VALUE]|[NAME]:[FLAG_ACTION]:(VALUE)]", 1);
+        registerCoreMember(FollowCommand.class, "FOLLOW", "follow (STOP)", 0);
+		
+        registerCoreMember(GiveCommand.class, "GIVE", "give [MONEY|ITEM:#(:#)|ITEM:MATERIAL_TYPE(:#)] (QTY:#)", 1);
+		
+        registerCoreMember(HealCommand.class, "HEAL", "heal (AMT:#) (TARGET:NPC|PLAYER)", 0);
+		
+        registerCoreMember(IfCommand.class, "IF",
+            "if (!)[COMPARABLE] (OPERATOR) (COMPARED_TO) (BRIDGE) (...) [COMMAND] (ELSE) (COMMAND) // see documentation.", 2);
+		
+        registerCoreMember(ListenCommand.class, "LISTEN",
+            "listen [Listener_Type] [ID:ListenerID] [Listener Arguments] // see documentation.", 2);
+        registerCoreMember(LookCommand.class, "LOOK", "look [LOCATION:x,y,z,world]", 1);
+        registerCoreMember(LookcloseCommand.class, "LOOKCLOSE", "lookclose [TOGGLE:TRUE|FALSE] (RANGE:#.#) (REALISTIC)", 1);
+		
+        registerCoreMember(ModifyBlockCommand.class, "MODIFYBLOCK",
+            "modifyblock [LOCATION:x,y,z,world] [MATERIAL:DATA VALUE] (RADIUS:##) (HEIGHT:##) (DEPTH:##)", 2);
+		
+        registerCoreMember(NameplateCommand.class, "NAMEPLATE", "nameplate [COLOR] (PLAYER)", 1);
+        registerCoreMember(NarrateCommand.class, "NARRATE", "narrate (PLAYER:player_name) ['Text to narrate']", 1);
+        registerCoreMember(NewCommand.class, "NEW", "new ITEMSTACK [ITEM:item] (QTY:qty)", 2);
+		
+        registerCoreMember(PlaySoundCommand.class, "PLAYSOUND", "playsound [LOCATION:x,y,z,world] [SOUND:NAME] (VOLUME:#) (PITCH:#)", 2);
+		
+        registerCoreMember(RandomCommand.class, "RANDOM", "random [#]", 1);
+        registerCoreMember(RuntaskCommand.class, "RUNTASK",
+            "runtask (id:id_name{script_name}) [script:script_name] (instant|queue:queue_type{player_task}) (delay:#{0})", 1);
+		
+        registerCoreMember(ScribeCommand.class, "SCRIBE",
+            "scribe [script:book_script] (give|{drop}|equip) (location:x,y,z,world) OR scribe [item:id.name] [script:book_script]", 1);
+        registerCoreMember(ShootCommand.class, "SHOOT", "shoot [ENTITY:entity] (RIDE) (BURN) (EXPLODE) (FIREWORKS) (LOCATION:location)", 1);
+        registerCoreMember(StrikeCommand.class, "STRIKE", "strike (no_damage) [location:x,y,z,world]", 1);
+        registerCoreMember(SwitchCommand.class, "SWITCH", "switch [LOCATION:x,y,z,world] (STATE:ON|OFF|TOGGLE) (DURATION:#)", 1);
+		
+        registerCoreMember(TakeCommand.class, "TAKE", "take [MONEY|ITEMINHAND|ITEM:#(:#)|ITEM:MATERIAL_TYPE(:#)] (QTY:#)", 1);
+        registerCoreMember(TeleportCommand.class, "TELEPORT", "teleport (NPC) [LOCATION:x,y,z,world] (TARGETS:[NPC.#]|[PLAYER.PlayerName])", 1);
+        registerCoreMember(TriggerCommand.class, "TRIGGER", "trigger [NAME:Trigger_Name] [(TOGGLE:TRUE|FALSE)|(COOLDOWN:#.#)|(RADIUS:#)]", 2);
+		
+        registerCoreMember(VulnerableCommand.class, "VULNERABLE", "vulnerable (toggle:{true}|false|toggle)", 0);
+		
+        registerCoreMember(WaitCommand.class, "WAIT",
+            "wait (duration:#{5s}) (queue:queue_type) (player:player_name{attached}) (npcid:#{attached})", 0);
+
+        registerCoreMember(ZapCommand.class, "ZAP", "zap [#|STEP:step_name] (SCRIPT:script_name{current_script}) (DURATION:#{0})", 0);
 
         dB.echoApproval("Loaded core commands: " + instances.keySet().toString());
 	}
+	
+    private <T extends AbstractCommand> void registerCoreMember(Class<T> cmd, String name, String hint, int args) {
+        try {
+            cmd.newInstance().activate().as(name).withOptions(hint, args);
+        } catch(Exception e) {
+            dB.echoError("Could not register command " + name + ": " + e.getMessage());
+            if (dB.showStackTraces) e.printStackTrace();
+        }
+    }
 
 	@Override
 	public void disableCoreMembers() {
--- src/main/java/net/aufdemrand/denizen/scripts/requirements/RequirementRegistry.java
+++ src/main/java/net/aufdemrand/denizen/scripts/requirements/RequirementRegistry.java
@@ -57,61 +57,35 @@ public class RequirementRegistry implements DenizenRegistry {
 
 	@Override
     public void registerCoreMembers() {
-		String enchantedHint = "(-)enchanted (ITEMINHAND)";
-        new EnchantedRequirement().activate().as("ENCHANTED").withOptions(enchantedHint, 1);
-		
-        String flaggedHint = "(-)flagged T.B.D.";
-        new FlaggedRequirement().activate().as("FLAGGED").withOptions(flaggedHint, 0);
-		
-        String inGroupHint = "(-)ingroup [GROUP]";
-        new InGroupRequirement().activate().as("INGROUP").withOptions(inGroupHint, 1);
-		
-        String permissionHint = "(-)permission [PERMISSION]";
-        new PermissionRequirement().activate().as("PERMISSION").withOptions(permissionHint, 1);
-		
-        String holdingHint = "(-)holding [ITEMNAME]";
-        new HoldingRequirement().activate().as("HOLDING").withOptions(holdingHint, 1);
-		
-        String liquidHint = "(-)islquid [LOCATION:x,y,z,world]";
-        new LiquidRequirement().activate().as("ISLIQUID").withOptions(liquidHint, 1);
-		
-        String itemHint = "(-)item [ITEM] (QTY:#)";
-        new ItemRequirement().activate().as("ITEM").withOptions(itemHint, 1);
-        
-        String moneyHint = "(-)money [QTY:#]";
-        new MoneyRequirement().activate().as("MONEY").withOptions(moneyHint, 1);
-		
-        String ownerHint = "(-)owner (no args)";
-        new OwnerRequirement().activate().as("OWNER").withOptions(ownerHint, 0);
-		
-        String opHint = "(-)op (no args)";;
-        new OpRequirement().activate().as("OP").withOptions(opHint, 0);
-		
-        String poweredHint = "(-)ispowered [LOCATION:x,y,z,world]";
-        new PoweredRequirement().activate().as("ISPOWERED").withOptions(poweredHint, 1);
-		
-        String scriptHint = "(-)script T.B.D.";
-        new ScriptRequirement().activate().as("SCRIPT").withOptions(scriptHint, 0);
-		
-        String sneakingHint = "(-)sneaking (no args)";
-        new SneakingRequirement().activate().as("SNEAKING").withOptions(sneakingHint, 0);
-		
-        String stormHint = "(-)storm (no args)";
-        new StormRequirement().activate().as("STORMING").withOptions(stormHint, 0);
-		
-        String sunnyHint = "(-)sunny (no args)";
-        new SunnyRequirement().activate().as("SUNNY").withOptions(sunnyHint, 0);
-		
-        String timeHint = "(-)time [DAWN, DAY, DUSK, NIGHT]";
-		new TimeRequirement().activate().as("TIME").withOptions(timeHint, 1);
-		
-		String worldGuardRegionHint = "(-)inregion [NAME:regionname]";
-        new WorldGuardRegionRequirement().activate().as("INREGION").withOptions(worldGuardRegionHint, 1);
-		
-        String procedureHint = "(-)procedure [SCRIPT:procedure_script]";
-        new ProcedureRequirement().activate().as("PROCEDURE").withOptions(procedureHint, 1);
-        
+        registerCoreMember(EnchantedRequirement.class, "ENCHANTED", "enchanted (ITEMINHAND)", 1);
+        registerCoreMember(FlaggedRequirement.class, "FLAGGED", "flagged T.B.D.", 0);
+        registerCoreMember(HoldingRequirement.class, "HOLDING", "holding [ITEMNAME]", 1);
+        registerCoreMember(InGroupRequirement.class, "INGROUP", "ingroup [GROUP]", 1);
+        registerCoreMember(ItemRequirement.class, "ITEM", "item [ITEM] (QTY:#)", 1);
+        registerCoreMember(LiquidRequirement.class, "ISLIQUID", "isliquid [LOCATION:x,y,z,world]", 1);
+        registerCoreMember(MoneyRequirement.class, "MONEY", "money [QTY:#]", 1);
+        registerCoreMember(OpRequirement.class, "OP", "op (no args)", 0);
+        registerCoreMember(OwnerRequirement.class, "OWNER", "owner (no args)", 0);
+        registerCoreMember(PermissionRequirement.class, "PERMISSION", "permission [PERMISSION]", 1);
+        registerCoreMember(PoweredRequirement.class, "ISPOWERED", "ispowered [LOCATION:x,y,z,world]", 1);
+        registerCoreMember(ProcedureRequirement.class, "PROCEDURE", "procedure [SCRIPT:procedure_script]", 1);
+        registerCoreMember(ScriptRequirement.class, "SCRIPT", "script T.B.D.", 0);
+        registerCoreMember(SneakingRequirement.class, "SNEAKING", "sneaking (no args)", 0);
+        registerCoreMember(StormRequirement.class, "STORMING", "storm (no args)", 0);
+        registerCoreMember(SunnyRequirement.class, "SUNNY", "sunny (no args)", 0);
+        registerCoreMember(TimeRequirement.class, "TIME", "time [DAWN, DAY, DUSK, NIGHT]", 1);
+        registerCoreMember(WorldGuardRegionRequirement.class, "INREGION", "inregion [NAME:regionname]", 1);
+
         dB.echoApproval("Loaded core requirements: " + instances.keySet().toString());
+    }
+	
+    private <T extends AbstractRequirement> void registerCoreMember(Class<T> requirement, String name, String hint, int args) {
+        try {
+            requirement.newInstance().activate().as(name).withOptions("(-)" + hint, args);
+        } catch(Exception e) {
+            dB.echoError("Could not register requirement " + name + ": " + e.getMessage());
+            if (dB.showStackTraces) e.printStackTrace();
+        }
     }
 
 }
