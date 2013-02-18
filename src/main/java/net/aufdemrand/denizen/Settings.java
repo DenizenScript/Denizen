f65a4241a7608f5b624ad32034ba0b995a8e2d9a
David Cernat <davidcernat@gmail.com>
2013-02-18 19:56:16 +0200

    Updated Settings and config.

--- src/main/java/net/aufdemrand/denizen/Settings.java
+++ src/main/java/net/aufdemrand/denizen/Settings.java
@@ -8,21 +8,15 @@ public class Settings {
 	# Default time in seconds of cooldown set to Denizens when a trigger is triggered.
 	# Not all triggers may use this, it is optional!
 	#
-    # Format: default_triggername_cooldown_in_seconds: #.#
-    # For example: default_chat_cooldown_in_seconds: 2.0
-    
-    default_click_cooldown_in_seconds: 2.0
-    default_location_cooldown_in_seconds: 15.0
-    default_proximity_cooldown_in_seconds: 1.0
-    default_damage_cooldown_in_seconds: 0.5
-    default_chat_cooldown_in_seconds: 2.0
-	 
+    # Format: [Name] Trigger Default Cooldown in Seconds: #.#
+    # For example: Click Trigger Default Cooldown in Seconds: 2.0
+     
 	 */
 	
 	public static long DefaultCooldown(String triggerName) {
 		return (long) (DenizenAPI.getCurrentInstance().getConfig()
-                .getDouble("default_" + triggerName.toLowerCase().replace(" ", "_")
-                        + "_cooldown_in_seconds", 5) * 1000);
+                .getDouble(triggerName
+                        + " Trigger Default Cooldown in Seconds", 5) * 1000);
 	}
 	
 	/* 
@@ -34,13 +28,13 @@ public class Settings {
 	# Select true if you would like invulnerable NPCs to use both right and left clicks
 	# to activate the click trigger.
 
-	disabled_damage_trigger_instead_triggers_click: false
+	Click Trigger Allows Left Click: false
 	
 	 */
 	
-	public static boolean DisabledDamageTriggerInsteadTriggersClick() {
+	public static boolean ClickTriggerAllowsLeftClick() {
 		return DenizenAPI.getCurrentInstance().getConfig()
-                .getBoolean("disabled_damage_trigger_instead_triggers_click", false);
+                .getBoolean("Click Trigger Allows Left Click", false);
 	}
 	 
 	/*
@@ -49,13 +43,13 @@ public class Settings {
 	# execution, which is where commands triggered by scripts are stored.
 	# Recommend 5-15.
 
-	interact_delay_in_ticks: 7
+	Interact Delay in Ticks: 7
 
 	 */
 
 	public static int InteractDelayInTicks() {
 		int delay = DenizenAPI.getCurrentInstance().getConfig()
-                .getInt("interact_delay_in_ticks", 10);
+                .getInt("Interact Delay in Ticks", 10);
 		// Check for users setting delay to 0, which will in turn lock up the server
 		if (delay < 1) delay = 1;
 		return delay;
@@ -66,13 +60,13 @@ public class Settings {
 	# Default engage timeout. When NPCs are set to ENGAGE, this is the default timeout
     # that they will auto-DISENGAGE if not otherwise specified. (Default, 150 seconds)
 
-	engage_timeout_in_seconds: 150
+	Engage Timeout in Seconds: 150
 	  
 	 */
 	
 	public static int EngageTimeoutInSeconds() {
 		return DenizenAPI.getCurrentInstance().getConfig()
-                .getInt("engage_timeout_in_seconds", 150);
+                .getInt("Engage Timeout in Seconds", 150);
 	}
 
 	/*
@@ -81,18 +75,19 @@ public class Settings {
 	# interact-script triggers. Not all triggers use a range,
 	# as it may not be applicable to the trigger.
 	#
-    # Format: triggername_trigger_range_in_blocks: #.#
-    # For example: location_trigger_range_in_blocks: 1.0
+    # Format: [Name] Trigger Default Range in Blocks: #.#
+    # For example: Location Trigger Default Range in Blocks: 1.0
   
-	proximity_trigger_range_in_blocks: 10
-	location_trigger_range_in_blocks: 2
-	chat_trigger_range_in_blocks: 2
+  	Chat Trigger Default Range in Blocks: 3
+	Proximity Trigger Default Range in Blocks: 10
+	Location Trigger Default Range in Blocks: 2
+	
 
 	 */
 
 	public static int TriggerRangeInBlocks(String triggerName) {
 		return DenizenAPI.getCurrentInstance().getConfig()
-                .getInt(triggerName.toLowerCase().replace(" ", "_") + "_trigger_range_in_blocks", -1);
+                .getInt(triggerName + " Trigger Default Range in Blocks", -1);
 	}
 
 
@@ -102,57 +97,50 @@ public class Settings {
     # to speak locally. Following is the format of the chatting
     # that will occur.
 
-    player_chat_no_target: <player.name> says '<chat.message>'
-    player_chat_to_target: <player.name> says to <chat.target>, '<chat.message>'
-    player_chat_to_targets: <player.name> says to <chat.targets>, '<chat.message>'
+    Chat No Target: <player.name> says '<chat.message>'
+    Chat to Target: <player.name> says to <chat.target>, '<chat.message>'
+    Chat to Targets: <player.name> says to <chat.targets>, '<chat.message>'
 
      */
 
-
-
-    public static boolean CheckLineOfSightWhenChatting() {
+    public static String ChatToNpcFormat() {
         return DenizenAPI.getCurrentInstance().getConfig()
-                .getBoolean("check_line_of_sight_when_chatting", true);
+                .getString("Chat to NPC Format", "You -> <npc.name.nickname>: <text>");
+    }
+
+    public static String ChatToNpcBystandersFormat() {
+        return DenizenAPI.getCurrentInstance().getConfig()
+                .getString("Chat to NPC Bystanders Format", "<player.name> -> <npc.name.nickname>: <text>");
+    }
+
+    public static int ChatToNpcBystandersRange() {
+        return DenizenAPI.getCurrentInstance().getConfig()
+                .getInt("Chat to NPC Bystanders Range", 4);
+    }
+
+    public static boolean ChatOnlyWhenHavingLineOfSightToNPC() {
+        return DenizenAPI.getCurrentInstance().getConfig()
+                .getBoolean("Chat Only When Having Line of Sight to NPC", true);
     }
 
     public static boolean ChatOnlyWhenLookingAtNPC() {
         return DenizenAPI.getCurrentInstance().getConfig()
-                .getBoolean("chat_with_npc_only_when_looking_at_npc", true);
+                .getBoolean("Chat Only When Looking at NPC", true);
     }
 
     public static boolean ChatGloballyIfFailedChatTriggers() {
         return DenizenAPI.getCurrentInstance().getConfig()
-                .getBoolean("chat_globally_if_failed_chat_triggers", false);
+                .getBoolean("Chat Globally If Failed Chat Triggers", true);
     }
 
     public static boolean ChatGloballyIfNoChatTriggers() {
         return DenizenAPI.getCurrentInstance().getConfig()
-                .getBoolean("chat_globally_if_no_chat_triggers", false);
+                .getBoolean("Chat Globally If No Chat Triggers", true);
     }
 
     public static boolean ChatGloballyIfNotInteractable() {
         return DenizenAPI.getCurrentInstance().getConfig()
-                .getBoolean("chat_globally_if_not_interactable", false);
-    }
-
-    public static String PlayerChatToNpcFormat() {
-        return DenizenAPI.getCurrentInstance().getConfig()
-                .getString("player_chat_to_npc_format", "You -> <npc.name.nickname>: <text>");
-    }
-    
-    public static int PlayerChatToNpcRange() {
-        return DenizenAPI.getCurrentInstance().getConfig()
-                .getInt("player_chat_to_npc_range", 3);
-    }
-
-    public static String PlayerChatToNpcToBystandersFormat() {
-        return DenizenAPI.getCurrentInstance().getConfig()
-                .getString("player_chat_to_npc_to_bystanders_format", "<player.name> -> <npc.name.nickname>: <text>");
-    }
-
-    public static int PlayerChatToNpcBystandersRange() {
-        return DenizenAPI.getCurrentInstance().getConfig()
-                .getInt("player_chat_to_npc_to_bystanders_range", 4);
+                .getBoolean("Chat Globally If Not Interactable", true);
     }
 
 }
