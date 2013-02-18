b0ce83454da1b88acc42571906dd74a030a3d1e2
Jeremy Schroeder <jeremy@ssdbg.com>
2013-02-18 09:26:57 -0500

    Add ProcedureScriptTag for running procedure scripts.

--- src/main/java/net/aufdemrand/denizen/tags/core/ProcedureScriptTag.java
+++ src/main/java/net/aufdemrand/denizen/tags/core/ProcedureScriptTag.java
@@ -2,10 +2,16 @@ package net.aufdemrand.denizen.tags.core;
 
 import net.aufdemrand.denizen.Denizen;
 import net.aufdemrand.denizen.events.ReplaceableTagEvent;
+import net.aufdemrand.denizen.scripts.ScriptBuilder;
+import net.aufdemrand.denizen.scripts.ScriptEntry;
+import net.aufdemrand.denizen.scripts.ScriptQueue;
+import net.aufdemrand.denizen.scripts.commands.core.DetermineCommand;
+import net.aufdemrand.denizen.utilities.arguments.Script;
+import net.aufdemrand.denizen.utilities.arguments.aH;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
-import java.util.UUID;
+import java.util.List;
 
 public class ProcedureScriptTag implements Listener {
 
@@ -15,12 +21,30 @@ public class ProcedureScriptTag implements Listener {
 
    @EventHandler
     public void utilTags(ReplaceableTagEvent event) {
-    if (!event.matches("UTIL")) return;
-
+    if (!event.matches("PROC")) return;
        if (event.getType() == null) return;
 
-       if (event.getType().equalsIgnoreCase("RANDOM_UUID"))
-           event.setReplaced(UUID.randomUUID().toString());
+       // Get the script from the type argument
+       Script script = aH.getScriptFrom(event.getType());
+
+       // Build script entries
+       List<ScriptEntry> entries = script.getContainer().getBaseEntries(event.getPlayer(), event.getNPC());
+
+       // Return if no entries built
+       if (entries.isEmpty()) return;
+
+       // Create new ID -- this is what we will look for when determining an outcome
+       long id = DetermineCommand.getNewId();
+
+       // Add the reqId to each of the entries
+       ScriptBuilder.addObjectToEntries(entries, "ReqId", id);
+
+       ScriptQueue._getInstantQueue(ScriptQueue._getNextId()).addEntries(entries).start();
+
+       if (DetermineCommand.outcomes.containsKey(id)) {
+           event.setReplaced(DetermineCommand.outcomes.get(id));
+       }
+
 
    }
     
