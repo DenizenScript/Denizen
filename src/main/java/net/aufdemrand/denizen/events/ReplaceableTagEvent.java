c73263011b71f98790d4f67c5319b42e2edb3155
David Cernat <davidcernat@gmail.com>
2013-02-19 00:39:05 +0200

    For real this time.

--- src/main/java/net/aufdemrand/denizen/events/ReplaceableTagEvent.java
+++ src/main/java/net/aufdemrand/denizen/events/ReplaceableTagEvent.java
@@ -80,7 +80,9 @@ public class ReplaceableTagEvent extends Event {
     Pattern bracketRegex = Pattern.compile("\\[.*?\\]");
     
     // Value pattern that matches everything after the last : found
-    Pattern valueRegex = Pattern.compile(":.*?$");
+    // that isn't followed by ] without being followed by [ first,
+    // and is therefore not between brackets
+    Pattern valueRegex = Pattern.compile(":[^\\]]+(\\[([^\\[])+)?$");
 
     // Component pattern that matches groups of characters that are not
     // [] or . and that optionally contain [] and a . at the end
