a98eee84e47b80a7a0ade1c16758d988cdbf63f8
David Cernat <davidcernat@gmail.com>
2013-02-20 20:07:31 +0200

    Added <util.random.int[].to[]> tag.

--- src/main/java/net/aufdemrand/denizen/tags/core/UtilTags.java
+++ src/main/java/net/aufdemrand/denizen/tags/core/UtilTags.java
@@ -2,9 +2,12 @@ package net.aufdemrand.denizen.tags.core;
 
 import net.aufdemrand.denizen.Denizen;
 import net.aufdemrand.denizen.events.ReplaceableTagEvent;
+import net.aufdemrand.denizen.utilities.arguments.aH;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class UtilTags implements Listener {
@@ -16,11 +19,37 @@ public class UtilTags implements Listener {
    @EventHandler
     public void utilTags(ReplaceableTagEvent event) {
     if (!event.matches("UTIL")) return;
+    
+    String type = event.getType() != null ? event.getType() : "";
+    String subType = event.getSubType() != null ? event.getSubType() : "";
+    String subTypeContext = event.getSubTypeContext() != null ? event.getSubTypeContext().toUpperCase() : "";
+    String specifier = event.getSpecifier() != null ? event.getSpecifier() : "";
+    String specifierContext = event.getSpecifierContext() != null ? event.getSpecifierContext().toUpperCase() : "";
 
-       if (event.getType() == null) return;
-
-       if (event.getType().equalsIgnoreCase("RANDOM_UUID"))
-           event.setReplaced(UUID.randomUUID().toString());
+       if (type.equalsIgnoreCase("RANDOM"))
+    	   if (subType.equalsIgnoreCase("INT"))
+    	   {
+    		   if (specifier.equalsIgnoreCase("TO"))
+    		   {
+    			   if (aH.matchesInteger(subTypeContext) && aH.matchesInteger(specifierContext))
+    			   {
+    				   int min = aH.getIntegerFrom(subTypeContext);
+    				   int max = aH.getIntegerFrom(specifierContext);
+    				   
+    				   if (min > max) // in case the first number is larger than the second, reverse them
+    				   {
+    					   int store = max;
+    					   min = max;
+    					   max = store;
+    				   }
+    				   
+    				   Random rand = new Random();
+    				   event.setReplaced(String.valueOf(rand.nextInt(max - min + 1) + min));
+    			   }
+    		   }
+    	   }
+    	   else if (subType.equalsIgnoreCase("UUID"))
+    		   event.setReplaced(UUID.randomUUID().toString());
 
    }
     
