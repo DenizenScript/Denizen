a7fb9452d60af743baf9c3edcad1dba33ea4c896
SpaceEmotion <spaceemotion@gmail.com>
2013-03-07 10:31:40 +0100

    Fix formats not working correctly

--- src/main/java/net/aufdemrand/denizen/scripts/containers/core/FormatScriptContainer.java
+++ src/main/java/net/aufdemrand/denizen/scripts/containers/core/FormatScriptContainer.java
@@ -1,31 +1,27 @@
 package net.aufdemrand.denizen.scripts.containers.core;
 
-import net.aufdemrand.denizen.npc.dNPC;
 import net.aufdemrand.denizen.scripts.ScriptEntry;
 import net.aufdemrand.denizen.scripts.containers.ScriptContainer;
 import net.aufdemrand.denizen.tags.TagManager;
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
 
 public class FormatScriptContainer extends ScriptContainer {
-	
+
     public FormatScriptContainer(ConfigurationSection configurationSection, String scriptContainerName) {
         super(configurationSection, scriptContainerName);
     }
-    
-    public String getFormat(Player player, dNPC npc) {
-    	String format = getContents().getString("FORMAT", "<text>");
-    	format = TagManager.tag(player, npc, format, false);
-        return format;
+
+    public String getFormat() {
+        return getString("FORMAT", "<text>");
     }
-    
+
     public void setFormat(String format) {
-        getContents().set("FORMAT", format);
+        set("FORMAT", format);
     }
-    
+
     public String getFormattedText(ScriptEntry entry) {
-        String text = (String) entry.getObject("text");
-        return getFormat(entry.getPlayer(), entry.getNPC()).replace("<text>", text);
+        String text = getFormat().replaceAll("<text>", (String) entry.getObject("text"));
+        return TagManager.tag(entry.getPlayer(), entry.getNPC(), text);
     }
     
 }
