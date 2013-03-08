779dc78aebc4f33ebd9c8ce2606716d41a0d9edd
GnomeffinWay <baseboss889@yahoo.com>
2013-03-07 23:44:37 -0600

    Fishing improvements

--- src/main/java/net/aufdemrand/denizen/npc/traits/FishingTrait.java
+++ src/main/java/net/aufdemrand/denizen/npc/traits/FishingTrait.java
@@ -1,6 +1,7 @@
 package net.aufdemrand.denizen.npc.traits;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import net.aufdemrand.denizen.utilities.DenizenAPI;
 import net.aufdemrand.denizen.utilities.debugging.dB;
@@ -9,19 +10,17 @@ import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.util.PlayerAnimation;
 import net.minecraft.server.v1_4_R1.EntityFishingHook;
 import net.minecraft.server.v1_4_R1.EntityHuman;
+import net.minecraft.server.v1_4_R1.EntityItem;
+import net.minecraft.server.v1_4_R1.Item;
+import net.minecraft.server.v1_4_R1.ItemStack;
+import net.minecraft.server.v1_4_R1.MathHelper;
 import net.minecraft.server.v1_4_R1.WorldServer;
 
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_4_R1.CraftWorld;
 import org.bukkit.craftbukkit.v1_4_R1.entity.CraftPlayer;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerPickupItemEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 public class FishingTrait extends Trait {
@@ -29,7 +28,7 @@ public class FishingTrait extends Trait {
 	@Persist("fishing")
     private boolean fishing = false;
 	
-	@Persist("fishing hole")
+	@Persist("fishing spot")
 	private Location fishingLocation = null;
 	
 	ArrayList<Location> available = new ArrayList<Location>();
@@ -37,7 +36,7 @@ public class FishingTrait extends Trait {
 	WorldServer nmsworld = null;
 	Location fishingSpot = null;
 	EntityFishingHook fishHook = null;
-	Entity fish = null;
+	EntityItem fish = null;
 	int reelCount = 100;
 	int castCount = 0;
 	
@@ -46,16 +45,20 @@ public class FishingTrait extends Trait {
 	    reelCount++;
 	    castCount++;
 	    
-	    if(reelCount == 500) {
+	    if(fish != null) 
+	        if(fish.getBukkitEntity().getLocation().distance(npc.getBukkitEntity().getLocation())<3) {
+	            try{
+	                fish.getBukkitEntity().remove();
+	            } catch(Exception e) { }
+	        }
+	        
+	    if(reelCount == 400) {
 	        reel();
 	        reelCount = 0;
-	        castCount = 425;
+	        castCount = 325;
 	    }
 	    
-	    if(castCount == 500) {
-	        try{
-	            fish.remove();
-	        } catch(Exception e) { }
+	    if(castCount == 400) {
 	        cast();
 	        castCount = 0;
 	    }
@@ -65,9 +68,36 @@ public class FishingTrait extends Trait {
 	public void onSpawn() {
 		eh = ((CraftPlayer) npc.getBukkitEntity()).getHandle();
 		nmsworld = ((CraftWorld) npc.getBukkitEntity().getWorld()).getHandle();
-        fishHook = new EntityFishingHook(nmsworld, eh);
 	}
-	
+	   
+    /**
+     * Makes the NPC fish at the specified location
+     * 
+     * TODO Reimplement variance, so each cast doesn't land in the exact same spot.
+     * 
+     * @param location
+     */
+    public void startFishing(Location location) {
+        DenizenAPI.getDenizenNPC(npc).action("start fishing", null);
+        fishingLocation = location;
+        cast();
+        dB.log("... " + npc.getName() + " should be fishing!");
+        fishing = true;
+    }
+    
+    /**
+     * Makes the stop fishing.
+     */
+    public void stopFishing() {
+        DenizenAPI.getDenizenNPC(npc).action("stop fishing", null);
+
+        dB.log("...removing fish hook.");
+        reel();
+        
+        fishingLocation = null;
+        fishing = false;
+    }
+    
 	/**
 	 * Makes the NPC fish in the nearest water
 	 * 
@@ -76,112 +106,79 @@ public class FishingTrait extends Trait {
 	public void startFishing() {
 		fishing = true;
 		fishingLocation = npc.getBukkitEntity().getLocation();
-		//fishingSpot = fishingLocation.clone();
 		return;
-	}
-	
-	private void findAvailable() {
-	    available.clear();
-	    Location loc = fishingLocation.clone();
-	    Location xLeft = loc.add(-1, 0, 0);
-	    loc = fishingLocation.clone();
-	    Location xRight = loc.add(1, 0, 0);
-        loc = fishingLocation.clone();
-	    Location zBack = loc.add(0, 0, -1);
-        loc = fishingLocation.clone();
-	    Location zForward = loc.add(0, 0, 1);
-	    
-	    available.add(fishingLocation);
-
-	    if(xLeft.getBlock().getTypeId() == 9 || xLeft.getBlock().getTypeId() == 8)
-	        available.add(xLeft);
-        if(xRight.getBlock().getTypeId() == 9 || xRight.getBlock().getTypeId() == 8)
-            available.add(xRight);
-        if(zBack.getBlock().getTypeId() == 9 || zBack.getBlock().getTypeId() == 8)
-            available.add(zBack);
-        if(zForward.getBlock().getTypeId() == 9 || zForward.getBlock().getTypeId() == 8)
-            available.add(zForward);
 	}
 	
 	private void cast() {
 		DenizenAPI.getDenizenNPC(npc).action("cast fishing rod", null);
-	    findAvailable();
+
+        double v = 34;
+        double g = 20;
+        Location from = null;
+        Location to = null;
+
         fishHook = new EntityFishingHook(nmsworld, eh);
         nmsworld.addEntity(fishHook);
+        from = npc.getBukkitEntity().getLocation();
+        from = from.add(0,.33,0);
+        to = fishingLocation;
+
+        Vector test = to.clone().subtract(from).toVector();
+        Double elev = test.getY();
+        Double testAngle = launchAngle(from, to, v, elev, g);
+        Double hangtime = hangtime(testAngle, v, elev, g);
+        Vector victor = to.clone().subtract(from).toVector();
+        Double dist = Math.sqrt(Math.pow(victor.getX(), 2) + Math.pow(victor.getZ(), 2));
+        elev = victor.getY();
         
-        int x = (int)(Math.random()*available.size());
+        if (dist == 0)
+            return;
+
+        Double launchAngle = launchAngle(from, to, v, elev, g);
+        if (launchAngle == null)
+            return;
+        victor.setY(Math.tan(launchAngle) * dist);
+        victor = normalizeVector(victor);
+        v = v + (.5 * Math.pow(hangtime, 2));   
+        Random rand = new Random(1234);
+        v = v+ (rand.nextDouble() - .8)/2;
+        victor = victor.multiply(v / 20.0);
+
+        Projectile theHook = (Projectile) fishHook.getBukkitEntity();
+        theHook.setShooter(npc.getBukkitEntity());
+        theHook.setVelocity(victor);
         
-        fishingSpot = available.get(x);
-        Vector v1 = fishHook.getBukkitEntity().getLocation().toVector();
-        Vector v2 = fishingSpot.toVector();
-        Vector v3 = v2.subtract(v1).normalize().multiply(1.5);
-        
-        ((Projectile) fishHook.getBukkitEntity()).setShooter(npc.getBukkitEntity());
-        PlayerAnimation.ARM_SWING.play((Player) npc.getBukkitEntity());
-        fishHook.getBukkitEntity().setVelocity(v3);
-        
-	}
+        PlayerAnimation.ARM_SWING.play((Player) npc.getBukkitEntity());	
+    }
 	
 	private void reel() {
-	    
 		DenizenAPI.getDenizenNPC(npc).action("reel in fishing rod", null);
 		
-	    int chance = (int)(Math.random()*100);
+        int chance = (int)(Math.random()*100);
+        
+        try{
+            fishHook.getBukkitEntity().remove();
+        } catch(Exception e){ }
+        
+        if (chance>65 && fishHook != null) {
+            try{
+                fish.getBukkitEntity().remove();
+            } catch(Exception e) { }
+            fish = new EntityItem(nmsworld, fishHook.locX, fishHook.locY, fishHook.locZ, new ItemStack(Item.RAW_FISH));
+            double d5 = npc.getBukkitEntity().getLocation().getX() - fishHook.locX;
+            double d6 = npc.getBukkitEntity().getLocation().getY() - fishHook.locY;
+            double d7 = npc.getBukkitEntity().getLocation().getZ() - fishHook.locZ;
+            double d8 = (double) MathHelper.sqrt(d5 * d5 + d6 * d6 + d7 * d7);
+            double d9 = 0.1D;
 
-	    if(chance > 65) {
-			DenizenAPI.getDenizenNPC(npc).action("catch fish", null);
-
-	        Entity fish = fishingSpot.getWorld().dropItem(fishingSpot, (new ItemStack(Material.RAW_FISH)));
-	        Vector v1 = fish.getLocation().toVector();
-	        Vector v2 = npc.getBukkitEntity().getLocation().add(0,14,0).toVector();
-	        Vector v3 = v2.subtract(v1).normalize().multiply(0.975);
-	        fish.setVelocity(v3);
-	    }
-	    PlayerAnimation.ARM_SWING.play((Player) npc.getBukkitEntity());
-        fishHook.getBukkitEntity().remove();
-	}
-	
-	/**
-	 * Makes the NPC fish at the specified location
-	 * 
-	 * TODO Add variance, so each cast doesnt land in the exact same spot.
-	 * 
-	 * @param location
-	 */
-	public void startFishing(Location location) {
-		DenizenAPI.getDenizenNPC(npc).action("start fishing", null);
-
-		nmsworld.addEntity(fishHook);
-		
-		Vector v1 = fishHook.getBukkitEntity().getLocation().toVector();
-		Vector v2 = location.toVector();
-		Vector v3 = v2.subtract(v1).normalize().multiply(1.5);
-		
-		((Projectile) fishHook.getBukkitEntity()).setShooter(npc.getBukkitEntity());
+            fish.motX = d5 * d9;
+            fish.motY = d6 * d9 + (double) MathHelper.sqrt(d8) * 0.08D;
+            fish.motZ = d7 * d9;
+            nmsworld.addEntity(fish);
+            DenizenAPI.getDenizenNPC(npc).action("catch fish", null);
+        }
+        
         PlayerAnimation.ARM_SWING.play((Player) npc.getBukkitEntity());
-		fishHook.getBukkitEntity().setVelocity(v3);
-		
-		fishing = true;
-		fishingLocation = location;
-		fishingSpot = fishingLocation.clone();
-	    findAvailable();
-	}
-	
-	/**
-	 * Makes the stop fishing.
-	 */
-	public void stopFishing() {
-		
-		DenizenAPI.getDenizenNPC(npc).action("stop fishing", null);
-
-        PlayerAnimation.ARM_SWING.play((Player) npc.getBukkitEntity());
-
-		dB.log("...removing fish hook.");
-		fishHook.getBukkitEntity().remove();
-		
-		fishingLocation = null;
-		fishingSpot = null;
-		fishing = false;
 	}
 	
 	/**
@@ -195,7 +192,7 @@ public class FishingTrait extends Trait {
 	
 	/**
 	 * Gets the location the NPC is casting to
-	 * Returns null if the NPC isnt fishing
+	 * Returns null if the NPC isn't fishing
 	 * 
 	 * @return Location
 	 */
@@ -206,5 +203,35 @@ public class FishingTrait extends Trait {
 	public FishingTrait() {
 		super("fishing");
 	}
+
+    public static Double launchAngle(Location from, Location to, double v, double elev, double g){
+        Vector victor = from.clone().subtract(to).toVector();
+        Double dist =  Math.sqrt(Math.pow(victor.getX(), 2) + Math.pow(victor.getZ(), 2));
+        double v2 = Math.pow(v,2);
+        double v4 = Math.pow(v,4);
+        double derp =  g*(g*Math.pow(dist,2)+2*elev*v2);
+
+        if(v4 < derp)
+            return null;
+        else
+            return Math.atan( (v2-   Math.sqrt(v4 - derp))/(g*dist));
+    }
+
+    public static double hangtime(double launchAngle, double v, double elev, double g){
+        double a = v * Math.sin(launchAngle);
+        double b = -2*g*elev;
+
+        if(Math.pow(a, 2) + b < 0)
+            return 0;
+
+        return (a + Math.sqrt(Math.pow(a, 2) + b))  /  g;
+    }
+    
+    public static Vector normalizeVector(Vector victor){
+        double  mag = Math.sqrt(Math.pow(victor.getX(), 2) + Math.pow(victor.getY(), 2)  + Math.pow(victor.getZ(), 2));
+        if (mag !=0) 
+            return victor.multiply(1/mag);
+        return victor.multiply(0);
+    }
 
 }
