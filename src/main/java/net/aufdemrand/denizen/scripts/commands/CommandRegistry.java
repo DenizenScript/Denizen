ca8fbd1c1738a231a458cbd2b41731450afc28ef
Mason Adkins <zappalives0@gmail.com>
2013-03-24 14:58:53 -0400

    added EQUIP command (to equip items to NPCs)

--- src/main/java/net/aufdemrand/denizen/scripts/commands/CommandRegistry.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/CommandRegistry.java
@@ -90,9 +90,12 @@ public class CommandRegistry implements DenizenRegistry {
 		
         registerCoreMember(EngageCommand.class, 
         		"ENGAGE", "engage (duration:#) (npcid:#)", 0);
-        
+
         registerCoreMember(EngraveCommand.class, 
                 "ENGRAVE", "engrave (SET|REMOVE) (TARGET:player_name)", 0);
+        
+        registerCoreMember(EquipCommand.class, 
+                "EQUIP", "equip ({HAND}|BOOTS|LEGS|CHEST|HEAD) [item:#(:#)|item:material(:#)]", 1);
         
         registerCoreMember(ExecuteCommand.class, 
         		"EXECUTE", "execute [as_player|as_op|as_npc|as_server] [\"Bukkit command\"]", 2);
---
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/EquipCommand.java
@@ -0,0 +1,80 @@
+package net.aufdemrand.denizen.scripts.commands.core;
+
+import net.aufdemrand.denizen.exceptions.CommandExecutionException;
+import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
+import net.aufdemrand.denizen.scripts.ScriptEntry;
+import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
+import net.aufdemrand.denizen.utilities.arguments.Item;
+import net.aufdemrand.denizen.utilities.arguments.aH;
+import net.aufdemrand.denizen.utilities.debugging.dB;
+import net.aufdemrand.denizen.utilities.debugging.dB.Messages;
+import net.citizensnpcs.api.npc.NPC;
+import net.citizensnpcs.api.trait.trait.Equipment;
+
+public class EquipCommand extends AbstractCommand{
+	public enum EquipType { HAND, BOOTS, LEGS, CHEST, HEAD }
+	
+	@Override
+	public void parseArgs(ScriptEntry scriptEntry)
+			throws InvalidArgumentsException {
+		
+		EquipType equipType = EquipType.HAND;
+		Item item = null;
+		
+		for (String arg : scriptEntry.getArguments()) {
+			if (aH.matchesItem(arg)) {
+				item = aH.getItemFrom(arg);
+				dB.echoDebug(Messages.DEBUG_SET_ITEM, arg);
+			} else if (aH.matchesArg("ITEMINHAND, HAND, HOLDING", arg)) {
+				try {
+					equipType = EquipType.valueOf(arg);
+					dB.echoDebug("... equiping to " + equipType.name());
+				} catch (Exception e) { throw new InvalidArgumentsException(Messages.ERROR_UNKNOWN_ARGUMENT, arg); }
+			} 
+		}
+		
+		if (item == null) {
+			dB.echoError("...no item specified to equip!");
+		}
+		
+		scriptEntry.addObject("item", item)
+			.addObject("equipType", equipType);
+	}
+
+	@Override
+	public void execute(ScriptEntry scriptEntry)
+			throws CommandExecutionException {
+		
+		EquipType equipType = (EquipType) scriptEntry.getObject("equipType");
+		Item item = (Item) scriptEntry.getObject("item");
+		NPC npc = scriptEntry.getNPC().getCitizen();
+		
+		if (!npc.hasTrait(Equipment.class)) npc.addTrait(Equipment.class);
+		Equipment trait = npc.getTrait(Equipment.class);
+		
+		switch (equipType) {
+		case BOOTS:
+			trait.set(4, item);
+			dB.echoDebug("..." + item.getType() + " equipped to BOOTS");
+			break;
+		case CHEST:
+			trait.set(2, item);
+			dB.echoDebug("..." + item.getType() + " equipped to CHEST");
+			break;
+		case HAND:
+			trait.set(0, item);
+			dB.echoDebug("..." + item.getType() + " equipped to HAND");
+			break;
+		case HEAD:
+			trait.set(1, item);
+			dB.echoDebug("..." + item.getType() + " equipped to HEAD");
+			break;
+		case LEGS:
+			trait.set(3, item);
+			dB.echoDebug("..." + item.getType() + " equipped to LEGS");
+			break;
+		}
+		
+	}
+
+}
