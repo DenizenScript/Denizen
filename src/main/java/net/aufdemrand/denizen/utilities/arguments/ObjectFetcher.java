d2be7cbfd6a45cecd805cad931011899091239ec
Jeremy Schroeder <jeremy@ssdbg.com>
2013-04-08 14:41:19 -0400

    Add ObjectFetcher annotation for upcoming object mechanisms :)

--- src/main/java/net/aufdemrand/denizen/utilities/arguments/ObjectFetcher.java
+++ src/main/java/net/aufdemrand/denizen/utilities/arguments/ObjectFetcher.java
@@ -1,5 +1,12 @@
 package net.aufdemrand.denizen.utilities.arguments;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface ObjectFetcher {
 
     String value();
