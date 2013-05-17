e9d54cb993fd1700a272f7f5ca7769c6ea8cfc7c
Mason Adkins <zappalives0@gmail.com>
2013-05-16 20:20:53 -0400

    allow unsafe enchantments in item scripts.

--- src/main/java/net/aufdemrand/denizen/scripts/containers/core/ItemScriptContainer.java
+++ src/main/java/net/aufdemrand/denizen/scripts/containers/core/ItemScriptContainer.java
@@ -76,7 +76,7 @@ public class ItemScriptContainer extends ScriptContainer {
                         }
                         // Add enchantment
                         Enchantment ench = Enchantment.getByName(enchantment.toUpperCase());
-                        stack.getItemStack().addEnchantment(ench, level);
+                        stack.getItemStack().addUnsafeEnchantment(ench, level);
                     } catch (Exception e) {
                         dB.echoError("While constructing '" + getName() + "', there has been a problem. '" + enchantment + "' is an invalid Enchantment!");
                         continue;
