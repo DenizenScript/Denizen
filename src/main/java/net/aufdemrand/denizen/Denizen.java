9b8d06686b93de34a32df49f54fa0ccb991f0e7e
Jeremy Schroeder <jeremy@ssdbg.com>
2013-05-27 18:21:27 -0400

    Version increment.

--- src/main/java/net/aufdemrand/denizen/Denizen.java
+++ src/main/java/net/aufdemrand/denizen/Denizen.java
@@ -43,7 +43,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Denizen extends JavaPlugin {
     public final static int configVersion = 2;
-    public final static String versionTag = "0.8.8 pre-release";
+    public final static String versionTag = "0.9.1";
 
     private boolean startedSuccessful = false;
 
