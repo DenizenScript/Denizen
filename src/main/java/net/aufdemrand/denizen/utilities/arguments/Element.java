e11de6d911fd4244841cb29eca277291e7d8e20a
Jeremy Schroeder <jeremy@ssdbg.com>
2013-06-14 23:23:20 -0400

    add contains attribute to element

--- src/main/java/net/aufdemrand/denizen/utilities/arguments/Element.java
+++ src/main/java/net/aufdemrand/denizen/utilities/arguments/Element.java
@@ -10,6 +10,7 @@ import org.bukkit.util.StringUtil;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Element implements dScriptArgument {
 
@@ -95,8 +96,8 @@ public class Element implements dScriptArgument {
                 || attribute.startsWith("as_money")) {
             try {
                 DecimalFormat d = new DecimalFormat("0.00");
-            return new Element(String.valueOf(d.format(Double.valueOf(element))))
-                    .getAttribute(attribute.fulfill(1)); }
+                return new Element(String.valueOf(d.format(Double.valueOf(element))))
+                        .getAttribute(attribute.fulfill(1)); }
             catch (NumberFormatException e) {
                 dB.echoError("'" + element + "' is not a valid Money format.");
                 return null;
@@ -142,17 +143,31 @@ public class Element implements dScriptArgument {
                     .getAttribute(attribute.fulfill(1));
         }
 
+        if (attribute.startsWith("contains")) {
+            String contains = attribute.getContext(1);
+
+            if (contains.toLowerCase().startsWith("regex:")) {
+                if (Pattern.compile(contains, Pattern.CASE_INSENSITIVE).matcher(element).matches())
+                    return new Element("true").getAttribute(attribute.fulfill(1));
+                else return new Element("false").getAttribute(attribute.fulfill(1));
+            }
+
+            else if (element.toLowerCase().contains(contains.toLowerCase()))
+                return new Element("true").getAttribute(attribute.fulfill(1));
+            else return new Element("false").getAttribute(attribute.fulfill(1));
+        }
+
         if (attribute.startsWith("split") && attribute.startsWith("limit", 2)) {
             String split_string = (attribute.hasContext(1) ? attribute.getContext(1) : " ");
             Integer limit = (attribute.hasContext(2) ? attribute.getIntContext(2) : 1);
-            if (split_string.toUpperCase().startsWith("regex:"))
+            if (split_string.toLowerCase().startsWith("regex:"))
                 return new dList(Arrays.asList(element.split(split_string.split(":", 2)[1], limit))).getAttribute(attribute.fulfill(1));
             else
                 return new dList(Arrays.asList(StringUtils.split(element, split_string, limit))).getAttribute(attribute.fulfill(1));        }
 
         if (attribute.startsWith("split")) {
             String split_string = (attribute.hasContext(1) ? attribute.getContext(1) : " ");
-            if (split_string.toUpperCase().startsWith("regex:"))
+            if (split_string.toLowerCase().startsWith("regex:"))
                 return new dList(Arrays.asList(element.split(split_string.split(":", 2)[1]))).getAttribute(attribute.fulfill(1));
             else
                 return new dList(Arrays.asList(StringUtils.split(element, split_string))).getAttribute(attribute.fulfill(1));
