d7c87eddb6a7a2debb6aef2c691f44f5e355b3e9
Jeremy Schroeder <jeremy@ssdbg.com>
2013-07-04 15:05:21 -0400

    ...

--- src/main/java/net/aufdemrand/denizen/scripts/commands/core/RunCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/RunCommand.java
@@ -2,10 +2,7 @@ package net.aufdemrand.denizen.scripts.commands.core;
 
 import net.aufdemrand.denizen.exceptions.CommandExecutionException;
 import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
-import net.aufdemrand.denizen.objects.Duration;
-import net.aufdemrand.denizen.objects.Element;
-import net.aufdemrand.denizen.objects.aH;
-import net.aufdemrand.denizen.objects.dScript;
+import net.aufdemrand.denizen.objects.*;
 import net.aufdemrand.denizen.scripts.ScriptEntry;
 import net.aufdemrand.denizen.scripts.ScriptQueue;
 import net.aufdemrand.denizen.scripts.ScriptRegistry;
@@ -41,24 +38,18 @@ public class RunCommand extends AbstractCommand {
                 "Put a script's execution off by specifying a delay. \n" +
                 "- run gate_closer delay:10s \n" +
                 "Use run with a delay and 'loop' argument to create a script on a " +
-                "timer. You can even specify the amount of loops.\n" +
-                "- run server_announcement loop 1h";
-                //"Attach some definitions to "
+                "timer. You can even specify the amount of loops. \n" +
+                "- run server_announcement loop 1h \n" +
+                "You can also attach some definitions to the task script being run. \n" +
+                "- run get_color_of def:i@epic_leather_armor";
     }
 
     public String getUsage() {
-        return "run [script] (as:p@player|n@npc) (id:id_name) (delay:duration) (loop) (q:#)";
+        return "run [script] (as:p@player|n@npc) (def:...|...) (id:id_name) (delay:duration) (loop) (q:#)";
     }
 
     @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-
-        // Initialize necessary fields
-        Map<String, String> context = null;
-        Boolean instant = false;
-        dScript script = null;
-        Duration delay = null;
-        ScriptQueue queue = scriptEntry.getResidingQueue();
 
         for (aH.Argument arg : aH.interpret(scriptEntry.getArguments())) {
 
@@ -72,7 +63,7 @@ public class RunCommand extends AbstractCommand {
             else if (arg.matches("instant, instantly"))
                 scriptEntry.addObject("instant", Element.TRUE);
 
-            else if (arg.matchesPrefix("d, delay")
+            else if (arg.matchesPrefix("delay")
                     && arg.matchesArgumentType(Duration.class))
                 scriptEntry.addObject("delay", arg.asType(Duration.class));
 
@@ -82,6 +73,17 @@ public class RunCommand extends AbstractCommand {
             else if (arg.matchesPrefix("q, quantity")
                 && arg.matchesPrimitive(aH.PrimitiveType.Integer))
                 scriptEntry.addObject("quantity", arg.asElement());
+
+            else if (arg.matchesPrefix("a, as")
+                    && arg.matchesArgumentType(dPlayer.class))
+                scriptEntry.setPlayer((dPlayer) arg.asType(dPlayer.class));
+
+            else if (arg.matchesPrefix("a, as")
+                    && arg.matchesArgumentType(dNPC.class))
+                scriptEntry.setNPC((dNPC) arg.asType(dNPC.class));
+
+            else if (arg.matchesPrefix("d, def"))
+                scriptEntry.addObject("definitions", arg.asType(dList.class));
         }
 
         if (!scriptEntry.hasObject("script"))
@@ -92,85 +94,7 @@ public class RunCommand extends AbstractCommand {
 	@Override
     public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
 
-        Map<String, String> context = (HashMap<String, String>) scriptEntry.getObject("context");
-        dScript script = (dScript) scriptEntry.getObject("script");
-        ScriptQueue queue = (ScriptQueue) scriptEntry.getObject("queue");
-        Boolean instant = (Boolean) scriptEntry.getObject("instant");
-        Duration delay = (Duration) scriptEntry.getObject("delay");
 
-        // Debug output
-        dB.report(this.getName(),
-                script.debug()
-                        + (delay != null ? delay.debug() : "")
-                        + aH.debugObj("Instant", instant.toString())
-                        + aH.debugObj("Queue", queue.id)
-                        + (instant == false ? aH.debugObj("Speed", queue.getSpeed()) : "" )
-                        + (context != null ? aH.debugObj("Context", context.toString()) : "")
-                        + (scriptEntry.getPlayer() != null
-                        ? aH.debugObj("Player", scriptEntry.getPlayer().getName()) : "")
-                        + (scriptEntry.getNPC() != null
-                        ? aH.debugObj("NPC", scriptEntry.getNPC().toString()) : ""));
-
-        if (instant) {
-            // Instant, but no delay
-            if (delay == null)
-
-                if (scriptEntry.getResidingQueue() != queue)
-                    // Instant, no delay, new queue
-                    ((TaskScriptContainer) script.getContainer()).setSpeed(Duration.valueOf("0"))
-                            .runTaskScript(queue.id,
-                                    scriptEntry.getPlayer(),
-                                    scriptEntry.getNPC(),
-                                    context);
-
-                else
-                    // Instant, no delay, injection into current queue
-                    ((TaskScriptContainer) script.getContainer()).setSpeed(Duration.valueOf("0"))
-                            .injectTaskScript(queue.id,
-                                    scriptEntry.getPlayer(),
-                                    scriptEntry.getNPC(),
-                                    context);
-
-
-            else
-                // Instant, has delay, new queue
-                ((TaskScriptContainer) script.getContainer()).setSpeed(Duration.valueOf("0"))
-                        .runTaskScriptWithDelay(queue.id,
-                                scriptEntry.getPlayer(),
-                                scriptEntry.getNPC(),
-                                context,
-                                delay);
-
-        } else {
-
-            if (delay == null)
-
-                // Not instant, no delay, new queue
-                if (scriptEntry.getResidingQueue() != queue)
-                    ((TaskScriptContainer) script.getContainer())
-                            .runTaskScript(queue.id,
-                                    scriptEntry.getPlayer(),
-                                    scriptEntry.getNPC(),
-                                    context);
-
-                else
-                    // Not instant, no delay, injection into current queue
-                    ((TaskScriptContainer) script.getContainer())
-                            .injectTaskScript(queue.id,
-                                    scriptEntry.getPlayer(),
-                                    scriptEntry.getNPC(),
-                                    context);
-
-
-            else
-                // Not instant, delayed, new queue
-                ((TaskScriptContainer) script.getContainer())
-                        .runTaskScriptWithDelay(queue.id,
-                                scriptEntry.getPlayer(),
-                                scriptEntry.getNPC(),
-                                context,
-                                delay);
-        }
     }
 
 }
