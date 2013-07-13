b2116203489c0027dcce50e4552f0e0c4a3e2d5d
Jeremy Schroeder <jeremy@ssdbg.com>
2013-07-13 14:56:38 -0400

    Fix speed on WalkCommand.

--- src/main/java/net/aufdemrand/denizen/scripts/commands/npc/WalkCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/npc/WalkCommand.java
@@ -79,10 +79,11 @@ public class WalkCommand extends AbstractCommand {
                 scriptEntry.getNPC().getNavigator().getDefaultParameters().range((float) distance + 10);
         }
 
+        scriptEntry.getNPC().getNavigator().setTarget(loc);
+
         if (speed != null)
             scriptEntry.getNPC().getNavigator().getLocalParameters().speedModifier(speed.asFloat());
 
-        scriptEntry.getNPC().getNavigator().setTarget(loc);
     }
 
 
