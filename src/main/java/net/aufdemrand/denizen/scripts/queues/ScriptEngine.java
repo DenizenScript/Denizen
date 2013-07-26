44f4d120f15fe072cf49fbb16d4d4db3aec1f740
Jeremy Schroeder <jeremy@ssdbg.com>
2013-07-26 02:43:48 -0400

    Move ScriptEngine.java

--- src/main/java/net/aufdemrand/denizen/scripts/queues/ScriptEngine.java
+++ src/main/java/net/aufdemrand/denizen/scripts/queues/ScriptEngine.java
@@ -1,6 +1,7 @@
-package net.aufdemrand.denizen.scripts;
+package net.aufdemrand.denizen.scripts.queues;
 
 import net.aufdemrand.denizen.Denizen;
+import net.aufdemrand.denizen.scripts.ScriptEntry;
 import net.aufdemrand.denizen.scripts.commands.CommandExecuter;
 import net.aufdemrand.denizen.scripts.queues.ScriptQueue;
 import net.aufdemrand.denizen.scripts.queues.core.Delayable;
@@ -9,10 +10,11 @@ import net.aufdemrand.denizen.utilities.debugging.dB;
 
 public class ScriptEngine {
 
-    final private Denizen denizen;
 
+    final private Denizen denizen;
     final private RequirementChecker requirementChecker;
     final private CommandExecuter commandExecuter;
+
 
     public ScriptEngine(Denizen denizenPlugin) {
         denizen  = denizenPlugin;
@@ -20,6 +22,7 @@ public class ScriptEngine {
         commandExecuter = new CommandExecuter(denizen);
         requirementChecker = new RequirementChecker(denizen);
     }
+
 
     public void revolve(ScriptQueue scriptQueue) {
         // Check last ScriptEntry to see if it should be waited for
@@ -31,41 +34,46 @@ public class ScriptEngine {
         ScriptEntry scriptEntry = scriptQueue.getNext();
 
         while (scriptEntry != null) {
-            // Update ScriptEntry with queue information
+            // Mark script entry with Queue that is sending it to the executer
             scriptEntry.setSendingQueue(scriptQueue);
 
-                // Mark script entry with Queue that is sending it to the executer
-                scriptEntry.setSendingQueue(scriptQueue);
-                // Execute the scriptEntry
-                try {
-                    getScriptExecuter().execute(scriptEntry);
-                } catch (Throwable e) {
-                    dB.echoError("Woah! An exception has been called with this command!");
-                    if (!dB.showStackTraces)
-                        dB.echoError("Enable '/denizen stacktrace' for the nitty-gritty.");
-                    else e.printStackTrace();
-                }
-                // Set as last entry executed
-                scriptQueue.setLastEntryExecuted(scriptEntry);
+            // Execute the scriptEntry
+            try { getScriptExecuter().execute(scriptEntry); }
 
-                // Check if the scriptQueue is delayed
-                if (scriptQueue instanceof Delayable)
-                    if (((Delayable) scriptQueue).isDelayed()) break;
+            catch (Throwable e) {
+                dB.echoError("Woah! An exception has been called with this command!");
+                if (!dB.showStackTraces)
+                    dB.echoError("Enable '/denizen stacktrace' for the nitty-gritty.");
+                else e.printStackTrace();
+            }
+            // Set as last entry executed
+            scriptQueue.setLastEntryExecuted(scriptEntry);
 
-                // If the entry is instant, and not injected, get the next Entry
-                if (scriptEntry.isInstant() && !scriptQueue.hasInjectedItems) {
-                    // Remove from execution list
-                    scriptEntry = scriptQueue.getNext();
-                }
-                // If injected, end the revolution to allow the queue to catch
-                // up.
-                else if (scriptQueue.hasInjectedItems) {
-                    scriptQueue.hasInjectedItems = false;
-                    break;
-                }
-                // If entry isn't instant, end the revolution and wait for another
-                else
-                    break;
+            //
+            // Determine what should happen after execution:
+            //
+
+            // Check if the scriptQueue is delayed
+            if (scriptQueue instanceof Delayable) {
+                // dB.log("Is delayed? " + ((Delayable) scriptQueue).isDelayed());
+                if (((Delayable) scriptQueue).isDelayed()) break;
+            }
+
+            // If the entry is instant, and not injected, get the next Entry
+            if (scriptEntry.isInstant()) { // ---> What does this do? && !scriptQueue.hasInjectedItems) {
+                // Remove from execution list
+                scriptEntry = scriptQueue.getNext();
+            }
+
+            // ---> What does this do?
+            // else if (scriptQueue.hasInjectedItems) {
+            //    scriptQueue.hasInjectedItems = false;
+            //    break;
+            // }
+
+            // If entry isn't instant, end the revolution and wait for another
+            else
+                break;
         }
     }
 
