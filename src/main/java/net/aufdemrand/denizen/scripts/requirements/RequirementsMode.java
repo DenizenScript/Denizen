31645a9890dbcada3cd876b7c23e0d7965f29cb9
SpaceEmotion <spaceemotion@gmail.com>
2013-08-05 14:44:15 +0200

    [Experimental] Cleaned up the requirements, added FIRST_NUM_AND_ANY_NUM Info for developers: This commit changes the FIRST_AND_ANY_NUM to a "First 1 and any #" mode

--- src/main/java/net/aufdemrand/denizen/scripts/requirements/RequirementsMode.java
+++ src/main/java/net/aufdemrand/denizen/scripts/requirements/RequirementsMode.java
@@ -5,12 +5,12 @@ import java.util.regex.Pattern;
 
 public class RequirementsMode {
 
+    public enum Mode { ALL, NONE, ANY_NUM, FIRST_NUM_AND_ANY_NUM }
 
-    public enum Mode { ALL, NONE, ANY_NUM, FIRST_AND_ANY_NUM }
-    private static Pattern intsOnly = Pattern.compile("(\\d+)");
+    private final static Pattern intsOnly = Pattern.compile("(\\d+)");
 
-    protected Mode mode;
-    protected int modeInt;
+    protected final Mode mode;
+    protected int[] modeInt;
 
     public Mode getMode() {
         return mode;
@@ -23,21 +23,68 @@ public class RequirementsMode {
      * ALL <br/>
      * NONE <br/>
      * ANY # <br/>
-     * FIRST AND ANY # <br/>
+     * FIRST (#) AND ANY # (the first number is optional)<br />
      *
      * @param arg the value of the Requirements.Mode yaml-key from the script
      */
     public RequirementsMode(String arg) {
-        arg = arg.toUpperCase();
-        if (arg.equals("NONE")) mode = Mode.NONE;
-        else if (arg.equals("ALL")) mode = Mode.ALL;
-        else if (arg.contains("FIRST")) mode = Mode.FIRST_AND_ANY_NUM;
-        else if (arg.contains("ANY")) mode = Mode.ANY_NUM;
+        arg = arg.trim().toUpperCase();
 
-        Matcher findInt = intsOnly.matcher(arg);
-        if (findInt.matches())
-            modeInt = Integer.valueOf(findInt.group(1));
-        else modeInt = 1;
+        if (arg.equals("NONE")) {
+            mode = Mode.NONE;
+
+        } else if (arg.equals("ALL")) {
+            mode = Mode.ALL;
+
+        } else {
+            boolean first = arg.contains("FIRST");
+            boolean any = arg.contains("ANY");
+
+            Matcher found = intsOnly.matcher(arg);
+
+            if(!any) {
+                mode = Mode.NONE;
+
+            } else if(!first) {
+                mode = Mode.ANY_NUM;
+
+                if(found.matches()) {
+                    modeInt = new int[] { Integer.valueOf(found.group(1)) };
+
+                } else {
+                    modeInt = new int[] { 1 };
+                }
+            } else {
+                mode = Mode.FIRST_NUM_AND_ANY_NUM;
+
+                int count = found.groupCount();
+                modeInt = new int[2];
+
+                if(count >= 2) {
+                    modeInt[0] = Integer.valueOf(found.group(1));
+                    modeInt[1] = Integer.valueOf(found.group(2));
+
+                } else if(count == 1) {
+                    int index = 1;
+
+                    // Check if the "FIRST" argument has a number
+                    if(arg.startsWith("FIRST AND")) {
+                        modeInt[0] = 1;
+
+                    } else {
+                        modeInt[0] = Integer.valueOf(found.group(index));
+                        index++;
+                    }
+
+                    // Check if we end with a number or not
+                    if(arg.endsWith("ANY")) {
+                        modeInt[1] = 1;
+                    } else {
+                        modeInt[1] = Integer.valueOf(found.group(index));
+                    }
+                }
+            }
+        }
     }
 
     public RequirementsMode(Mode mode) {
@@ -46,7 +93,7 @@ public class RequirementsMode {
 
     public RequirementsMode(Mode mode, int num) {
         this.mode = mode;
-        this.modeInt = num;
+        this.modeInt = new int[] { num };
     }
 
 }
