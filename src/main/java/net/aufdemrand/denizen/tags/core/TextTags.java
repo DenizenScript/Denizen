396e5e23018b0ffdb44230ef47e2106c1d73700b
MorphanOne <morphanone@gmail.com>
2013-10-21 00:19:39 -0400

    ChatColor meta

--- src/main/java/net/aufdemrand/denizen/tags/core/TextTags.java
+++ src/main/java/net/aufdemrand/denizen/tags/core/TextTags.java
@@ -81,7 +81,159 @@ public class TextTags implements Listener {
     final String[] code = {"0","1","2","3","4","5","6","7","8","9"
             ,"a","b","c","d","e","f","k","l","m","n","o","r"};
 
-    // TODO: Meta for all of these! All of them!
+    // <--[tag]
+    // @attribute <&0>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Black.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&1>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Dark Blue.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&2>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Dark Green.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&3>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Dark Aqua.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&4>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Dark Red.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&5>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Dark Purple.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&6>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Gold.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&7>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Gray.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&8>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Dark Gray.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&9>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Blue.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&a>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Green.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&b>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Aqua.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&c>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Red.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&d>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Light Purple.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&e>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters Yellow.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&f>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters White.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&k>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters obfuscated.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&l>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters bolded.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&m>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters strikethroughed.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&n>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters underlined.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&o>
+    // @returns ChatColor
+    // @description
+    // Makes the following characters italicized.
+    // -->
+    
+    // <--[tag]
+    // @attribute <&r>
+    // @returns ChatColor
+    // @description
+    // Resets the following characters to normal.
+    // -->
 
     @EventHandler
     public void colorTags(ReplaceableTagEvent event) {
