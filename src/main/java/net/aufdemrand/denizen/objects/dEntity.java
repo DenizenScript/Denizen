57cc1fcb6417c5202cb40dc2c164d985bc1fd3d4
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2013-10-28 15:51:21 -0700

    Use Citizens' NPC.teleport() function, fixes #492

--- src/main/java/net/aufdemrand/denizen/objects/dEntity.java
+++ src/main/java/net/aufdemrand/denizen/objects/dEntity.java
@@ -592,7 +592,8 @@ public class dEntity implements dObject, Adjustable {
 
     public void spawnAt(Location location) {
         // If the entity is already spawned, teleport it.
-        if (entity != null && isUnique()) entity.teleport(location);
+        if (npc != null) npc.teleport(location, TeleportCause.PLUGIN);
+        else if (entity != null && isUnique()) entity.teleport(location);
 
         else {
             if (npc != null) {
