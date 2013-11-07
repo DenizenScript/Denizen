f03495276a340306fdc9e0dbabf8382907147ecc
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2013-11-06 16:15:24 -0800

    Add new command 'despawn'
    This command will despawn an NPC without permanently removing it.
    Will defaultly used the linked NPC but can use a list of NPCs instead.

--- src/main/java/net/aufdemrand/denizen/scripts/commands/entity/RemoveCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/entity/RemoveCommand.java
@@ -32,19 +32,16 @@ public class RemoveCommand extends AbstractCommand {
 
             if (!scriptEntry.hasObject("entities")
                     && arg.matchesArgumentList(dEntity.class)) {
-
                 scriptEntry.addObject("entities", ((dList) arg.asType(dList.class)).filter(dEntity.class));
             }
 
             else if (!scriptEntry.hasObject("region")
                     && arg.matchesPrefix("region, r")) {
-
                 scriptEntry.addObject("region", arg.asElement());
             }
 
             else if (!scriptEntry.hasObject("world")
                     && arg.matchesArgumentType(dWorld.class)) {
-
                 scriptEntry.addObject("world", arg.asType(dWorld.class));
             }
 
