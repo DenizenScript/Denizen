4e20f2525910c2007609a2017b7ee0114ac23d78
Jeremy Schroeder <jeremy@ssdbg.com>
2013-11-10 13:47:33 -0500

    Overhaul Reset Command. Allow complete reset of a player in Saves. Fixes #512

--- src/main/java/net/aufdemrand/denizen/scripts/commands/core/ResetCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/ResetCommand.java
@@ -2,11 +2,10 @@ package net.aufdemrand.denizen.scripts.commands.core;
 
 import net.aufdemrand.denizen.exceptions.CommandExecutionException;
 import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
-import net.aufdemrand.denizen.objects.Duration;
-import net.aufdemrand.denizen.objects.dScript;
+import net.aufdemrand.denizen.objects.*;
 import net.aufdemrand.denizen.scripts.ScriptEntry;
 import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
-import net.aufdemrand.denizen.objects.aH;
+import net.aufdemrand.denizen.utilities.DenizenAPI;
 import net.aufdemrand.denizen.utilities.debugging.dB;
 import net.citizensnpcs.api.CitizensAPI;
 import net.citizensnpcs.api.npc.NPC;
@@ -14,12 +13,10 @@ import org.bukkit.entity.EntityType;
 
 public class ResetCommand extends AbstractCommand {
 
-    private enum Type { FINISH, FAIL, PLAYER_COOLDOWN, GLOBAL_COOLDOWN }
+    private enum Type { FINISH, FAIL, PLAYER_COOLDOWN, GLOBAL_COOLDOWN, SAVES }
 
     @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
-
-        // TODO: UPDATE THIS COMMAND!
 
         for (aH.Argument arg : aH.interpret(scriptEntry.getArguments())) {
 
@@ -27,47 +24,85 @@ public class ResetCommand extends AbstractCommand {
                     && !scriptEntry.hasObject("type"))
                 scriptEntry.addObject("type", Type.FINISH);
 
-            if (arg.matches("fails, failed, fail")
+            else if (arg.matches("fails, failed, fail")
                     && !scriptEntry.hasObject("type"))
                 scriptEntry.addObject("type", Type.FAIL);
 
-            if (arg.matches("cooldown")
+            else if (arg.matches("cooldown")
                     && !scriptEntry.hasObject("type"))
                 scriptEntry.addObject("type", Type.PLAYER_COOLDOWN);
 
-            if (arg.matches("global_cooldown")
+            else if (arg.matches("global_cooldown")
                     && !scriptEntry.hasObject("type"))
                 scriptEntry.addObject("type", Type.GLOBAL_COOLDOWN);
+
+            else if (arg.matches("saves") && !scriptEntry.hasObject("type"))
+                scriptEntry.addObject("type", Type.SAVES);
+
+            else if (arg.matchesArgumentType(dScript.class))
+                scriptEntry.addObject("script", arg.asType(dScript.class));
+
+            else if (arg.matchesArgumentList(dPlayer.class))
+                scriptEntry.addObject("players", ((dList) arg.asType(dList.class)).filter(dPlayer.class));
 
             else throw new InvalidArgumentsException("Unknown argument '" + arg + "'!");
         }
 
+        // Use attached player if none is specified, and we're not resetting GLOBAL_COOLDOWN
+        if (!scriptEntry.getObject("type").equals(Type.GLOBAL_COOLDOWN))
+            scriptEntry.defaultObject("players", scriptEntry.getPlayer());
+
+        // Must specify a script unless resetting SAVES
+        if (!scriptEntry.hasObject("script") && !scriptEntry.getObject("type").equals(Type.SAVES))
+            throw new InvalidArgumentsException("Must specify a script!");
     }
 
     @Override
     public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
 
-        Type type = (Type) scriptEntry.getObject("type");
+        // We allow players to be a single player or multiple players
+        dObject player = scriptEntry.getdObject("players");
+        dList players;
+        if (player instanceof dPlayer)
+            players = new dList(player.identify());
+        else players = (dList) scriptEntry.getdObject("players");
+
+        Element type = (Element) scriptEntry.getdObject("type");
         dScript script = (dScript) scriptEntry.getObject("script");
 
-        switch (type) {
-            case FAIL:
-                FailCommand.resetFails(scriptEntry.getPlayer().getName(), script.getName());
-                return;
+        dB.report(scriptEntry, getName(),
+                (players != null ? players.debug() : "")
+                        + type.debug()
+                        + (script != null ? script.debug() : ""));
 
-            case FINISH:
-                FinishCommand.resetFinishes(scriptEntry.getPlayer().getName(), script.getName());
-                return;
-
-            case PLAYER_COOLDOWN:
-                CooldownCommand.setCooldown(scriptEntry.getPlayer().getName(), Duration.ZERO, script.getName(), false);
-                return;
-
-            case GLOBAL_COOLDOWN:
-                CooldownCommand.setCooldown(scriptEntry.getPlayer().getName(), Duration.ZERO, script.getName(), true);
-                return;
+        // Deal with GLOBAL_COOLDOWN reset first, since there's no player/players involved
+        if (Type.valueOf(type.asString()) == Type.GLOBAL_COOLDOWN) {
+            CooldownCommand.setCooldown(null, Duration.ZERO, script.getName(), true);
+            return;
         }
 
+        // Now deal with the rest
+        for (String object : players) {
+            dPlayer resettable = dPlayer.valueOf(object);
+
+            switch (Type.valueOf(type.asString())) {
+                case FAIL:
+                    FailCommand.resetFails(resettable.getName(), script.getName());
+                    return;
+
+                case FINISH:
+                    FinishCommand.resetFinishes(resettable.getName(), script.getName());
+                    return;
+
+                case PLAYER_COOLDOWN:
+                    CooldownCommand.setCooldown(resettable.getName(), Duration.ZERO, script.getName(), false);
+                    return;
+
+                case SAVES:
+                    DenizenAPI.getCurrentInstance().getSaves().set("Players." + resettable.getName(), null);
+            }
+
+        }
     }
 
 }
