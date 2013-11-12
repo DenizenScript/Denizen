4385578c0616579607272aae05a8264aac1002ab
Jeremy Schroeder <jeremy@ssdbg.com>
2013-11-12 16:51:24 -0500

    Insert some API for the possibility of denizen auto-registering custom commands. (Still implementing...)

--- src/main/java/net/aufdemrand/denizen/events/EventManager.java
+++ src/main/java/net/aufdemrand/denizen/events/EventManager.java
@@ -14,10 +14,18 @@ import net.aufdemrand.denizen.scripts.queues.ScriptQueue;
 import net.aufdemrand.denizen.scripts.queues.core.InstantQueue;
 import net.aufdemrand.denizen.utilities.DenizenAPI;
 import net.aufdemrand.denizen.utilities.debugging.dB;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.SimplePluginManager;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -87,6 +95,56 @@ public class EventManager implements Listener {
 
         }
 
+        // Register any custom commands found with Bukkit
+        // TODO:
+
+    }
+
+    private Plugin plugin  = DenizenAPI.getCurrentInstance();
+
+    public void registerCommand(String... aliases) {
+        PluginCommand command = getCommand(aliases[0], plugin);
+
+        command.setAliases(Arrays.asList(aliases));
+        getCommandMap().register(plugin.getDescription().getName(), command);
+    }
+
+    private static PluginCommand getCommand(String name, Plugin plugin) {
+        PluginCommand command = null;
+
+        try {
+            Constructor<PluginCommand> c = PluginCommand.class.getDeclaredConstructor(String.class, Plugin.class);
+            c.setAccessible(true);
+
+            command = c.newInstance(name, plugin);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return command;
+    }
+
+    private static CommandMap getCommandMap() {
+        CommandMap commandMap = null;
+
+        try {
+            if (Bukkit.getPluginManager() instanceof SimplePluginManager) {
+                Field f = SimplePluginManager.class.getDeclaredField("commandMap");
+                f.setAccessible(true);
+
+                commandMap = (CommandMap) f.get(Bukkit.getPluginManager());
+            }
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return commandMap;
     }
 
 
