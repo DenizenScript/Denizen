c9b25a2edaec636665a19c5e6baf67d603672d75
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2013-11-26 19:37:27 -0800

    More debug information in dB submit
    Debug submit information will now begin with:
    CraftBukkit Version, all plugins with versions, all loaded worlds and
    all online players.

--- src/main/java/net/aufdemrand/denizen/utilities/debugging/DebugSubmit.java
+++ src/main/java/net/aufdemrand/denizen/utilities/debugging/DebugSubmit.java
@@ -1,7 +1,12 @@
 package net.aufdemrand.denizen.utilities.debugging;
 
+import net.aufdemrand.denizen.Denizen;
+import net.citizensnpcs.api.CitizensAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -22,10 +27,39 @@ public class DebugSubmit extends Thread {
             uc.setDoOutput(true);
             uc.setConnectTimeout(10000);
             uc.connect();
-            uc.getOutputStream().write(
-                ("postid=pastetext&pastetype=log"
+            String pluginlist = "";
+            int newlineLength = 0;
+            for (Plugin pl: Bukkit.getPluginManager().getPlugins()) {
+                String temp = ((char)0x01) + (pl.isEnabled() ? "2": "4") + pl.getName() + ": " + pl.getDescription().getVersion() + ", ";
+                pluginlist += temp;
+                newlineLength += temp.length();
+                if (newlineLength > 80) {
+                    newlineLength = 0;
+                    pluginlist += "\n";
+                }
+            }
+            String worldlist = "";
+            for (World w: Bukkit.getWorlds()) {
+                worldlist += w.getName() + ", ";
+            }
+            String playerlist = "";
+            newlineLength = 0;
+            for (Player pla: Bukkit.getOnlinePlayers()) {
+                String temp = pla.getDisplayName().replace(ChatColor.COLOR_CHAR, (char)0x01) + ((char)0x01) + "7(" + pla.getName() + "), ";
+                playerlist += temp;
+                newlineLength += temp.length();
+                if (newlineLength > 80) {
+                    newlineLength = 0;
+                    playerlist += "\n";
+                }
+            }
+            if (playerlist.length() < 2)
+                playerlist = "No Online Players, ";
+            uc.getOutputStream().write(("postid=pastetext&pastetype=log"
                         + "&response=micro&pastetitle=Denizen+Debug+Logs+From+" + URLEncoder.encode(Bukkit.getServer().getMotd().replace(ChatColor.COLOR_CHAR, (char) 0x01))
-                        + "&pastecontents=" + recording)
+                        + "&pastecontents=" + URLEncoder.encode("CraftBukkit Version: " + Bukkit.getServer().getVersion() + "\nActive Plugins: "
+                        + pluginlist.substring(0, pluginlist.length() - 2) + "\nLoaded Worlds: " + worldlist.substring(0, worldlist.length() - 2) + "\nOnline Players: "
+                        + playerlist.substring(0, playerlist.length() - 2) + "\n\n") + recording)
                         .getBytes("UTF-8"));
             in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
             Result = in.readLine();
