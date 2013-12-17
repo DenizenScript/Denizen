0c3d32762824a3b60df0cfd59fd824a560e167d2
MorphanOne <morphanone@gmail.com>
2013-12-17 00:23:06 -0500

    Automatically add plugins and execution jar in RuntimeCompiler
    For some reason, it won't let you actually compile any .java files.

--- src/main/java/net/aufdemrand/denizen/utilities/RuntimeCompiler.java
+++ src/main/java/net/aufdemrand/denizen/utilities/RuntimeCompiler.java
@@ -3,8 +3,10 @@ package net.aufdemrand.denizen.utilities;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import net.aufdemrand.denizen.Denizen;
@@ -23,6 +25,22 @@ public class RuntimeCompiler {
         this.denizen = denizen;
     }
 
+    private final static File pluginsFolder = new File("plugins");
+
+    private final static FilenameFilter jarFilter = new FilenameFilter() {
+        @Override
+        public boolean accept(File dir, String name) {
+            return (name.toLowerCase().endsWith(".jar"));
+        }
+    };
+
+    private final static FilenameFilter javaFilter = new FilenameFilter() {
+        @Override
+        public boolean accept(File dir, String name) {
+            return (name.toLowerCase().endsWith(".java"));
+        }
+    };
+
     @SuppressWarnings("unchecked")
     public void loader() {
 
@@ -30,51 +48,51 @@ public class RuntimeCompiler {
         dB.log("Loading external dependencies for run-time compiler.");
         try {
             File file = new File(denizen.getDataFolder() + File.separator + "externals" + File.separator + "dependencies");
-            File[] files = file.listFiles();
-            if (files.length > 0){
-                for (File f : files){
-                    String fileName = f.getName();
-                    if (fileName.substring(fileName.lastIndexOf('.') + 1).equalsIgnoreCase("JAR")) {
-                        dependencies.add(f.getPath());
-                        dB.log("Loaded  " + f.getName());
-                    }
-                }
+            for (File f : file.listFiles(jarFilter)){
+                dependencies.add(f.getPath());
+                dB.log("Loaded  " + f.getName());
             }
-        } catch (Exception error) { dB.log("No dependencies to load."); }
+        } catch (Exception error) { 
+            dB.log("No dependencies to load."); 
+        }
 
         try {
             File file = new File(denizen.getDataFolder() + File.separator + "externals");
-            File[] files = file.listFiles();
+            File[] files = file.listFiles(javaFilter);
             if (files.length > 0){
                 for (File f : files){
                     String fileName = f.getName();
 
-                    if (fileName.substring(fileName.lastIndexOf('.') + 1).equalsIgnoreCase("JAVA") && !fileName.startsWith(".")) {
-                        dB.log("Processing '" + fileName + "'... ");
+                    dB.log("Processing '" + fileName + "'... ");
 
-                        JavaSourceCompiler javaSourceCompiler = new JavaSourceCompilerImpl();
-                        JavaSourceCompiler.CompilationUnit compilationUnit = javaSourceCompiler.createCompilationUnit();
-                        if (!dependencies.isEmpty()) compilationUnit.addClassPathEntries(dependencies);
+                    JavaSourceCompiler javaSourceCompiler = new JavaSourceCompilerImpl();
+                    JavaSourceCompiler.CompilationUnit compilationUnit = javaSourceCompiler.createCompilationUnit();
+                    if (!dependencies.isEmpty()) compilationUnit.addClassPathEntries(dependencies);
+                    compilationUnit.addClassPathEntries(Arrays.asList(denizen.getDataFolder().list(jarFilter)));
+                    compilationUnit.addClassPathEntries(Arrays.asList(pluginsFolder.list(jarFilter)))
 
-                        try {
-                            compilationUnit.addJavaSource(fileName.replace(".java", ""), readFile(f.getAbsolutePath()));
-                            ClassLoader classLoader = javaSourceCompiler.compile(compilationUnit);
-                            Class<dExternal> load = (Class<dExternal>) classLoader.loadClass(fileName.replace(".java", ""));
-                            dExternal loadedClass = load.newInstance();
-                            loadedClass.load();
-                        } catch (Exception e) {
-                            if (e instanceof IllegalStateException)
-                                dB.echoError("No JDK found! External .java files will not be loaded.");
-                            else {
-                                dB.echoError(ChatColor.RED + "Woah! Error compiling " + fileName + "!");
-                                dB.echoError(e);
-                            }
+                    try {
+                        compilationUnit.addJavaSource(fileName.replace(".java", ""), readFile(f.getAbsolutePath()));
+                        ClassLoader classLoader = javaSourceCompiler.compile(compilationUnit);
+                        Class<dExternal> load = (Class<dExternal>) classLoader.loadClass(fileName.replace(".java", ""));
+                        dExternal loadedClass = load.newInstance();
+                        loadedClass.load();
+                    } catch (Exception e) {
+                        if (e instanceof IllegalStateException) {
+                            dB.echoError("No JDK found! External .java files will not be loaded.");
+                            dB.echoError(e);
+                        }
+                        else {
+                            dB.echoError(ChatColor.RED + "Woah! Error compiling " + fileName + "!");
+                            dB.echoError(e);
                         }
                     }
                 }
                 dB.echoApproval("All externals loaded!");
             } else dB.echoError("Woah! No externals in /plugins/Denizen/externals/.../ to load!");
-        } catch (Exception error) { /* No externals */ }
+        } catch (Exception error) {
+            dB.echoError(error);
+        }
     }
 
     private String readFile(String file) throws IOException {
