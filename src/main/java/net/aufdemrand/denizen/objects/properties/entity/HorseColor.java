a442493b365812e412681cf50a363e6cc5015cf4
NegroBob <meancraft@hotmail.com>
2014-01-15 15:44:30 -0800

    Renaming the class the actual right thing

--- src/main/java/net/aufdemrand/denizen/objects/properties/entity/HorseColor.java
+++ src/main/java/net/aufdemrand/denizen/objects/properties/entity/HorseColor.java
@@ -9,7 +9,7 @@ import net.aufdemrand.denizen.tags.Attribute;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
 
-public class HorseProperty implements Property {
+public class HorseColor implements Property {
 
 
     public static boolean describes(dObject entity) {
