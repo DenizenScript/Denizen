b9a319b8ad3275cd1f3a75128233049256052233
Mason Adkins <zappalives0@gmail.com>
2014-01-26 21:41:50 -0500

    Added <entity[<eid>]....> to EntityTags

--- src/main/java/net/aufdemrand/denizen/tags/core/EntityTags.java
+++ src/main/java/net/aufdemrand/denizen/tags/core/EntityTags.java
@@ -1,6 +1,10 @@
 package net.aufdemrand.denizen.tags.core;
 
 import net.aufdemrand.denizen.Denizen;
+import net.aufdemrand.denizen.events.bukkit.ReplaceableTagEvent;
+import net.aufdemrand.denizen.objects.dEntity;
+import net.aufdemrand.denizen.objects.dPlayer;
+import net.aufdemrand.denizen.tags.Attribute;
 import net.aufdemrand.denizen.utilities.DenizenAPI;
 import net.aufdemrand.denizen.utilities.debugging.dB;
 import org.bukkit.entity.Entity;
@@ -28,5 +32,40 @@ public class EntityTags implements Listener {
 
         // Write the entity's SpawnReason to it's metadata
         entity.setMetadata("spawnreason", new FixedMetadataValue(DenizenAPI.getCurrentInstance(), reason));
+    }
+
+    //////////
+    //  ReplaceableTagEvent handler
+    ////////
+
+    @EventHandler
+    public void playerTags(ReplaceableTagEvent event) {
+
+        if (!event.matches("entity, e") || event.replaced()) return;
+
+        // Build a new attribute out of the raw_tag supplied in the script to be fulfilled
+        Attribute attribute = new Attribute(event.raw_tag, event.getScriptEntry());
+
+        dEntity e = null;
+
+        // Entity tag may specify a new entity in the <entity[context]...> portion of the tag.
+        if (attribute.hasContext(1)) {
+            // Check if this is a valid player and update the dPlayer object reference.
+            if (attribute.getIntContext(1) >= 0)
+               e = dEntity.valueOf(attribute.getContext(1));
+            else {
+                dB.echoDebug(event.getScriptEntry(), "Could not match '"
+                        + attribute.getContext(1) + "' to a valid entity!");
+                return;
+            }
+        }
+
+        if (e == null || !e.isValid()) {
+            dB.echoDebug(event.getScriptEntry(), "Invalid or missing entity for tag <" + event.raw_tag + ">!");
+            event.setReplaced("null");
+            return;
+        }
+
+        event.setReplaced(e.getAttribute(attribute.fulfill(1)));
     }
 }
