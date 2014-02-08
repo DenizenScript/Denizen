6072e51537c47730ee5f56505dc3be02e36f62e9
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-02-07 17:24:40 -0800

    Add EntitySize property
    - spawn slime[size=1]

--- src/main/java/net/aufdemrand/denizen/objects/properties/PropertyParser.java
+++ src/main/java/net/aufdemrand/denizen/objects/properties/PropertyParser.java
@@ -37,6 +37,7 @@ public class PropertyParser {
         registerProperty(EntityPotion.class, dEntity.class);
         registerProperty(EntityPowered.class, dEntity.class);
         registerProperty(EntityProfession.class, dEntity.class);
+        registerProperty(EntitySize.class, dEntity.class);
         registerProperty(EntityTame.class, dEntity.class);
 
         // register core dInventory properties
