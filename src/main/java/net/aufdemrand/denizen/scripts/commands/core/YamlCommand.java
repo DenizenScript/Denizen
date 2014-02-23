709f7d5597b9e12ac1d872f9ddecc3376a76c804
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-02-22 16:03:39 -0800

    Add a FlagCommand style syntax to YAMLCommand
    Also 'yaml  unload'
    and also 'yaml[name].read[x].as_list.escaped'
    This update has been needed for quite a while!

--- src/main/java/net/aufdemrand/denizen/scripts/commands/core/YamlCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/core/YamlCommand.java
@@ -1,11 +1,14 @@
 package net.aufdemrand.denizen.scripts.commands.core;
 
+import com.sk89q.worldedit.util.YAMLConfiguration;
 import net.aufdemrand.denizen.events.bukkit.ReplaceableTagEvent;
 import net.aufdemrand.denizen.exceptions.CommandExecutionException;
 import net.aufdemrand.denizen.exceptions.InvalidArgumentsException;
+import net.aufdemrand.denizen.flags.FlagManager;
 import net.aufdemrand.denizen.scripts.ScriptEntry;
 import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
 import net.aufdemrand.denizen.tags.Attribute;
+import net.aufdemrand.denizen.tags.core.EscapeTags;
 import net.aufdemrand.denizen.utilities.DenizenAPI;
 import net.aufdemrand.denizen.objects.*;
 import net.aufdemrand.denizen.utilities.debugging.dB;
@@ -43,10 +46,12 @@ public class YamlCommand extends AbstractCommand implements Listener {
         return yamls.get(id.toUpperCase());
     }
 
-    enum Action{ LOAD, CREATE, READ, WRITE, SAVE }
+    enum Action{ LOAD, UNLOAD, CREATE, WRITE, SAVE, SET }
 
     @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
+
+        boolean isSet = false;
 
         for (aH.Argument arg : aH.interpret(scriptEntry.getArguments())) {
             if (!scriptEntry.hasObject("action") &&
@@ -64,6 +69,17 @@ public class YamlCommand extends AbstractCommand implements Listener {
             else if (!scriptEntry.hasObject("action") &&
                     arg.matches("CREATE")) {
                 scriptEntry.addObject("action", new Element("CREATE"));
+            }
+
+            else if (!scriptEntry.hasObject("action") &&
+                    arg.matches("SET")) {
+                scriptEntry.addObject("action", new Element("SET"));
+                isSet = true;
+            }
+
+            else if (!scriptEntry.hasObject("action") &&
+                    arg.matches("UNLOAD")) {
+                scriptEntry.addObject("action", new Element("UNLOAD"));
             }
 
             else if (!scriptEntry.hasObject("action") &&
@@ -88,6 +104,73 @@ public class YamlCommand extends AbstractCommand implements Listener {
             else if (!scriptEntry.hasObject("split") &&
                     arg.matches("split_list")) {
                 scriptEntry.addObject("split", Element.TRUE);
+            }
+
+            // Check for key:value/action
+            else if (isSet &&
+                    !scriptEntry.hasObject("value") &&
+                    arg.raw_value.split(":", 3).length == 2) {
+
+                String[] flagArgs = arg.raw_value.split(":", 2);
+                scriptEntry.addObject("key", new Element(flagArgs[0]));
+
+                if (flagArgs[1].equals("++") || flagArgs[1].equals("+")) {
+                    scriptEntry.addObject("flag_action", FlagManager.Action.INCREASE);
+                    scriptEntry.addObject("value", new Element(1));
+                }
+                else if (flagArgs[1].equals("--") || flagArgs[1].equals("-")) {
+                    scriptEntry.addObject("flag_action", FlagManager.Action.DECREASE);
+                    scriptEntry.addObject("value", new Element(1));
+                }
+                else if (flagArgs[1].equals("!")) {
+                    scriptEntry.addObject("flag_action", FlagManager.Action.DELETE);
+                    scriptEntry.addObject("value", Element.FALSE);
+                }
+                else if (flagArgs[1].equals("<-")) {
+                    scriptEntry.addObject("flag_action", FlagManager.Action.REMOVE);
+                    scriptEntry.addObject("value", Element.FALSE);
+                }
+                else {
+                    // No ACTION, we're just setting a value...
+                    scriptEntry.addObject("flag_action", FlagManager.Action.SET_VALUE);
+                    scriptEntry.addObject("value", new Element(flagArgs[1]));
+                }
+            }
+
+            // Check for key:action:value
+            else if (isSet &&
+                    !scriptEntry.hasObject("value") &&
+                    arg.raw_value.split(":", 3).length == 3) {
+                String[] flagArgs = arg.raw_value.split(":", 3);
+                scriptEntry.addObject("key", new Element(flagArgs[0]));
+
+                if (flagArgs[1].equals("->"))
+                    scriptEntry.addObject("flag_action", FlagManager.Action.INSERT);
+
+                else if (flagArgs[1].equals("<-"))
+                    scriptEntry.addObject("flag_action", FlagManager.Action.REMOVE);
+
+                else if (flagArgs[1].equals("||") || flagArgs[1].equals("|"))
+                    scriptEntry.addObject("flag_action", FlagManager.Action.SPLIT);
+
+                else if (flagArgs[1].equals("++") || flagArgs[1].equals("+"))
+                    scriptEntry.addObject("flag_action", FlagManager.Action.INCREASE);
+
+                else if (flagArgs[1].equals("--") || flagArgs[1].equals("-"))
+                    scriptEntry.addObject("flag_action", FlagManager.Action.DECREASE);
+
+                else if (flagArgs[1].equals("**") || flagArgs[1].equals("*"))
+                    scriptEntry.addObject("flag_action", FlagManager.Action.MULTIPLY);
+
+                else if (flagArgs[1].equals("//") || flagArgs[1].equals("/"))
+                    scriptEntry.addObject("flag_action", FlagManager.Action.DIVIDE);
+
+                else {
+                    scriptEntry.addObject("flag_action", FlagManager.Action.SET_VALUE);
+                    scriptEntry.addObject("value", new Element(arg.raw_value.split(":", 2)[1]));
+                    continue;
+                }
+                scriptEntry.addObject("value", new Element(flagArgs[2]));
             }
 
             else arg.reportUnhandled();
@@ -116,21 +199,24 @@ public class YamlCommand extends AbstractCommand implements Listener {
         Element key = scriptEntry.getElement("key");
         dObject value = scriptEntry.getdObject("value");
         Element split = scriptEntry.getElement("split");
-        Action action = Action.valueOf(scriptEntry.getElement("action").asString().toUpperCase());
-        String id = scriptEntry.getElement("id").asString();
+        FlagManager.Action flag_action = (FlagManager.Action) scriptEntry.getObject("flag_action");
+        Element actionelement = scriptEntry.getElement("action");
+        Element idelement = scriptEntry.getElement("id");
 
         YamlConfiguration yamlConfiguration;
 
         dB.report(scriptEntry, getName(),
-                aH.debugObj("action", action)
-                        + aH.debugObj("id", id)
+                        idelement.debug()
+                        + actionelement.debug()
                         + (filename != null ? filename.debug() : "")
+                        + (flag_action != null ? aH.debugObj("flag_action", flag_action.name()): "")
                         + (key != null ? key.debug() : "")
                         + (value != null ? value.debug() : "")
                         + (split != null ? split.debug() : ""));
 
         // Do action
-        id = id.toUpperCase();
+        Action action = Action.valueOf(actionelement.asString().toUpperCase());
+        String id = idelement.asString().toUpperCase();
 
         switch (action) {
 
@@ -141,8 +227,17 @@ public class YamlCommand extends AbstractCommand implements Listener {
                     return;
                 }
                 yamlConfiguration = YamlConfiguration.loadConfiguration(file);
+                if (yamls.containsKey(id))
+                    yamls.remove(id);
                 if (yamlConfiguration != null)
                     yamls.put(id, yamlConfiguration);
+                break;
+
+            case UNLOAD:
+                if (yamls.containsKey(id))
+                    yamls.remove(id);
+                else
+                    dB.echoError("Unknown YAML ID '" + id + "'");
                 break;
 
             case SAVE:
@@ -153,6 +248,8 @@ public class YamlCommand extends AbstractCommand implements Listener {
                         dB.echoError(e);
                     }
                 }
+                else
+                    dB.echoError("Unknown YAML ID '" + id + "'");
                 break;
 
             case WRITE:
@@ -164,6 +261,81 @@ public class YamlCommand extends AbstractCommand implements Listener {
                     else
                         yamls.get(id).set(key.asString(), value.identify());
                 }
+                else
+                    dB.echoError("Unknown YAML ID '" + id + "'");
+                break;
+
+            case SET:
+                if (yamls.containsKey(id)) {
+                    if (flag_action == null || key == null || value == null) {
+                        dB.echoError("Must specify a flag action and value!");
+                        return;
+                    }
+                    YamlConfiguration yaml = yamls.get(id);
+
+                    int index = -1;
+                    if (key.asString().contains("[")) {
+                        try {
+                            index = Integer.valueOf(key.asString().split("\\[")[1].replace("]", "")) - 1;
+                        } catch (Exception e) { index = -1; }
+                        key = Element.valueOf(key.asString().split("\\[")[0]);
+                    }
+
+                    String keystr = key.asString();
+                    String valuestr = value.identify();
+
+                    switch (flag_action) {
+                        case INCREASE:
+                            Set(yaml, index, keystr, String.valueOf(aH.getIntegerFrom(yaml.getString(keystr, "0")) + aH.getIntegerFrom(valuestr)));
+                            break;
+                        case DECREASE:
+                            Set(yaml, index, keystr, String.valueOf(aH.getIntegerFrom(yaml.getString(keystr, "0")) - aH.getIntegerFrom(valuestr)));
+                            break;
+                        case MULTIPLY:
+                            Set(yaml, index, keystr, String.valueOf(aH.getIntegerFrom(yaml.getString(keystr, "1")) * aH.getIntegerFrom(valuestr)));
+                            break;
+                        case DIVIDE:
+                            Set(yaml, index, keystr, String.valueOf(aH.getIntegerFrom(yaml.getString(keystr, "1")) / aH.getIntegerFrom(valuestr)));
+                            break;
+                        case DELETE:
+                            yaml.set(keystr, null);
+                            break;
+                        case SET_BOOLEAN:
+                            Set(yaml, index, keystr, valuestr);
+                            break;
+                        case SET_VALUE:
+                            Set(yaml, index, keystr, valuestr);
+                            break;
+                        case INSERT:
+                        {
+                            List<String> list = yaml.getStringList(keystr);
+                            list.add(valuestr);
+                            yaml.set(keystr, list);
+                            break;
+                        }
+                        case REMOVE:
+                        {
+                            List<String> list = yaml.getStringList(keystr);
+                            for (int i = 0; i < list.size(); i++) {
+                                if (list.get(i).equalsIgnoreCase(valuestr)) {
+                                    list.remove(i);
+                                    break;
+                                }
+                            }
+                            yaml.set(keystr, list);
+                            break;
+                        }
+                        case SPLIT:
+                        {
+                            List<String> list = yaml.getStringList(keystr);
+                            list.addAll(dList.valueOf(valuestr));
+                            yaml.set(keystr, list);
+                            break;
+                        }
+                    }
+                }
+                else
+                    dB.echoError("Unknown YAML ID '" + id + "'");
                 break;
 
             case CREATE:
@@ -174,6 +346,20 @@ public class YamlCommand extends AbstractCommand implements Listener {
 
     }
 
+    public void Set(YamlConfiguration yaml, int index, String key, String value) {
+        if (index == -1) {
+            yaml.set(key, value);
+        }
+        else {
+            List<String> list = yaml.getStringList(key);
+            if (index < 0)
+                index = 0;
+            if (index > list.size())
+                index = list.size() -1;
+            list.set(index,value);
+            yaml.set(key, list);
+        }
+    }
 
     @EventHandler
     public void yaml(ReplaceableTagEvent event) {
@@ -236,13 +422,28 @@ public class YamlCommand extends AbstractCommand implements Listener {
             // Returns the values of the key at the path as a dList.
             // -->
             if (attribute.startsWith("as_list")) {
+                attribute.fulfill(1);
                 List<String> list = getYaml(id).getStringList(path);
                 if (list == null) {
                     dB.echoDebug(event.getScriptEntry(), "YAML tag '" + event.raw_tag + "' has returned null.");
                     event.setReplaced(new Element("null").getAttribute(attribute));
                     return;
                 }
-                event.setReplaced(new dList(list).getAttribute(attribute.fulfill(1)));
+                // <--[tag]
+                // @attribute <yaml[<id>].read[<path>].as_list.escaped>
+                // @returns dList
+                // @description
+                // Returns the values of the key at the path as a pre-escaped dList.
+                // -->
+                if (attribute.startsWith("escaped")) {
+                    dList dlist = new dList();
+                    for (String str: list) {
+                        dlist.add(EscapeTags.Escape(str));
+                    }
+                    event.setReplaced(dlist.getAttribute(attribute.fulfill(1)));
+                }
+                else
+                    event.setReplaced(new dList(list).getAttribute(attribute));
                 return;
             }
 
