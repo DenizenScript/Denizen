0b94840086140281aa214966244bd4e74e5bf2b9
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-05-03 00:28:26 -0700

    Add new ACTION command, to match EVENT
    A command to run NPC actions! Full meta, mostly!
    Also patch an event command bug.
    Also patch some meta.

--- src/main/java/net/aufdemrand/denizen/scripts/commands/npc/ActionCommand.java
+++ src/main/java/net/aufdemrand/denizen/scripts/commands/npc/ActionCommand.java
@@ -1,4 +1,4 @@
-package net.aufdemrand.denizen.scripts.commands.core;
+package net.aufdemrand.denizen.scripts.commands.npc;
 
 import net.aufdemrand.denizen.events.EventManager;
 import net.aufdemrand.denizen.exceptions.CommandExecutionException;
@@ -8,31 +8,45 @@ import net.aufdemrand.denizen.scripts.ScriptEntry;
 import net.aufdemrand.denizen.scripts.commands.AbstractCommand;
 import net.aufdemrand.denizen.utilities.debugging.dB;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class EventCommand extends AbstractCommand {
+public class ActionCommand extends AbstractCommand {
 
     @Override
     public void parseArgs(ScriptEntry scriptEntry) throws InvalidArgumentsException {
 
         for (aH.Argument arg : aH.interpret(scriptEntry.getArguments())) {
 
-            if (!scriptEntry.hasObject("context")
+            if (!scriptEntry.hasObject("npcs")
+                && arg.matchesArgumentList(dNPC.class)) {
+                scriptEntry.addObject("npcs", arg.asType(dList.class).filter(dNPC.class));
+            }
+
+            else if (!scriptEntry.hasObject("context")
                     && arg.matchesPrefix("context", "c")) {
                 scriptEntry.addObject("context", arg.asType(dList.class));
             }
 
-            else if (!scriptEntry.hasObject("events")) {
-                scriptEntry.addObject("events", arg.asType(dList.class));
+            else if (!scriptEntry.hasObject("actions")) {
+                scriptEntry.addObject("actions", arg.asType(dList.class));
             }
 
             else
                 arg.reportUnhandled();
         }
 
-        if (!scriptEntry.hasObject("events"))
-            throw new InvalidArgumentsException("Must specify a list of event names!");
+        if (!scriptEntry.hasObject("actions"))
+            throw new InvalidArgumentsException("Must specify a list of action names!");
+
+        if (!scriptEntry.hasObject("npcs")) {
+            if (scriptEntry.hasNPC())
+                scriptEntry.addObject("npcs", Arrays.asList(scriptEntry.getNPC()));
+            else
+                throw new InvalidArgumentsException("Must specify an NPC to use!");
+        }
 
         scriptEntry.defaultObject("context", new dList());
 
@@ -42,10 +56,11 @@ public class EventCommand extends AbstractCommand {
     @Override
     public void execute(ScriptEntry scriptEntry) throws CommandExecutionException {
 
-        dList events = (dList) scriptEntry.getObject("events");
+        dList actions = (dList) scriptEntry.getObject("actions");
         dList context = (dList) scriptEntry.getObject("context");
+        List<dNPC> npcs = (List<dNPC>) scriptEntry.getObject("npcs");
 
-        dB.report(scriptEntry, getName(), events.debug() + context.debug());
+        dB.report(scriptEntry, getName(), actions.debug() + context.debug() + aH.debugList("npcs", npcs));
 
         if (context.size() % 2 == 1) { // Size is uneven!
             context.add("null");
@@ -57,8 +72,11 @@ public class EventCommand extends AbstractCommand {
             context_map.put(context.get(i), ObjectFetcher.pickObjectFor(context.get(i + 1)));
         }
 
-        String Determination = EventManager.doEvents(events, scriptEntry.getNPC(), scriptEntry.getPlayer(), context_map, true);
-        scriptEntry.addObject("determination", new Element(Determination));
+        for (dNPC npc: npcs) {
+            for (String action: actions) {
+                npc.action(action, scriptEntry.getPlayer(), context_map);
+            }
+        }
     }
 
 }
