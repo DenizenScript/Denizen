805976f5f5e45311c064ce0e5ea731ed75b76e11
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-07-12 15:00:42 -0700

    Deprecate inregion requirement with a warning
    Part of the WorldGuard link removal

--- src/main/java/net/aufdemrand/denizen/scripts/requirements/core/WorldGuardRegionRequirement.java
+++ src/main/java/net/aufdemrand/denizen/scripts/requirements/core/WorldGuardRegionRequirement.java
@@ -6,7 +6,6 @@ import net.aufdemrand.denizen.scripts.requirements.RequirementsContext;
 import net.aufdemrand.denizen.objects.aH;
 import net.aufdemrand.denizen.objects.aH.ArgumentType;
 import net.aufdemrand.denizen.utilities.debugging.dB;
-import net.aufdemrand.denizen.utilities.depends.WorldGuardUtilities;
 
 import java.util.List;
 
@@ -31,39 +30,9 @@ public class WorldGuardRegionRequirement extends AbstractRequirement {
     @Override
     public boolean check(RequirementsContext context, List<String> args) throws RequirementCheckException {
 
-        /*
-         * Instalize variables
-         */
-        String region = null;
-        boolean outcome = false;
-
-        /*
-         * If there are no arguments, throw an exception.
-         */
-        if (args == null || args.size() < 1)
-            throw new RequirementCheckException("Must provide a NAME:regionname!");
-
-        /*
-         * Parse through the given arguments
-         */
-        for (String arg : args) {
-            if (aH.matchesValueArg("NAME, N", arg, ArgumentType.String)) {
-                region = aH.getStringFrom(arg);
-                dB.echoDebug(context.getScriptContainer(), "...region set as: " + region);
-            } else throw new RequirementCheckException("Invalid argument specified!");
-        }
-
-        /*
-         * Check if player is in the given region.
-         */
-        outcome = WorldGuardUtilities.inRegion(context.getPlayer().getPlayerEntity().getLocation(), region);
-
-        /*
-         * Display proper debug output
-         */
-        if (outcome) dB.echoDebug(context.getScriptContainer(), "...player in region!");
-        else dB.echoDebug(context.getScriptContainer(), "...player is not in region!");
-
-        return outcome;
+        // TODO: Remove entirely.
+        dB.echoError("Requirements are deprecated, and Denizen no longer directly links WorldGuard." +
+                "Please use Depenizen and the tag l@location.in_region in an if command.");
+        return false;
     }
 }
