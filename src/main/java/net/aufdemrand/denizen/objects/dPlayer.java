fef565021a05c59ff10f4284770ade4b12375ef1
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2014-07-26 14:18:53 -0700

    Add mechanism player.show_workbench
    Inventory code is iffy... this is a temporary workbench handler until
    @Morphan1 makes it work nicely with the inventory system. This probably
    is solid enough to stand permanently on its own though.

--- src/main/java/net/aufdemrand/denizen/objects/dPlayer.java
+++ src/main/java/net/aufdemrand/denizen/objects/dPlayer.java
@@ -1755,6 +1755,20 @@ public class dPlayer implements dObject, Adjustable {
 
         // <--[mechanism]
         // @object dPlayer
+        // @name show_workbench
+        // @input dLocation
+        // @description
+        // Shows the player a workbench GUI corresponding to a given location.
+        // @tags
+        // <player.name.display>
+        // -->
+        if (mechanism.matches("show_workbench") && mechanism.requireObject(dLocation.class)) {
+            getPlayerEntity().openWorkbench(mechanism.getValue().asType(dLocation.class), true);
+            return;
+        }
+
+        // <--[mechanism]
+        // @object dPlayer
         // @name time
         // @input Element(Number)
         // @description
