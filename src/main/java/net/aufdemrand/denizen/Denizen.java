3647753878cc1a224abdf7356997516f31f604b0
Talamar1 <me@danielchester.com>
2015-06-11 08:27:32 -0400

    Rewrote "on item moves in inventory" event in new ScriptEvent format.

--- src/main/java/net/aufdemrand/denizen/Denizen.java
+++ src/main/java/net/aufdemrand/denizen/Denizen.java
@@ -572,7 +572,6 @@ public class Denizen extends JavaPlugin implements DenizenImplementation {
             OldEventManager.registerSmartEvent(new EntityInteractSmartEvent());
             OldEventManager.registerSmartEvent(new EntitySpawnSmartEvent());
             OldEventManager.registerSmartEvent(new FlagSmartEvent());
-            OldEventManager.registerSmartEvent(new ItemMoveSmartEvent());
             OldEventManager.registerSmartEvent(new ItemScrollSmartEvent());
             OldEventManager.registerSmartEvent(new ListPingSmartEvent());
             OldEventManager.registerSmartEvent(new NPCNavigationSmartEvent());
@@ -598,6 +597,7 @@ public class Denizen extends JavaPlugin implements DenizenImplementation {
             ScriptEvent.registerScriptEvent(new EntityDespawnScriptEvent());
             ScriptEvent.registerScriptEvent(new EntityKilledScriptEvent());
             ScriptEvent.registerScriptEvent(new EntityTeleportScriptEvent());
+            ScriptEvent.registerScriptEvent(new ItemMoveScriptEvent());
             ScriptEvent.registerScriptEvent(new LiquidSpreadScriptEvent());
             ScriptEvent.registerScriptEvent(new ResourcePackStatusScriptEvent());
             ScriptEvent.registerScriptEvent(new VehicleMoveScriptEvent());
