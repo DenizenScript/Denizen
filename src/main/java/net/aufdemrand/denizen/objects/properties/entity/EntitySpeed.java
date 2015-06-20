98a68948e1b72c840b7d3b1c650c0903033b54b9
Morphan1 <morphanone@gmail.com>
2015-06-20 06:50:53 -0400

    Allow changing the max speed of boats and minecarts

--- src/main/java/net/aufdemrand/denizen/objects/properties/entity/EntitySpeed.java
+++ src/main/java/net/aufdemrand/denizen/objects/properties/entity/EntitySpeed.java
@@ -7,12 +7,30 @@ import net.aufdemrand.denizencore.objects.Mechanism;
 import net.aufdemrand.denizencore.objects.dObject;
 import net.aufdemrand.denizencore.objects.properties.Property;
 import net.aufdemrand.denizencore.tags.Attribute;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Minecart;
 
 public class EntitySpeed implements Property {
 
     public static boolean describes(dObject entity) {
-        return entity instanceof dEntity
-                && ((dEntity) entity).isLivingEntity();
+        if (!(entity instanceof dEntity))
+            return false;
+        dEntity ent = (dEntity) entity;
+        if (ent.isLivingEntity()) {
+            return true;
+        }
+        else {
+            EntityType entityType = ent.getBukkitEntityType();
+            return entityType == EntityType.BOAT
+                    || entityType == EntityType.MINECART
+                    || entityType == EntityType.MINECART_CHEST
+                    || entityType == EntityType.MINECART_COMMAND
+                    || entityType == EntityType.MINECART_FURNACE
+                    || entityType == EntityType.MINECART_HOPPER
+                    || entityType == EntityType.MINECART_MOB_SPAWNER
+                    || entityType == EntityType.MINECART_TNT;
+        }
     }
 
     public static EntitySpeed getFrom(dObject entity) {
@@ -38,12 +56,34 @@ public class EntitySpeed implements Property {
 
     @Override
     public String getPropertyString() {
-        return String.valueOf(EntityMovement.getSpeed(entity.getBukkitEntity()));
+        return getSpeed().asString();
     }
 
     @Override
     public String getPropertyId() {
         return "speed";
+    }
+
+    public Element getSpeed() {
+        if (entity.isLivingEntity()) {
+            return new Element(EntityMovement.getSpeed(entity.getBukkitEntity()));
+        }
+        else {
+            EntityType entityType = entity.getBukkitEntityType();
+            if (entityType == EntityType.BOAT) {
+                return new Element(((Boat) entity.getBukkitEntity()).getMaxSpeed());
+            }
+            else if (entityType == EntityType.MINECART
+                    || entityType == EntityType.MINECART_CHEST
+                    || entityType == EntityType.MINECART_COMMAND
+                    || entityType == EntityType.MINECART_FURNACE
+                    || entityType == EntityType.MINECART_HOPPER
+                    || entityType == EntityType.MINECART_MOB_SPAWNER
+                    || entityType == EntityType.MINECART_TNT) {
+                return new Element(((Minecart) entity.getBukkitEntity()).getMaxSpeed());
+            }
+        }
+        return new Element(0.0);
     }
 
 
@@ -62,11 +102,10 @@ public class EntitySpeed implements Property {
         // @mechanism dEntity.speed
         // @group attributes
         // @description
-        // Returns the living entity's current speed when walking.
+        // Returns how fast the entity can move.
         // -->
         if (attribute.startsWith("speed"))
-            return new Element(EntityMovement.getSpeed(entity.getBukkitEntity()))
-                    .getAttribute(attribute.fulfill(1));
+            return getSpeed().getAttribute(attribute.fulfill(1));
 
 
         return null;
@@ -80,12 +119,30 @@ public class EntitySpeed implements Property {
         // @name speed
         // @input Element(Decimal)
         // @description
-        // Sets how fast the entity walks.
+        // Sets how fast the entity can move.
         // @tags
         // <e@entity.speed>
         // -->
         if (mechanism.matches("speed") && mechanism.requireDouble()) {
-            EntityMovement.setSpeed(entity.getBukkitEntity(), mechanism.getValue().asDouble());
+            double value = mechanism.getValue().asDouble();
+            if (entity.isLivingEntity()) {
+                EntityMovement.setSpeed(entity.getBukkitEntity(), value);
+            }
+            else {
+                EntityType entityType = entity.getBukkitEntityType();
+                if (entityType == EntityType.BOAT) {
+                    ((Boat) entity.getBukkitEntity()).setMaxSpeed(value);
+                }
+                else if (entityType == EntityType.MINECART
+                        || entityType == EntityType.MINECART_CHEST
+                        || entityType == EntityType.MINECART_COMMAND
+                        || entityType == EntityType.MINECART_FURNACE
+                        || entityType == EntityType.MINECART_HOPPER
+                        || entityType == EntityType.MINECART_MOB_SPAWNER
+                        || entityType == EntityType.MINECART_TNT) {
+                    ((Minecart) entity.getBukkitEntity()).setMaxSpeed(value);
+                }
+            }
         }
 
     }
