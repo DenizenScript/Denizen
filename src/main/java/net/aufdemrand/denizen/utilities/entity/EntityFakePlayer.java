890bdc712da181cc7c185b8f9f74df75611698ea
Morphan1 <morphanone@gmail.com>
2015-08-15 01:02:34 -0400

    Fix spawning fake_players
    Not sure when this happened or how but... Meh

--- src/main/java/net/aufdemrand/denizen/utilities/entity/EntityFakePlayer.java
+++ src/main/java/net/aufdemrand/denizen/utilities/entity/EntityFakePlayer.java
@@ -12,12 +12,12 @@ public class EntityFakePlayer extends EntityPlayer {
 
     public EntityFakePlayer(MinecraftServer minecraftserver, WorldServer worldserver, GameProfile gameprofile, PlayerInteractManager playerinteractmanager) {
         super(minecraftserver, worldserver, gameprofile, playerinteractmanager);
+        this.bukkitEntity = new CraftFakePlayer((CraftServer) Bukkit.getServer(), this);
         playerinteractmanager.setGameMode(WorldSettings.EnumGamemode.SURVIVAL);
         NetworkManager networkManager = new FakeNetworkManager(EnumProtocolDirection.CLIENTBOUND);
         playerConnection = new FakePlayerConnection(minecraftserver, networkManager, this);
         networkManager.a(playerConnection);
         datawatcher.watch(10, (byte) 127);
-        this.bukkitEntity = new CraftFakePlayer((CraftServer) Bukkit.getServer(), this);
         worldserver.addEntity(this, CreatureSpawnEvent.SpawnReason.CUSTOM);
     }
 
