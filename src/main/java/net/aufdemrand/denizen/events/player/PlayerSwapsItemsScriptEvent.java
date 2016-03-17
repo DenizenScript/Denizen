c5cf481e1cbada858ffe6efa77e64a317b0be471
Morphan1 <morphanone@gmail.com>
2016-03-17 09:26:30 -0400

    Implement new Spigot PlayerSwapHandItemsEvent

--- src/main/java/net/aufdemrand/denizen/events/player/PlayerSwapsItemsScriptEvent.java
+++ src/main/java/net/aufdemrand/denizen/events/player/PlayerSwapsItemsScriptEvent.java
@@ -2,12 +2,19 @@ package net.aufdemrand.denizen.events.player;
 
 import net.aufdemrand.denizen.BukkitScriptEntryData;
 import net.aufdemrand.denizen.events.BukkitScriptEvent;
+import net.aufdemrand.denizen.objects.dItem;
 import net.aufdemrand.denizen.objects.dPlayer;
+import net.aufdemrand.denizen.utilities.DenizenAPI;
+import net.aufdemrand.denizencore.objects.dObject;
 import net.aufdemrand.denizencore.scripts.ScriptEntryData;
 import net.aufdemrand.denizencore.scripts.containers.ScriptContainer;
 import net.aufdemrand.denizencore.utilities.CoreUtilities;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
-public class PlayerSwapsItemsScriptEvent extends BukkitScriptEvent {
+public class PlayerSwapsItemsScriptEvent extends BukkitScriptEvent implements Listener {
 
     // <--[event]
     // @Events
@@ -15,11 +22,17 @@ public class PlayerSwapsItemsScriptEvent extends BukkitScriptEvent {
     //
     // @Regex ^on player swaps items$
     //
-    // @Warning This event is a prototype and may change drastically in the future.
-    //
     // @Cancellable true
     //
-    // @Triggers when a player swaps the items in their main and offhand.
+    // @Triggers when a player swaps the items in their main and off hands.
+    //
+    // @Context
+    // <context.main> returns the dItem switched to the main hand.
+    // <context.offhand> returns the dItem switched to the off hand.
+    //
+    // @Determine
+    // "MAIN:" + dItem to set the item in the main hand.
+    // "OFFHAND:" + dItem to set the item in the off hand.
     //
     // -->
 
@@ -29,6 +42,9 @@ public class PlayerSwapsItemsScriptEvent extends BukkitScriptEvent {
 
     public static PlayerSwapsItemsScriptEvent instance;
     public dPlayer player;
+    public dItem mainhand;
+    public dItem offhand;
+    public PlayerSwapHandItemsEvent event;
 
     @Override
     public boolean couldMatch(ScriptContainer scriptContainer, String s) {
@@ -46,7 +62,52 @@ public class PlayerSwapsItemsScriptEvent extends BukkitScriptEvent {
     }
 
     @Override
+    public void init() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, DenizenAPI.getCurrentInstance());
+    }
+
+    @Override
+    public void destroy() {
+        PlayerSwapHandItemsEvent.getHandlerList().unregister(this);
+    }
+
+    @Override
+    public boolean applyDetermination(ScriptContainer container, String determination) {
+        String lower = CoreUtilities.toLowerCase(determination);
+        if (lower.startsWith("main:")) {
+            event.setMainHandItem(dItem.valueOf(determination.substring("main:".length())).getItemStack());
+            return true;
+        }
+        else if (lower.startsWith("offhand:")) {
+            event.setOffHandItem(dItem.valueOf(determination.substring("offhand:".length())).getItemStack());
+            return true;
+        }
+        return super.applyDetermination(container, determination);
+    }
+
+    @Override
     public ScriptEntryData getScriptEntryData() {
         return new BukkitScriptEntryData(player, null);
+    }
+
+    @Override
+    public dObject getContext(String name) {
+        if (name.equals("main")) {
+            return mainhand;
+        }
+        else if (name.equals("offhand")) {
+            return offhand;
+        }
+        return super.getContext(name);
+    }
+
+    @EventHandler
+    public void playerSwapsItems(PlayerSwapHandItemsEvent event) {
+        player = dPlayer.mirrorBukkitPlayer(event.getPlayer());
+        mainhand = new dItem(event.getMainHandItem());
+        offhand = new dItem(event.getOffHandItem());
+        this.event = event;
+        fire();
+        event.setCancelled(cancelled);
     }
 }
