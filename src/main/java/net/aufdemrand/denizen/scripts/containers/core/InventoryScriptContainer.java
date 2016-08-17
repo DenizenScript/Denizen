41f257808327a4bff7c6d5a3776795ae0628c744
Morphan1 <morphanone@gmail.com>
2016-08-16 23:18:56 -0400

    Fix inventory script identification in special cases when title is set

--- src/main/java/net/aufdemrand/denizen/scripts/containers/core/InventoryScriptContainer.java
+++ src/main/java/net/aufdemrand/denizen/scripts/containers/core/InventoryScriptContainer.java
@@ -104,10 +104,10 @@ public class InventoryScriptContainer extends ScriptContainer {
             if (contains("INVENTORY")) {
                 if (InventoryType.valueOf(getString("INVENTORY").toUpperCase()) != null) {
                     inventory = new dInventory(InventoryType.valueOf(getString("INVENTORY").toUpperCase()));
-                    inventory.setIdentifiers("script", getName());
                     if (contains("TITLE")) {
                         inventory.setTitle(TagManager.tag(getString("TITLE"), context));
                     }
+                    inventory.setIdentifiers("script", getName());
                 }
                 else {
                     dB.echoError("Invalid inventory type specified. Assuming \"CHEST\"");
