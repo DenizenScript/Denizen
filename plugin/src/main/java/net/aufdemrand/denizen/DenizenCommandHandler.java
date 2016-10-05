9f0b2d1379417533ea754e5d3e6a8ba7e7e4753b
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-10-04 21:11:33 -0700

    morphan1 exists

--- plugin/src/main/java/net/aufdemrand/denizen/DenizenCommandHandler.java
+++ plugin/src/main/java/net/aufdemrand/denizen/DenizenCommandHandler.java
@@ -270,7 +270,7 @@ public class DenizenCommandHandler {
     public void version(CommandContext args, CommandSender sender) throws CommandException {
         Messaging.sendInfo(sender, "<2>DENIZEN<7>: scriptable Minecraft!"); // TODO: "It's Scriptable!"?
         Messaging.send(sender, "");
-        Messaging.send(sender, "<7>by: <f>aufdemrand and mcmonkey, with help from many skilled contributors!");
+        Messaging.send(sender, "<7>by: <f>mcmonkey and Morphan1, originally by aufdemrand, and with help from many skilled contributors!");
         Messaging.send(sender, "<7>version: <f>" + Denizen.versionTag + "<7>, core version: <f>" + DenizenCore.VERSION);
     }
 
