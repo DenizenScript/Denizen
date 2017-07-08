74184b15d8cd8acaa32355a2f145b468a7824043
BloodEko <1sicherheit@gmx.de>
2017-07-08 08:38:39 +0200

    editing describtion
    Editing documentation.

--- plugin/src/main/java/net/aufdemrand/denizen/scripts/commands/BukkitCommandRegistry.java
+++ plugin/src/main/java/net/aufdemrand/denizen/scripts/commands/BukkitCommandRegistry.java
@@ -3779,7 +3779,7 @@ public class BukkitCommandRegistry extends CommandRegistry {
 
         // <--[command]
         // @Name Take
-        // @Syntax take [money/iteminhand/scriptname:<scriptname>/bydisplay:<name>/bycover:<title>|<author>/slot:<#>/<item>|...] (quantity:<#>) (from:<inventory>)
+        // @Syntax take [money/iteminhand/scriptname:<name>/bydisplay:<name>/bycover:<title>|<author>/slot:<#>/<item>|...] (quantity:<#>) (from:<inventory>)
         // @Required 1
         // @Stable stable
         // @Short Takes an item from the player.
