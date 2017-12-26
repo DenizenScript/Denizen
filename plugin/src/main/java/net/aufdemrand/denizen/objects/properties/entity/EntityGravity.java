c1a602eef496dee17d6bd3f93c5462d5a6986565
Mergu <jhopkins@vt.edu>
2017-12-26 00:11:49 -0500

    extend gravity mec/tag to all entities post-1.9

--- plugin/src/main/java/net/aufdemrand/denizen/objects/properties/entity/EntityGravity.java
+++ plugin/src/main/java/net/aufdemrand/denizen/objects/properties/entity/EntityGravity.java
@@ -1,5 +1,7 @@
 package net.aufdemrand.denizen.objects.properties.entity;
 
+import net.aufdemrand.denizen.nms.NMSHandler;
+import net.aufdemrand.denizen.nms.NMSVersion;
 import net.aufdemrand.denizen.objects.dEntity;
 import net.aufdemrand.denizencore.objects.Element;
 import net.aufdemrand.denizencore.objects.Mechanism;
@@ -7,12 +9,14 @@ import net.aufdemrand.denizencore.objects.dObject;
 import net.aufdemrand.denizencore.objects.properties.Property;
 import net.aufdemrand.denizencore.tags.Attribute;
 import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.EntityType;
 
 public class EntityGravity implements Property {
 
     public static boolean describes(dObject entity) {
-        return entity instanceof dEntity && ((dEntity) entity).getBukkitEntityType() == EntityType.ARMOR_STAND;
+        if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_10_R1)) {
+            return entity instanceof dEntity;
+        }
+        return entity instanceof dEntity && ((dEntity) entity).getBukkitEntity() instanceof ArmorStand;
     }
 
     public static EntityGravity getFrom(dObject entity) {
@@ -41,6 +45,15 @@ public class EntityGravity implements Property {
 
     @Override
     public String getPropertyString() {
+        if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_10_R1)) {
+            if (dentity.getBukkitEntity().hasGravity()) {
+                return null;
+            }
+            else {
+                return "false";
+            }
+        }
+
         if (((ArmorStand) dentity.getBukkitEntity()).hasGravity()) {
             return null;
         }
@@ -71,9 +84,13 @@ public class EntityGravity implements Property {
         // @mechanism dEntity.gravity
         // @group properties
         // @description
-        // If the entity is an armor stand, returns whether the armor stand has gravity.
+        // Returns whether the entity has gravity.
         // -->
         if (attribute.startsWith("gravity")) {
+            if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_10_R1)) {
+                return new Element(dentity.getBukkitEntity().hasGravity())
+                        .getAttribute(attribute.fulfill(1));
+            }
             return new Element(((ArmorStand) dentity.getBukkitEntity()).hasGravity())
                     .getAttribute(attribute.fulfill(1));
         }
@@ -89,13 +106,18 @@ public class EntityGravity implements Property {
         // @name gravity
         // @input Element(Boolean)
         // @description
-        // Changes the gravity state of an armor stand.
+        // Changes the gravity state of an entity.
         // @tags
         // <e@entity.gravity>
         // -->
 
         if (mechanism.matches("gravity") && mechanism.requireBoolean()) {
-            ((ArmorStand) dentity.getBukkitEntity()).setGravity(mechanism.getValue().asBoolean());
+            if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_10_R1)) {
+                dentity.getBukkitEntity().setGravity(mechanism.getValue().asBoolean());
+            }
+            else {
+                ((ArmorStand) dentity.getBukkitEntity()).setGravity(mechanism.getValue().asBoolean());
+            }
         }
     }
 }
