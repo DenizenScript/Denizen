d87ea022c935ee8ca2c1855b68a42fbec4e7695c
mcmonkey4eva <agoodwin@freneticllc.com>
2018-03-23 05:24:05 -0700

    Add a verbose debug tool for error messages

--- plugin/src/main/java/net/aufdemrand/denizen/utilities/debugging/dB.java
+++ plugin/src/main/java/net/aufdemrand/denizen/utilities/debugging/dB.java
@@ -263,6 +263,14 @@ public class dB {
         ConsoleSender.sendMessage(ChatColor.LIGHT_PURPLE + " " + ChatColor.RED + "ERROR" +
                 (script != null ? " in script '" + script.getName() + "'" : "") + "! "
                 + ChatColor.WHITE + trimMessage(message));
+        if (net.aufdemrand.denizencore.utilities.debugging.dB.verbose) {
+            try {
+                throw new RuntimeException("Verbose info for above error");
+            }
+            catch (Throwable e) {
+                echoError(source, e);
+            }
+        }
     }
 
     private static boolean ThrowErrorEvent = true;
