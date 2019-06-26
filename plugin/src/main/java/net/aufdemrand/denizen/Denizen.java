0243eb6115f0edfb636d631c1dc909fd69a5c88b
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-06-25 21:12:51 -0700

    move traits to a trait registry

--- plugin/src/main/java/net/aufdemrand/denizen/Denizen.java
+++ plugin/src/main/java/net/aufdemrand/denizen/Denizen.java
@@ -11,6 +11,7 @@ import net.aufdemrand.denizen.nms.NMSHandler;
 import net.aufdemrand.denizen.nms.interfaces.FakeArrow;
 import net.aufdemrand.denizen.nms.interfaces.FakePlayer;
 import net.aufdemrand.denizen.nms.interfaces.ItemProjectile;
+import net.aufdemrand.denizen.npc.TraitRegistry;
 import net.aufdemrand.denizen.npc.dNPCRegistry;
 import net.aufdemrand.denizen.npc.speech.DenizenChat;
 import net.aufdemrand.denizen.npc.traits.*;
@@ -316,24 +317,7 @@ public class Denizen extends JavaPlugin {
         try {
             if (Depends.citizens != null) {
                 // Register traits
-                // TODO: should this be a separate function?
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(TriggerTrait.class).withName("triggers"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(PushableTrait.class).withName("pushable"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(AssignmentTrait.class).withName("assignment"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(NicknameTrait.class).withName("nickname"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(HealthTrait.class).withName("health"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(ConstantsTrait.class).withName("constants"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(HungerTrait.class).withName("hunger"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(SittingTrait.class).withName("sitting"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(FishingTrait.class).withName("fishing"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(SleepingTrait.class).withName("sleeping"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(SneakingTrait.class).withName("sneaking"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(InvisibleTrait.class).withName("invisible"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(MobproxTrait.class).withName("mobprox"));
-                CitizensAPI.getTraitFactory().registerTrait(TraitInfo.create(MirrorTrait.class).withName("mirror"));
-
-                // Register Speech AI
-                CitizensAPI.getSpeechFactory().register(DenizenChat.class, "denizen_chat");
+                TraitRegistry.registerMainTraits();
             }
         }
         catch (Exception e) {
