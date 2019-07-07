7ffd67967fc4b7a65652178283ca2224ec89b50c
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-07-07 01:37:20 -0700

    add explicit 'on npc spawns' script event

--- plugin/src/main/java/net/aufdemrand/denizen/events/ScriptEventRegistry.java
+++ plugin/src/main/java/net/aufdemrand/denizen/events/ScriptEventRegistry.java
@@ -6,6 +6,7 @@ import net.aufdemrand.denizen.events.player.*;
 import net.aufdemrand.denizen.events.world.*;
 import net.aufdemrand.denizen.nms.NMSHandler;
 import net.aufdemrand.denizen.nms.NMSVersion;
+import net.aufdemrand.denizen.utilities.depends.Depends;
 import net.aufdemrand.denizencore.events.ScriptEvent;
 
 public class ScriptEventRegistry {
@@ -66,6 +67,9 @@ public class ScriptEventRegistry {
         ScriptEvent.registerScriptEvent(new ItemEnchantedScriptEvent());
         ScriptEvent.registerScriptEvent(new ItemMergesScriptEvent());
         ScriptEvent.registerScriptEvent(new ItemSpawnsScriptEvent());
+        if (Depends.citizens != null) {
+            ScriptEvent.registerScriptEvent(new NPCSpawnScriptEvent());
+        }
         ScriptEvent.registerScriptEvent(new PigZappedScriptEvent());
         ScriptEvent.registerScriptEvent(new ProjectileHitsScriptEvent());
         ScriptEvent.registerScriptEvent(new ProjectileLaunchedScriptEvent());
---
+++ plugin/src/main/java/net/aufdemrand/denizen/events/entity/NPCSpawnScriptEvent.java
@@ -0,0 +1,88 @@
+package net.aufdemrand.denizen.events.entity;
+
+import net.aufdemrand.denizen.BukkitScriptEntryData;
+import net.aufdemrand.denizen.events.BukkitScriptEvent;
+import net.aufdemrand.denizen.objects.dLocation;
+import net.aufdemrand.denizen.objects.dNPC;
+import net.aufdemrand.denizencore.objects.dObject;
+import net.aufdemrand.denizencore.scripts.ScriptEntryData;
+import net.aufdemrand.denizencore.scripts.containers.ScriptContainer;
+import net.aufdemrand.denizencore.utilities.CoreUtilities;
+import net.citizensnpcs.api.event.NPCSpawnEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class NPCSpawnScriptEvent extends BukkitScriptEvent implements Listener {
+
+    // <--[event]
+    // @Events
+    // npc spawns
+    //
+    // @Regex ^on npc spawns$
+    // @Switch in <area>
+    //
+    // @Cancellable true
+    //
+    // @Triggers when an NPC spawns.
+    //
+    // @Context
+    // <context.location> returns the location the entity will spawn at.
+    //
+    // -->
+
+    public NPCSpawnScriptEvent() {
+        instance = this;
+    }
+
+    public static NPCSpawnScriptEvent instance;
+    public dNPC npc;
+    public dLocation location;
+    public NPCSpawnEvent event;
+
+    @Override
+    public boolean couldMatch(ScriptContainer scriptContainer, String s) {
+        String lower = CoreUtilities.toLowerCase(s);
+        return lower.startsWith("npc spawns");
+    }
+
+    @Override
+    public boolean matches(ScriptPath path) {
+        if (!runInCheck(path, location)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "NPCSpawn";
+    }
+
+    @Override
+    public boolean applyDetermination(ScriptContainer container, String determination) {
+        return super.applyDetermination(container, determination);
+    }
+
+    @Override
+    public ScriptEntryData getScriptEntryData() {
+        return new BukkitScriptEntryData(null, npc);
+    }
+
+    @Override
+    public dObject getContext(String name) {
+        if (name.equals("location")) {
+            return location;
+        }
+        return super.getContext(name);
+    }
+
+    @EventHandler
+    public void onNPCSpawn(NPCSpawnEvent event) {
+        this.npc = new dNPC(event.getNPC());
+        location = new dLocation(event.getLocation());
+        this.event = event;
+        fire(event);
+    }
+
+}
