d5e369c28ff333ad6d25a5a8317d23b837a2e4aa
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-07-10 17:38:43 -0700

    listen to npc teleporting for teleport script event

--- plugin/src/main/java/net/aufdemrand/denizen/events/entity/EntityTeleportScriptEvent.java
+++ plugin/src/main/java/net/aufdemrand/denizen/events/entity/EntityTeleportScriptEvent.java
@@ -158,9 +158,6 @@ public class EntityTeleportScriptEvent extends BukkitScriptEvent implements List
 
     @EventHandler
     public void onPlayerTeleports(PlayerTeleportEvent event) {
-        if (dEntity.isNPC(event.getPlayer())) {
-            return;
-        }
         from = new dLocation(event.getFrom());
         to = new dLocation(event.getTo());
         entity = new dEntity(event.getPlayer());
