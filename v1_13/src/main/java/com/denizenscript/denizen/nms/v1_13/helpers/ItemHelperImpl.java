1409bc09124b75193e808f3a490cc3c6835d5998
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-09-15 13:16:38 -0700

    fix exact-match requirement for 1.13 furnace recipes

--- v1_13/src/main/java/com/denizenscript/denizen/nms/v1_13/helpers/ItemHelperImpl.java
+++ v1_13/src/main/java/com/denizenscript/denizen/nms/v1_13/helpers/ItemHelperImpl.java
@@ -40,6 +40,7 @@ public class ItemHelperImpl extends ItemHelper {
     public void registerFurnaceRecipe(String keyName, ItemStack result, ItemStack ingredient, float exp, int time, String type) {
         MinecraftKey key = new MinecraftKey("denizen", "furnace_recipe_" + keyName);
         RecipeItemStack itemRecipe = new RecipeItemStack(Arrays.asList(new RecipeItemStack.StackProvider(CraftItemStack.asNMSCopy(ingredient))).stream());
+        itemRecipe.exact = true;
         FurnaceRecipe recipe = new FurnaceRecipe(key, "custom", itemRecipe, CraftItemStack.asNMSCopy(result), exp, time);
         ((CraftServer) Bukkit.getServer()).getServer().getCraftingManager().a(recipe);
     }
