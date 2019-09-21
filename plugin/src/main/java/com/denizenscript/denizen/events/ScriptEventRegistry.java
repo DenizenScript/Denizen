faa4aaccdaf3fa97b2aef77dba779036476d5478
CounterCrysis <47706960+CounterCrysis@users.noreply.github.com>
2019-09-21 05:18:16 -0700

    Add Exp Bottle Breaks Event (#2044)
    * Exp Bottle Breaks
    
    * PR Comments
    
    * PR Comments

--- plugin/src/main/java/com/denizenscript/denizen/events/ScriptEventRegistry.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/ScriptEventRegistry.java
@@ -102,6 +102,7 @@ public class ScriptEventRegistry {
         // Player events
         ScriptEvent.registerScriptEvent(new BiomeEnterExitScriptEvent());
         ScriptEvent.registerScriptEvent(new ChatScriptEvent());
+        ScriptEvent.registerScriptEvent(new ExperienceBottleBreaksScriptEvent());
         ScriptEvent.registerScriptEvent(new ItemRecipeFormedScriptEvent());
         ScriptEvent.registerScriptEvent(new ItemScrollScriptEvent());
         ScriptEvent.registerScriptEvent(new ListPingScriptEvent());
