c640e6e480d11adaac943b3bfd40db44884ab7fb
Xeane <Ph4i1ur3@yahoo.com>
2019-11-30 16:53:18 -0500

    Waterlogged mech/tag (#2083)
    * waterlogged tag/mech added
    
    * death to the extra space!

---
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialWaterlogged.java
@@ -0,0 +1,87 @@
+package com.denizenscript.denizen.objects.properties.material;
+
+import com.denizenscript.denizen.objects.MaterialTag;
+import com.denizenscript.denizencore.objects.Mechanism;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.properties.Property;
+import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import org.bukkit.block.data.Waterlogged;
+
+public class MaterialWaterlogged implements Property {
+
+    public static boolean describes(ObjectTag material) {
+        return material instanceof MaterialTag
+                && ((MaterialTag) material).hasModernData()
+                && ((MaterialTag) material).getModernData().data instanceof Waterlogged;
+    }
+
+    public static MaterialWaterlogged getFrom(ObjectTag _material) {
+        if (!describes(_material)) {
+            return null;
+        }
+        else {
+            return new MaterialWaterlogged((MaterialTag) _material);
+        }
+    }
+
+    public static final String[] handledMechs = new String[] {
+            "waterlogged"
+    };
+
+    private MaterialWaterlogged(MaterialTag _material) {
+        material = _material;
+    }
+
+    MaterialTag material;
+
+    public static void registerTags() {
+
+        // <--[tag]
+        // @attribute <MaterialTag.waterlogged>
+        // @returns ElementTag(Boolean)
+        // @mechanism MaterialTag.waterlogged
+        // @group properties
+        // @description
+        // Returns whether this block is waterlogged or not.
+        // -->
+        PropertyParser.<MaterialWaterlogged>registerTag("waterlogged", (attribute, material) -> {
+            return new ElementTag(material.isWaterlogged());
+        });
+    }
+
+    public Waterlogged getWaterlogged() {
+        return (Waterlogged) material.getModernData().data;
+    }
+
+    public boolean isWaterlogged() {
+        return getWaterlogged().isWaterlogged();
+    }
+
+    @Override
+    public String getPropertyString() {
+        return String.valueOf(isWaterlogged());
+    }
+
+    @Override
+    public String getPropertyId() {
+        return "waterlogged";
+    }
+
+    @Override
+    public void adjust(Mechanism mechanism) {
+
+        // <--[mechanism]
+        // @object MaterialTag
+        // @name waterlogged
+        // @input ElementTag(Boolean)
+        // @description
+        // Sets this block to be waterlogged, or not.
+        // @tags
+        // <MaterialTag.waterlogged>
+        // -->
+        if (mechanism.matches("waterlogged") && mechanism.requireBoolean()) {
+            getWaterlogged().setWaterlogged(mechanism.getValue().asBoolean());
+        }
+    }
+}
