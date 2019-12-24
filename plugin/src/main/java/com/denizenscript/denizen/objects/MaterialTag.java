78c43d7df809ce097fe810711a40787540d48588
Ph4i1ur3 <Ph4i1ur3@yahoo.com>
2019-12-24 10:32:15 -0500

    Fixed alphabetical placement of `MaterialTag.is_campfire` from previous commit.

--- plugin/src/main/java/com/denizenscript/denizen/objects/MaterialTag.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/MaterialTag.java
@@ -500,6 +500,19 @@ public class MaterialTag implements ObjectTag, Adjustable {
         });
 
         // <--[tag]
+        // @attribute <MaterialTag.is_campfire>
+        // @returns ElementTag(Boolean)
+        // @group properties
+        // @description
+        // Returns whether the material is a campfire material.
+        // When this returns true, <@link tag MaterialTag.signal_fire>,
+        // and <@link mechanism MaterialTag.signal_fire> are accessible.
+        // -->
+        registerTag("is_campfire", (attribute, object) -> {
+            return new ElementTag(MaterialCampfire.describes(object));
+        });
+
+        // <--[tag]
         // @attribute <MaterialTag.is_directional>
         // @returns ElementTag(Boolean)
         // @group properties
@@ -549,19 +562,6 @@ public class MaterialTag implements ObjectTag, Adjustable {
         // -->
         registerTag("is_bamboo", (attribute, object) -> {
             return new ElementTag(MaterialLeafSize.describes(object));
-        });
-
-        // <--[tag]
-        // @attribute <MaterialTag.is_campfire>
-        // @returns ElementTag(Boolean)
-        // @group properties
-        // @description
-        // Returns whether the material is a campfire material.
-        // When this returns true, <@link tag MaterialTag.signal_fire>,
-        // and <@link mechanism MaterialTag.signal_fire> are accessible.
-        // -->
-        registerTag("is_campfire", (attribute, object) -> {
-            return new ElementTag(MaterialCampfire.describes(object));
         });
 
         // <--[tag]
