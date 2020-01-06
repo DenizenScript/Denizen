51f639d506cea69c36f27433417cd62914937501
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-01-05 16:22:36 -0800

    remove the 'elder' and 'skeleton' historical properties

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/PropertyRegistry.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/PropertyRegistry.java
@@ -52,7 +52,6 @@ public class PropertyRegistry {
         PropertyParser.registerProperty(EntityCustomName.class, EntityTag.class);
         PropertyParser.registerProperty(EntityDisabledSlots.class, EntityTag.class);
         PropertyParser.registerProperty(EntityPotionEffects.class, EntityTag.class);
-        PropertyParser.registerProperty(EntityElder.class, EntityTag.class);
         PropertyParser.registerProperty(EntityEquipment.class, EntityTag.class);
         PropertyParser.registerProperty(EntityExplosionFire.class, EntityTag.class);
         PropertyParser.registerProperty(EntityExplosionRadius.class, EntityTag.class);
@@ -83,7 +82,6 @@ public class PropertyRegistry {
         PropertyParser.registerProperty(EntitySilent.class, EntityTag.class);
         PropertyParser.registerProperty(EntitySitting.class, EntityTag.class);
         PropertyParser.registerProperty(EntitySize.class, EntityTag.class);
-        PropertyParser.registerProperty(EntitySkeleton.class, EntityTag.class);
         PropertyParser.registerProperty(EntitySpeed.class, EntityTag.class);
         PropertyParser.registerProperty(EntitySpell.class, EntityTag.class);
         PropertyParser.registerProperty(EntityTame.class, EntityTag.class);
