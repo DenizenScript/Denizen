f3ac3ef69fd4b270c235d358f2c50dc8b234d8fc
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-04-21 03:33:02 -0700

    fix inventory conversion typo

--- plugin/src/main/java/com/denizenscript/denizen/utilities/Conversion.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/Conversion.java
@@ -98,7 +98,7 @@ public class Conversion {
         else if (arg.matchesArgumentList(ItemTag.class)) {
             List<ItemTag> list = ListTag.valueOf(string, scriptEntry.getContext()).filter(ItemTag.class, scriptEntry);
             ItemStack[] items = convertItems(list).toArray(new ItemStack[list.size()]);
-            InventoryTag inventory = new InventoryTag(Math.max(InventoryTag.maxSlots, (items.length / 9) * 9 + 9));
+            InventoryTag inventory = new InventoryTag(Math.min(InventoryTag.maxSlots, (items.length / 9) * 9 + 9));
             inventory.setContents(items);
             return new AbstractMap.SimpleEntry<>(items.length, inventory);
         }
