c0b13c021b49ff6b0fab9359e4cce93f0b07f166
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-04-24 16:46:13 -0700

    deprecate inventory add/remove, related cleaning

--- plugin/src/main/java/com/denizenscript/denizen/scripts/commands/item/InventoryCommand.java
+++ plugin/src/main/java/com/denizenscript/denizen/scripts/commands/item/InventoryCommand.java
@@ -15,6 +15,7 @@ import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.core.ListTag;
 import com.denizenscript.denizencore.scripts.ScriptEntry;
 import com.denizenscript.denizencore.scripts.commands.AbstractCommand;
+import com.denizenscript.denizencore.utilities.Deprecations;
 
 import java.util.AbstractMap;
 import java.util.List;
@@ -23,7 +24,7 @@ public class InventoryCommand extends AbstractCommand {
 
     public InventoryCommand() {
         setName("inventory");
-        setSyntax("inventory [open/close/copy/move/swap/add/remove/set/keep/exclude/fill/clear/update/adjust <mechanism>:<value>] (destination:<inventory>) (origin:<inventory>/<item>|...) (slot:<slot>)");
+        setSyntax("inventory [open/close/copy/move/swap/set/keep/exclude/fill/clear/update/adjust <mechanism>:<value>] (destination:<inventory>) (origin:<inventory>/<item>|...) (slot:<slot>)");
         setRequiredArguments(1, 6);
     }
 
@@ -35,11 +36,11 @@ public class InventoryCommand extends AbstractCommand {
     // be used for a wide range of purposes - from looting fallen enemies to serving as interactive menus with item
     // 'buttons'.
     //
-    // In Denizen, all Notable dInventories (saved by the Note command) are automatically converted into a
+    // In Denizen, all noted inventories (saved by the Note command) are automatically converted into a
     // virtual copy of the saved inventory. This enables you to open and edit the items inside freely, with automatic
     // saving, as if it were a normal inventory.
     //
-    // Notables are not the only way to create virtual inventories, however. Using in@generic along with inventory
+    // Noting is not the only way to create virtual inventories, however. Using 'generic' along with inventory
     // properties will allow you to create temporary custom inventories to do with as you please. The properties that
     // can be used like this are:
     //
@@ -61,7 +62,7 @@ public class InventoryCommand extends AbstractCommand {
 
     // <--[command]
     // @Name Inventory
-    // @Syntax inventory [open/close/copy/move/swap/add/remove/set/keep/exclude/fill/clear/update/adjust <mechanism>:<value>] (destination:<inventory>) (origin:<inventory>/<item>|...) (slot:<slot>)
+    // @Syntax inventory [open/close/copy/move/swap/set/keep/exclude/fill/clear/update/adjust <mechanism>:<value>] (destination:<inventory>) (origin:<inventory>/<item>|...) (slot:<slot>)
     // @Required 1
     // @Maximum 6
     // @Short Edits the inventory of a player, NPC, or chest.
@@ -77,8 +78,8 @@ public class InventoryCommand extends AbstractCommand {
     // destination to be a valid player inventory.
     // Using "close" closes any inventory that the currently attached player has opened.
     //
-    // Note that instead of "add", you should usually use <@link command give>,
-    // and instead of "remove", you should usually use <@link command take>.
+    // Note that to add items to an inventory, you should usually use <@link command give>,
+    // and to remove items from an inventory, you should usually use <@link command take>.
     //
     // @Tags
     // <PlayerTag.inventory>
@@ -288,6 +289,7 @@ public class InventoryCommand extends AbstractCommand {
 
                 // Add origin's contents to destination
                 case ADD:
+                    Deprecations.oldInventoryCommands.warn(scriptEntry);
                     if (origin == null) {
                         Debug.echoError(scriptEntry.getResidingQueue(), "Missing origin argument!");
                         return;
@@ -297,6 +299,7 @@ public class InventoryCommand extends AbstractCommand {
 
                 // Remove origin's contents from destination
                 case REMOVE:
+                    Deprecations.oldInventoryCommands.warn(scriptEntry);
                     if (origin == null) {
                         Debug.echoError(scriptEntry.getResidingQueue(), "Missing origin argument!");
                         return;
