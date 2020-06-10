a2a8d4a5d37aab28703d3bba8d385f62bfaddad8
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-09 19:49:13 -0700

    assignment command: to list argument

--- plugin/src/main/java/com/denizenscript/denizen/scripts/commands/npc/AssignmentCommand.java
+++ plugin/src/main/java/com/denizenscript/denizen/scripts/commands/npc/AssignmentCommand.java
@@ -1,29 +1,34 @@
 package com.denizenscript.denizen.scripts.commands.npc;
 
+import com.denizenscript.denizen.objects.NPCTag;
 import com.denizenscript.denizen.utilities.Utilities;
 import com.denizenscript.denizen.utilities.debugging.Debug;
 import com.denizenscript.denizen.npc.traits.AssignmentTrait;
 import com.denizenscript.denizencore.exceptions.InvalidArgumentsException;
 import com.denizenscript.denizencore.objects.Argument;
 import com.denizenscript.denizencore.objects.ArgumentHelper;
+import com.denizenscript.denizencore.objects.core.ListTag;
 import com.denizenscript.denizencore.objects.core.ScriptTag;
 import com.denizenscript.denizencore.scripts.ScriptEntry;
 import com.denizenscript.denizencore.scripts.commands.AbstractCommand;
+
+import java.util.Collections;
+import java.util.List;
 
 public class AssignmentCommand extends AbstractCommand {
 
     public AssignmentCommand() {
         setName("assignment");
-        setSyntax("assignment [set/remove] (script:<name>)");
-        setRequiredArguments(1, 2);
+        setSyntax("assignment [set/remove] (script:<name>) (to:<npc>|...)");
+        setRequiredArguments(1, 3);
         isProcedural = false;
     }
 
     // <--[command]
     // @Name Assignment
-    // @Syntax assignment [set/remove] (script:<name>)
+    // @Syntax assignment [set/remove] (script:<name>) (to:<npc>|...)
     // @Required 1
-    // @Maximum 2
+    // @Maximum 3
     // @Plugin Citizens
     // @Short Changes an NPC's assignment.
     // @Group npc
@@ -34,7 +39,7 @@ public class AssignmentCommand extends AbstractCommand {
     //
     // Uses the script: argument, which accepts an assignment-type script.
     //
-    // For this command to work an npc must be attached to the script queue or an npc specified with npc:NPCTag.
+    // Optionally, specify a list of NPCs to apply the trait to. If unspecified, the linked NPC will be used.
     //
     // @Tags
     // <NPCTag.script>
@@ -76,19 +81,25 @@ public class AssignmentCommand extends AbstractCommand {
                     throw new InvalidArgumentsException("Script specified is not an 'assignment-type' container.");
                 }
             }
+            else if (!scriptEntry.hasObject("npcs")
+                    && arg.matchesArgumentList(NPCTag.class)) {
+                scriptEntry.addObject("npcs", arg.asType(ListTag.class).filter(NPCTag.class, scriptEntry));
+            }
             else {
                 arg.reportUnhandled();
             }
         }
 
-        if (!Utilities.entryHasNPC(scriptEntry)) {
-            throw new InvalidArgumentsException("NPC linked was missing or invalid.");
-        }
 
+        if (!scriptEntry.hasObject("npcs")) {
+            if (!Utilities.entryHasNPC(scriptEntry)) {
+                throw new InvalidArgumentsException("This command requires a linked NPC!");
+            }
+            scriptEntry.addObject("npcs", Collections.singletonList(Utilities.getEntryNPC(scriptEntry)));
+        }
         if (!scriptEntry.hasObject("action")) {
             throw new InvalidArgumentsException("Must specify an action!");
         }
-
         if (scriptEntry.getObject("action").equals(Action.SET) && !scriptEntry.hasObject("script")) {
             throw new InvalidArgumentsException("Script specified was missing or invalid.");
         }
@@ -99,19 +110,23 @@ public class AssignmentCommand extends AbstractCommand {
     public void execute(ScriptEntry scriptEntry) {
 
         ScriptTag script = scriptEntry.getObjectTag("script");
+        Action action = (Action) scriptEntry.getObject("action");
+        List<NPCTag> npcs = (List<NPCTag>) scriptEntry.getObject("npcs");
 
         if (scriptEntry.dbCallShouldDebug()) {
-            Debug.report(scriptEntry, getName(), ArgumentHelper.debugObj("action", scriptEntry.getObject("action")) + (script != null ? script.debug() : ""));
+            Debug.report(scriptEntry, getName(),
+                    ArgumentHelper.debugObj("action", action)
+                    + (script != null ? script.debug() : "")
+                    + ArgumentHelper.debugList("npc", npcs));
         }
 
-        // Perform desired action
-        if (scriptEntry.getObject("action").equals(Action.SET)) {
-            Utilities.getEntryNPC(scriptEntry).getCitizen().getTrait(AssignmentTrait.class)
-                    .setAssignment(script.getName(), Utilities.getEntryPlayer(scriptEntry));
-        }
-        else if (scriptEntry.getObject("action").equals(Action.REMOVE)) {
-            Utilities.getEntryNPC(scriptEntry).getCitizen().getTrait(AssignmentTrait.class)
-                    .removeAssignment(Utilities.getEntryPlayer(scriptEntry));
+        for (NPCTag npc : npcs) {
+            if (action.equals(Action.SET)) {
+                npc.getCitizen().getTrait(AssignmentTrait.class).setAssignment(script.getName(), Utilities.getEntryPlayer(scriptEntry));
+            }
+            else if (action.equals(Action.REMOVE)) {
+                npc.getCitizen().getTrait(AssignmentTrait.class).removeAssignment(Utilities.getEntryPlayer(scriptEntry));
+            }
         }
     }
 }
