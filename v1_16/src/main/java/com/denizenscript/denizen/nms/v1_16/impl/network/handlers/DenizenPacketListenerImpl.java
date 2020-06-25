4f9d04b38471f829fd80530184f7dcb5f7152db4
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-06-24 18:56:58 -0700

    initial v1_16 submodule as duplicate of v1_15

---
+++ v1_16/src/main/java/com/denizenscript/denizen/nms/v1_16/impl/network/handlers/DenizenPacketListenerImpl.java
@@ -0,0 +1,97 @@
+package com.denizenscript.denizen.nms.v1_16.impl.network.handlers;
+
+import com.denizenscript.denizen.nms.v1_16.impl.network.packets.PacketInResourcePackStatusImpl;
+import com.denizenscript.denizen.nms.v1_16.impl.network.packets.PacketInSteerVehicleImpl;
+import com.denizenscript.denizen.utilities.packets.DenizenPacketHandler;
+import com.denizenscript.denizencore.utilities.debugging.Debug;
+import io.netty.util.concurrent.Future;
+import io.netty.util.concurrent.GenericFutureListener;
+import com.denizenscript.denizen.nms.NMSHandler;
+import net.minecraft.server.v1_16_R1.*;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import javax.annotation.Nullable;
+import java.nio.charset.StandardCharsets;
+
+public class DenizenPacketListenerImpl extends AbstractListenerPlayInImpl {
+
+    private static DenizenPacketHandler packetHandler;
+
+    public String brand = "unknown";
+
+    public DenizenPacketListenerImpl(NetworkManager networkManager, EntityPlayer entityPlayer) {
+        super(networkManager, entityPlayer, entityPlayer.playerConnection);
+    }
+
+    public static void enable(DenizenPacketHandler handler) {
+        packetHandler = handler;
+        Bukkit.getServer().getPluginManager().registerEvents(new PlayerEventListener(), NMSHandler.getJavaPlugin());
+    }
+
+    @Override
+    public void a(final PacketPlayInSteerVehicle packet) {
+        if (!packetHandler.receivePacket(player.getBukkitEntity(), new PacketInSteerVehicleImpl(packet))) {
+            super.a(packet);
+        }
+    }
+
+    @Override
+    public void a(PacketPlayInResourcePackStatus packet) {
+        packetHandler.receivePacket(player.getBukkitEntity(), new PacketInResourcePackStatusImpl(packet));
+        super.a(packet);
+    }
+
+    @Override
+    public void a(PacketPlayInBlockPlace packet) {
+        packetHandler.receivePlacePacket(player.getBukkitEntity());
+        super.a(packet);
+    }
+
+    @Override
+    public void a(PacketPlayInBlockDig packet) {
+        packetHandler.receiveDigPacket(player.getBukkitEntity());
+        super.a(packet);
+    }
+
+    @Override
+    public void a(PacketPlayInCustomPayload packet) {
+        if (NMSHandler.debugPackets) {
+            Debug.log("Custom packet payload: " + packet.tag.toString() + " sent from " + player.getName());
+        }
+        if (packet.tag.getNamespace().equals("minecraft") && packet.tag.getKey().equals("brand")) {
+            int i = packet.data.i(); // read off the varInt of length to get rid of it
+            brand = StandardCharsets.UTF_8.decode(packet.data.nioBuffer()).toString();
+        }
+        super.a(packet);
+    }
+
+    @Override
+    public void a(PacketPlayInFlying packet) {
+        if (DenizenPacketHandler.forceNoclip.contains(player.getUniqueID())) {
+            player.noclip = true;
+        }
+        super.a(packet);
+    }
+
+    // For compatibility with other plugins using Reflection weirdly...
+    @Override
+    public void sendPacket(Packet packet) {
+        super.sendPacket(packet);
+    }
+
+    @Override
+    public void a(Packet<?> packet, @Nullable GenericFutureListener<? extends Future<? super Void>> genericfuturelistener) {
+        super.a(packet, genericfuturelistener);
+    }
+
+    public static class PlayerEventListener implements Listener {
+        @EventHandler(priority = EventPriority.LOWEST)
+        public void onPlayerJoin(PlayerJoinEvent event) {
+            DenizenNetworkManagerImpl.setNetworkManager(event.getPlayer(), packetHandler);
+        }
+    }
+}
---
+++ v1_16/src/main/java/com/denizenscript/denizen/nms/v1_16/impl/network/packets/PacketInSteerVehicleImpl.java
@@ -0,0 +1,33 @@
+package com.denizenscript.denizen.nms.v1_16.impl.network.packets;
+
+import com.denizenscript.denizen.nms.interfaces.packets.PacketInSteerVehicle;
+import net.minecraft.server.v1_16_R1.PacketPlayInSteerVehicle;
+
+public class PacketInSteerVehicleImpl implements PacketInSteerVehicle {
+
+    private PacketPlayInSteerVehicle internal;
+
+    public PacketInSteerVehicleImpl(PacketPlayInSteerVehicle internal) {
+        this.internal = internal;
+    }
+
+    @Override
+    public float getLeftwardInput() {
+        return internal.b();
+    }
+
+    @Override
+    public float getForwardInput() {
+        return internal.c();
+    }
+
+    @Override
+    public boolean getJumpInput() {
+        return internal.d();
+    }
+
+    @Override
+    public boolean getDismountInput() {
+        return internal.e();
+    }
+}
