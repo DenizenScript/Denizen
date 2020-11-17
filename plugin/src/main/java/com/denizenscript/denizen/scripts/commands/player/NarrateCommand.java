d79ed44c80ca69ede0daf1800774ba9a1c99d10d
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2020-11-17 10:58:06 -0800

    add from:uuid arg to narrate command

--- plugin/src/main/java/com/denizenscript/denizen/scripts/commands/player/NarrateCommand.java
+++ plugin/src/main/java/com/denizenscript/denizen/scripts/commands/player/NarrateCommand.java
@@ -16,26 +16,29 @@ import com.denizenscript.denizencore.scripts.ScriptEntry;
 import com.denizenscript.denizencore.scripts.ScriptRegistry;
 import com.denizenscript.denizencore.scripts.commands.AbstractCommand;
 import com.denizenscript.denizencore.tags.TagManager;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.Bukkit;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class NarrateCommand extends AbstractCommand {
 
     public NarrateCommand() {
         setName("narrate");
-        setSyntax("narrate [<text>] (targets:<player>|...) (format:<script>) (per_player)");
-        setRequiredArguments(1, 4);
+        setSyntax("narrate [<text>] (targets:<player>|...) (format:<script>) (per_player) (from:<uuid>)");
+        setRequiredArguments(1, 5);
         setParseArgs(false);
         isProcedural = true;
     }
 
     // <--[command]
     // @Name Narrate
-    // @Syntax narrate [<text>] (targets:<player>|...) (format:<script>) (per_player)
+    // @Syntax narrate [<text>] (targets:<player>|...) (format:<script>) (per_player) (from:<uuid>)
     // @Required 1
-    // @Maximum 4
+    // @Maximum 5
     // @Short Shows some text to the player.
     // @Group player
     //
@@ -48,6 +51,8 @@ public class NarrateCommand extends AbstractCommand {
     // So, for example, "- narrate 'hello <player.name>' targets:<server.online_players>"
     // would normally say "hello bob" to every player (every player sees the exact same name in the text, ie bob sees "hello bob", steve also sees "hello bob", etc)
     // but if you use "per_player", each player online would see their own name (so bob sees "hello bob", steve sees "hello steve", etc).
+    //
+    // Optionally, specify 'from:<uuid>' to indicate that message came from a specific UUID (used for things like the vanilla client social interaction block option).
     //
     // @Tags
     // None
@@ -81,6 +86,10 @@ public class NarrateCommand extends AbstractCommand {
                     && arg.matchesPrefix("target", "targets", "t")) {
                 scriptEntry.addObject("targets", ListTag.getListFor(TagManager.tagObject(arg.getValue(), scriptEntry.getContext()), scriptEntry.getContext()).filter(PlayerTag.class, scriptEntry));
             }
+            else if (!scriptEntry.hasObject("from")
+                    && arg.matchesPrefix("from")) {
+                scriptEntry.addObject("from", arg.asElement());
+            }
             else if (!scriptEntry.hasObject("per_player")
                     && arg.matches("per_player")) {
                 scriptEntry.addObject("per_player", new ElementTag(true));
@@ -110,6 +119,7 @@ public class NarrateCommand extends AbstractCommand {
         String text = scriptEntry.getElement("text").asString();
         ScriptTag formatObj = scriptEntry.getObjectTag("format");
         ElementTag perPlayerObj = scriptEntry.getElement("per_player");
+        ElementTag from = scriptEntry.getElement("from");
         boolean perPlayer = perPlayerObj != null && perPlayerObj.asBoolean();
         BukkitTagContext context = (BukkitTagContext) scriptEntry.getContext();
         if (!perPlayer || targets == null) {
@@ -120,7 +130,17 @@ public class NarrateCommand extends AbstractCommand {
                     ArgumentHelper.debugObj("Narrating", text)
                             + ArgumentHelper.debugList("Targets", targets)
                             + (formatObj != null ? formatObj.debug() : "")
-                            + (perPlayerObj != null ? perPlayerObj.debug() : ""));
+                            + (perPlayerObj != null ? perPlayerObj.debug() : "")
+                            + (from != null ? from.debug() : ""));
+        }
+        UUID fromId = null;
+        if (from != null) {
+            if (from.asString().startsWith("p@")) {
+                fromId = UUID.fromString(from.asString().substring("p@".length()));
+            }
+            else {
+                fromId = UUID.fromString(from.asString());
+            }
         }
         FormatScriptContainer format = formatObj == null ? null : (FormatScriptContainer) formatObj.getContainer();
         if (targets == null) {
@@ -138,7 +158,13 @@ public class NarrateCommand extends AbstractCommand {
                     context.player = player;
                     personalText = TagManager.tag(personalText, context);
                 }
-                player.getPlayerEntity().spigot().sendMessage(FormattedTextHelper.parse(format != null ? format.getFormattedText(personalText, scriptEntry) : personalText));
+                BaseComponent[] component = FormattedTextHelper.parse(format != null ? format.getFormattedText(personalText, scriptEntry) : personalText);
+                if (fromId == null) {
+                    player.getPlayerEntity().spigot().sendMessage(component);
+                }
+                else {
+                    player.getPlayerEntity().spigot().sendMessage(ChatMessageType.CHAT, fromId, component);
+                }
             }
             else {
                 Debug.echoError("Narrated to non-existent player!?");
