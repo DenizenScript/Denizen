8491beab5bbb9263e8deebd5f27e6c5dc14dcf33
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2021-11-30 09:33:51 -0800

    1.18 part 2: basic version labels n wotnot

--- plugin/src/main/java/com/denizenscript/denizen/nms/NMSVersion.java
+++ plugin/src/main/java/com/denizenscript/denizen/nms/NMSVersion.java
@@ -6,7 +6,8 @@ public enum NMSVersion {
     v1_14,
     v1_15,
     v1_16,
-    v1_17;
+    v1_17,
+    v1_18;
 
     public boolean isAtLeast(NMSVersion version) {
         return ordinal() >= version.ordinal();
