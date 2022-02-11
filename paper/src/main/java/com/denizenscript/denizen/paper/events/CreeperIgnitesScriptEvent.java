ac1e26f01a94beaa9215afc8a1a9d8312adb0a9f
cinthesize <99015220+cinthesize@users.noreply.github.com>
2022-02-11 14:47:28 -0600

    Add CreeperIgniteScriptEvent (#2323)

---
+++ paper/src/main/java/com/denizenscript/denizen/paper/events/CreeperIgnitesScriptEvent.java
@@ -0,0 +1,72 @@
+package com.denizenscript.denizen.paper.events;
+
+import com.denizenscript.denizen.events.BukkitScriptEvent;
+import com.denizenscript.denizen.objects.EntityTag;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.ObjectTag;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import com.destroystokyo.paper.event.entity.CreeperIgniteEvent;
+
+public class CreeperIgnitesScriptEvent extends BukkitScriptEvent implements Listener {
+
+    // <--[event]
+    // @Events
+    // creeper ignites
+    //
+    // @Plugin Paper
+    //
+    // @Group Paper
+    //
+    // @Location true
+    //
+    // @Cancellable true
+    //
+    // @Triggers when a creeper is ignited by flint and steel.
+    //
+    // @Context
+    // <context.entity> returns the EntityTag of the creeper.
+    // <context.ignited> returns true if the creeper is ignited, or false if not. NOTE: In most cases, this will return true.
+    //
+    // -->
+
+    public CreeperIgnitesScriptEvent() {
+        instance = this;
+        registerCouldMatcher("creeper ignites");
+    }
+
+    public static CreeperIgnitesScriptEvent instance;
+    public EntityTag entity;
+    public CreeperIgniteEvent event;
+
+    @Override
+    public boolean matches(ScriptPath path) {
+        if (!runInCheck(path, entity.getLocation())) {
+            return false;
+        }
+        return super.matches(path);
+    }
+
+    @Override
+    public String getName() {
+        return "CreeperIgnites";
+    }
+
+    @Override
+    public ObjectTag getContext(String name) {
+        switch (name) {
+            case "entity":
+                return entity;
+            case "ignited":
+                return new ElementTag(event.isIgnited());
+        }
+        return super.getContext(name);
+    }
+
+    @EventHandler
+    public void onCreeperIgnites(CreeperIgniteEvent event) {
+        entity = new EntityTag(event.getEntity());
+        this.event = event;
+        fire(event);
+    }
+}
