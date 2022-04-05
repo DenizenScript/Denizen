5f9d817b163dbaa8ea8b29209d60de47e18a688c
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-04-05 14:20:27 -0700

    paper listping alternate_player_text determine (restrictedActions)

--- paper/src/main/java/com/denizenscript/denizen/paper/events/ServerListPingScriptEventPaperImpl.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/events/ServerListPingScriptEventPaperImpl.java
@@ -3,21 +3,59 @@ package com.denizenscript.denizen.paper.events;
 import com.denizenscript.denizen.events.server.ListPingScriptEvent;
 import com.denizenscript.denizen.objects.PlayerTag;
 import com.denizenscript.denizen.paper.PaperModule;
+import com.denizenscript.denizen.utilities.debugging.Debug;
 import com.denizenscript.denizencore.objects.ArgumentHelper;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.core.ListTag;
+import com.denizenscript.denizencore.utilities.CoreConfiguration;
 import com.denizenscript.denizencore.utilities.CoreUtilities;
 import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.profile.PlayerTextures;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 public class ServerListPingScriptEventPaperImpl extends ListPingScriptEvent {
+
+    public static class FakeProfile implements PlayerProfile {
+        public String name;
+        @Override public @Nullable String getName() {
+            return name;
+        }
+        @Override public @NotNull String setName(@Nullable String s) {
+            String old = name;
+            name = s;
+            return old;
+        }
+        @Override public @Nullable UUID getUniqueId() { return null; }
+        @Override public @Nullable UUID getId() { return null; }
+        @Override public @Nullable UUID setId(@Nullable UUID uuid) { return null; }
+        @Override public @NotNull PlayerTextures getTextures() { return null; }
+        @Override public void setTextures(@Nullable PlayerTextures playerTextures) { }
+        @Override public @NotNull Set<ProfileProperty> getProperties() { return null; }
+        @Override public boolean hasProperty(@Nullable String s) { return false; }
+        @Override public void setProperty(@NotNull ProfileProperty profileProperty) { }
+        @Override public void setProperties(@NotNull Collection<ProfileProperty> collection) { }
+        @Override public boolean removeProperty(@Nullable String s) { return false; }
+        @Override public void clearProperties() { }
+        @Override public boolean isComplete() { return false; }
+        @Override public @NotNull CompletableFuture<org.bukkit.profile.PlayerProfile> update() { return null; }
+        @Override public org.bukkit.profile.@NotNull PlayerProfile clone() { return null; }
+        @Override public boolean completeFromCache() { return false; }
+        @Override public boolean completeFromCache(boolean b) { return false; }
+        @Override public boolean completeFromCache(boolean b, boolean b1) { return false; }
+        @Override public boolean complete(boolean b) { return false; }
+        @Override public boolean complete(boolean b, boolean b1) { return false; }
+        @Override public @NotNull Map<String, Object> serialize() { return null; }
+    }
 
     @Override
     public boolean applyDetermination(ScriptPath path, ObjectTag determinationObj) {
@@ -41,6 +79,19 @@ public class ServerListPingScriptEventPaperImpl extends ListPingScriptEvent {
                 if (exclusions.contains(players.next().getUniqueId())) {
                     players.remove();
                 }
+            }
+            return true;
+        }
+        else if (lower.startsWith("alternate_player_text:")) {
+            if (!CoreConfiguration.allowRestrictedActions) {
+                Debug.echoError("Cannot use 'alternate_player_text' in list ping event: 'Allow restricted actions' is disabled in Denizen config.yml.");
+                return true;
+            }
+            ((PaperServerListPingEvent) event).getPlayerSample().clear();
+            for (String line : ListTag.valueOf(determination.substring("alternate_player_text:".length()), getTagContext(path))) {
+                FakeProfile lineProf = new FakeProfile();
+                lineProf.setName(line);
+                ((PaperServerListPingEvent) event).getPlayerSample().add(lineProf);
             }
             return true;
         }
