4f1d0788f8d777fa4ed41f9f7af37f5028f9ee25
Aya <31237389+tal5@users.noreply.github.com>
2022-06-13 08:57:14 +0300

    Add `EntityTag.auto_expire` property (#2340)

--- paper/src/main/java/com/denizenscript/denizen/paper/PaperModule.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/PaperModule.java
@@ -1,6 +1,8 @@
 package com.denizenscript.denizen.paper;
 
 import com.denizenscript.denizen.Denizen;
+import com.denizenscript.denizen.nms.NMSHandler;
+import com.denizenscript.denizen.nms.NMSVersion;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizen.objects.ItemTag;
 import com.denizenscript.denizen.objects.PlayerTag;
@@ -52,6 +54,9 @@ public class PaperModule {
 
         // Properties
         PropertyParser.registerProperty(EntityArmsRaised.class, EntityTag.class);
+        if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_18)) {
+            PropertyParser.registerProperty(EntityAutoExpire.class, EntityTag.class);
+        }
         PropertyParser.registerProperty(EntityCanTick.class, EntityTag.class);
         PropertyParser.registerProperty(EntityCarryingEgg.class, EntityTag.class);
         PropertyParser.registerProperty(EntityDrinkingPotion.class, EntityTag.class);
