ff397eb647949a065fe40d8374836a057a0717ef
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-10-16 12:41:22 -0700

    fix error from 'shoot' without 'speed' with pitch=90

--- plugin/src/main/java/com/denizenscript/denizen/utilities/entity/Velocity.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/entity/Velocity.java
@@ -18,13 +18,16 @@ public class Velocity {
      * @return A vector
      */
     public static Vector calculate(Vector from, Vector to, double gravity, double heightGain) {
-
         // Block locations
         int endGain = to.getBlockY() - from.getBlockY();
         double horizDist = Math.sqrt(distanceSquared(from, to));
-
+        if (horizDist == 0) {
+            if (to.getY() > from.getY()) {
+                return new Vector(0, 1, 0);
+            }
+            return new Vector(0, -1, 0);
+        }
         double maxGain = heightGain > (endGain + heightGain) ? heightGain : (endGain + heightGain);
-
         // Solve quadratic equation for velocity
         double a = -horizDist * horizDist / (4 * maxGain);
         double b = horizDist;
@@ -34,35 +37,28 @@ public class Velocity {
 
         // Vertical velocity
         double vy = Math.sqrt(maxGain * (gravity + 0.0013675090252708 * heightGain));
-
         // Horizontal velocity
         double vh = vy / slope;
-
         // Calculate horizontal direction
         int dx = to.getBlockX() - from.getBlockX();
         int dz = to.getBlockZ() - from.getBlockZ();
         double mag = Math.sqrt(dx * dx + dz * dz);
         double dirx = dx / mag;
         double dirz = dz / mag;
-
         // Horizontal velocity components
         double vx = vh * dirx;
         double vz = vh * dirz;
-
         return new Vector(vx, vy, vz);
     }
 
     private static double distanceSquared(Vector from, Vector to) {
-
         double dx = to.getBlockX() - from.getBlockX();
         double dz = to.getBlockZ() - from.getBlockZ();
-
         return dx * dx + dz * dz;
     }
 
     public static Vector spread(Vector from, double yaw, double pitch) {
         Vector vec = from.clone();
-
         float cosyaw = (float) Math.cos(yaw);
         float cospitch = (float) Math.cos(pitch);
         float sinyaw = (float) Math.sin(yaw);
