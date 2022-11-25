f70c3fd6d8c98e977a141c1b38253445ae34cd12
Hydroxycobalamin <54799651+Hydroxycobalamin@users.noreply.github.com>
2022-11-25 02:37:42 +0100

    Add type switches
    Adds 'type' switches for BlockPlaceEvent and VehicleCollidesEvent + cleanup

--- plugin/src/main/java/com/denizenscript/denizen/events/vehicle/VehicleCollidesEntityScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/vehicle/VehicleCollidesEntityScriptEvent.java
@@ -3,10 +3,10 @@ package com.denizenscript.denizen.events.vehicle;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizen.utilities.implementation.BukkitScriptEntryData;
 import com.denizenscript.denizen.events.BukkitScriptEvent;
-import com.denizenscript.denizencore.objects.Argument;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.scripts.ScriptEntryData;
+import com.denizenscript.denizencore.utilities.CoreUtilities;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.vehicle.VehicleEntityCollisionEvent;
@@ -26,6 +26,8 @@ public class VehicleCollidesEntityScriptEvent extends BukkitScriptEvent implemen
     //
     // @Location true
     //
+    // @Switch type:<entity> to only run if the colliding entity matches the EntityTag matcher input.
+    //
     // @Cancellable true
     //
     // @Triggers when a vehicle collides with an entity.
@@ -36,36 +38,31 @@ public class VehicleCollidesEntityScriptEvent extends BukkitScriptEvent implemen
     // <context.pickup> returns whether the vehicle can pick up the entity.
     //
     // @Determine
-    // "PICKUP:TRUE" to allow the vehicle to pick up the entity.
-    // "PICKUP:FALSE" to stop the vehicle from picking up the entity.
+    // "PICKUP:" + ElementTag(Boolean) to set whether the vehicle is allowed to pick up the entity or not.
     //
     // @Player when a vehicle collides with a player.
     //
     // @NPC when a vehicle collides with an NPC.
     //
+    // @Example
+    // on vehicle collides with entity:
+    //
+    // @Example
+    // on minecart collides with sheep:
+    //
+    // @Example
+    // on vehicle collides with entity type:creeper:
+    //
     // -->
 
     public VehicleCollidesEntityScriptEvent() {
+        registerCouldMatcher("<vehicle> collides with <entity>");
+        registerSwitches("type");
     }
 
-
+    public VehicleEntityCollisionEvent event;
     public EntityTag vehicle;
     public EntityTag entity;
-    public VehicleEntityCollisionEvent event;
-
-    @Override
-    public boolean couldMatch(ScriptPath path) {
-        if (!path.eventLower.contains("collides with")) {
-            return false;
-        }
-        if (!couldMatchEntity(path.eventArgLowerAt(3))) {
-            return false;
-        }
-        if (!exactMatchesVehicle(path.eventArgLowerAt(0))) {
-            return false;
-        }
-        return true;
-    }
 
     @Override
     public boolean matches(ScriptPath path) {
@@ -78,15 +75,18 @@ public class VehicleCollidesEntityScriptEvent extends BukkitScriptEvent implemen
         if (!runInCheck(path, vehicle.getLocation())) {
             return false;
         }
+        if (!path.tryObjectSwitch("type", entity)) {
+            return false;
+        }
         return super.matches(path);
     }
 
     @Override
     public boolean applyDetermination(ScriptPath path, ObjectTag determinationObj) {
         if (determinationObj instanceof ElementTag) {
-            Argument arg = Argument.valueOf(determinationObj.toString());
-            if (arg.matchesPrefix("pickup")) {
-                event.setPickupCancelled(!arg.asElement().asBoolean());
+            String lower = CoreUtilities.toLowerCase(determinationObj.toString());
+            if (lower.startsWith("pickup:")) {
+                event.setPickupCancelled(!(new ElementTag(lower.substring("pickup:".length())).asBoolean()));
                 return true;
             }
         }
@@ -101,12 +101,9 @@ public class VehicleCollidesEntityScriptEvent extends BukkitScriptEvent implemen
     @Override
     public ObjectTag getContext(String name) {
         switch (name) {
-            case "vehicle":
-                return vehicle;
-            case "entity":
-                return entity.getDenizenObject();
-            case "pickup":
-                return new ElementTag(!event.isPickupCancelled());
+            case "vehicle": return vehicle;
+            case "entity": return entity.getDenizenObject();
+            case "pickup": return new ElementTag(!event.isPickupCancelled());
         }
         return super.getContext(name);
     }
