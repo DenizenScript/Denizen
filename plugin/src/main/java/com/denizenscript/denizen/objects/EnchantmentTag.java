452d468b1af37963daad2a4e96979ba60c3a8331
Brando! <63469489+BreadcrumbIsTaken@users.noreply.github.com>
2022-12-19 08:08:10 -0800

    `EnchantmentTag` and `PolygonTag` examples (#2413)
    * Add `EnchantmentTag` tag examples.
    
    * Add `PolygonTag` tag and mech examples.
    
    * Fix wording of example note.
    
    * Add more clarification on tag examples.
    
    * Corrections and cleanup.
    
    * Fix line-length issues.

--- plugin/src/main/java/com/denizenscript/denizen/objects/EnchantmentTag.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/EnchantmentTag.java
@@ -167,6 +167,10 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // Gets the name of this enchantment. For vanilla enchantments, uses the vanilla name like 'sharpness'.
         // For Denizen custom enchantments, returns the 'id' specified in the script.
         // For any other enchantments, returns the full key.
+        // @example
+        // # This can narrate something like this:
+        // # "The item in your hand's first enchantment is sharpness!"
+        // - narrate "The item in your hand's first enchantment is <player.item_in_hand.enchantment_types.get[1].name||nothing>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "name", (attribute, object) -> {
             return new ElementTag(object.getCleanName());
@@ -177,6 +181,13 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag
         // @description
         // Returns the full key for this enchantment, like "minecraft:sharpness".
+        // @example
+        // # Narrates "The key for the flame enchantment is: minecraft:flame!"
+        // - narrate "The key for the flame enchantment is: <enchantment[flame].key>!"
+        // @example
+        // # This example uses a custom Denizen enchantment.
+        // # Narrates "The key for the my_enchantment enchantment is: denizen:my_enchantment!"
+        // - narrate "The key for the my_enchantment enchantment is: <enchantment[my_enchantment].key>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "key", (attribute, object) -> {
             return new ElementTag(object.enchantment.getKey().toString());
@@ -188,6 +199,10 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @description
         // Returns the full name for this enchantment for a given level, like "Sharpness V".
         // For vanilla enchantments, uses language translation keys.
+        // @example
+        // # Narrates "You don your Thorns III armor."
+        // # Note: vanilla enchantments have a color applied to them, such as grey or red.
+        // - narrate "You don your <enchantment[thorns].full_name[3]><reset> armor."
         // -->
         tagProcessor.registerTag(ElementTag.class, "full_name", (attribute, object) -> {
             if (!attribute.hasParam()) {
@@ -201,6 +216,10 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ScriptTag
         // @description
         // Returns the script that created this enchantment type, if any.
+        // @example
+        // # This can narrate something like this:
+        // # "The script used to create the my_enchantment enchantment is: my_enchantment_script"
+        // - narrate "The script used to create the my_enchantment enchantment is: <enchantment[my_enchantment].script.name>"
         // -->
         tagProcessor.registerTag(ScriptTag.class, "script", (attribute, object) -> {
             if (!object.enchantment.getKey().getNamespace().equals("denizen")) {
@@ -218,6 +237,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Number)
         // @description
         // Returns the minimum level of this enchantment. Usually '1'.
+        // @example
+        // # Narrates "The minimum enchantment level that you can get for Feather Falling is: 1!"
+        // - narrate "The minimum enchantment level that you can get for Feather Falling is: <enchantment[feather_falling].min_level>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "min_level", (attribute, object) -> {
             return new ElementTag(object.enchantment.getStartLevel());
@@ -228,6 +250,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Number)
         // @description
         // Returns the maximum level of this enchantment. Usually between 1 and 5.
+        // @example
+        // # Narrates "The maximum enchantment level that you can get for Feather Falling is: 4!"
+        // - narrate "The maximum enchantment level that you can get for Feather Falling is: <enchantment[feather_falling].max_level>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "max_level", (attribute, object) -> {
             return new ElementTag(object.enchantment.getMaxLevel());
@@ -238,6 +263,13 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Boolean)
         // @description
         // Returns whether this enchantment is only for spawning as treasure.
+        // @example
+        // # In the case of the "loyalty" enchantment, this will narrate:
+        // # "You cannot find this enchantment as treasure!"
+        // - if <enchantment[loyalty].treasure_only>:
+        //     - narrate "You can only find this enchantment as treasure!"
+        // - else:
+        //     - narrate "You cannot find this enchantment as treasure!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "treasure_only", (attribute, object) -> {
             return new ElementTag(object.enchantment.isTreasure());
@@ -248,6 +280,13 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Boolean)
         // @description
         // Returns whether this enchantment is only considered to be tradable. Villagers won't trade this enchantment if set to false.
+        // @example
+        // # In the case of the "loyalty" enchantment, this will narrate:
+        // # "Let's do some haggling to get this enchantment!"
+        // - if <enchantment[loyalty].is_tradable>:
+        //     - narrate "Let's do some haggling to get this enchantment!"
+        // - else:
+        //     - narrate "Villagers don't seem to want to trade this enchantment!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "is_tradable", (attribute, object) -> {
             return new ElementTag(NMSHandler.enchantmentHelper.isTradable(object.enchantment));
@@ -259,6 +298,13 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @description
         // Returns whether this enchantment is only considered to be discoverable.
         // If true, this will spawn from vanilla sources like the enchanting table. If false, it can only be given directly by script.
+        // @example
+        // # In the case of the "loyalty" enchantment, this will narrate:
+        // # "Time to do some discovering!"
+        // - if <enchantment[loyalty].is_discoverable>:
+        //     - narrate "Time to do some discovering!"
+        // - else:
+        //     - narrate "You'll have to be given this enchantment through a script!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "is_discoverable", (attribute, object) -> {
             return new ElementTag(NMSHandler.enchantmentHelper.isDiscoverable(object.enchantment));
@@ -269,6 +315,13 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Boolean)
         // @description
         // Returns whether this enchantment is only considered to be a curse. Curses are removed at grindstones, and spread from crafting table repairs.
+        // @example
+        // # In the case of the "vanishing_curse" enchantment, this will narrate:
+        // # "Watch out! This enchantment is a curse!"
+        // - if <enchantment[vanishing_curse].is_curse>:
+        //     - narrate "Watch out! This enchantment is a curse!"
+        // - else:
+        //     - narrate "Phew, this enchantment is not a curse!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "is_curse", (attribute, object) -> {
             return new ElementTag(NMSHandler.enchantmentHelper.isCurse(object.enchantment));
@@ -280,6 +333,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @description
         // Returns the category of this enchantment. Can be any of: ARMOR, ARMOR_FEET, ARMOR_LEGS, ARMOR_CHEST, ARMOR_HEAD,
         // WEAPON, DIGGER, FISHING_ROD, TRIDENT, BREAKABLE, BOW, WEARABLE, CROSSBOW, VANISHABLE
+        // @example
+        // # This narrates "The bane_of_arthropods enchantment is in the WEAPON category!"
+        // - narrate "The bane_of_arthropods enchantment is in the <enchantment[bane_of_arthropods].category> category!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "category", (attribute, object) -> {
             return new ElementTag(object.enchantment.getItemTarget());
@@ -290,6 +346,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag
         // @description
         // Returns the rarity of this enchantment. Can be any of: COMMON, UNCOMMON, RARE, VERY_RARE
+        // @example
+        // # This narrates "The infinity enchantment is VERY_RARE!"
+        // - narrate "The infinity enchantment is <enchantment[infinity].rarity>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "rarity", (attribute, object) -> {
             return new ElementTag(NMSHandler.enchantmentHelper.getRarity(object.enchantment));
@@ -301,6 +360,13 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @description
         // Returns whether this enchantment can enchant the given ItemTag (based on material mainly).
         // This is internally based on multiple factors, such as the enchantment's category and its own specific compatibility checks.
+        // @example
+        // # In the case of the "knockback" enchantment, this narrates
+        // # "You can apply the knockback enchantment in a diamond sword!"
+        // - if <enchantment[knockback].can_enchant[diamond_sword]>:
+        //     - narrate "You can apply the knockback enchantment in a diamond sword!"
+        // - else:
+        //     - narrate "You cannot apply the knockback enchantment in a diamond sword!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "can_enchant", (attribute, object) -> {
             if (!attribute.hasParam()) {
@@ -314,6 +380,13 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Boolean)
         // @description
         // Returns whether this enchantment is compatible with another given enchantment.
+        // @example
+        // # In the case of the "silk_touch" and "mending" enchantments, this narrates
+        // # "These enchantments are compatible together!"
+        // - if <enchantment[silk_touch].is_compatible[mending]>:
+        //     - narrate "These enchantments are compatible together!"
+        // - else:
+        //     - narrate "These enchantments are not compatible with each other!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "is_compatible", (attribute, object) -> {
             if (!attribute.hasParam()) {
@@ -327,6 +400,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Decimal)
         // @description
         // Returns the minimum cost for this enchantment for the given level.
+        // @example
+        // # This narrates "The minimum cost for the efficiency 3 enchantment is: 21!"
+        // - narrate "The minimum cost for the efficiency 3 enchantment is: <enchantment[efficiency].min_cost[3]>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "min_cost", (attribute, object) -> {
             if (!attribute.hasParam()) {
@@ -340,6 +416,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @returns ElementTag(Decimal)
         // @description
         // Returns the maximum cost for this enchantment for the given level.
+        // @example
+        // # This narrates "The maximum cost for the efficiency 3 enchantment is: 81!"
+        // - narrate "The maximum cost for the efficiency 3 enchantment is: <enchantment[efficiency].max_cost[3]>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, "max_cost", (attribute, object) -> {
             if (!attribute.hasParam()) {
@@ -354,7 +433,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // @description
         // Returns the damage bonus this enchantment applies against the given monster type.
         // The input is a MapTag with a level value and a monster type specified, where the type can be any of: ARTHROPOD, ILLAGER, WATER, UNDEAD, or UNDEFINED
-        // For example, <[my_enchantment].damage_bonus[level=3;type=undead]>
+        // @example
+        // # Narrates "With Bane of Arthropods 2, you get a damage bonus of 5 on arthropods!"
+        // - narrate "With Bane of Arthropods 2, you get a damage bonus of <enchantment[bane_of_arthropods].damage_bonus[level=2;type=arthropod]> on arthropods!"
         // -->
         tagProcessor.registerTag(ElementTag.class, MapTag.class, "damage_bonus", (attribute, object, map) -> {
             ElementTag level = map.getElement("level");
@@ -373,7 +454,9 @@ public class EnchantmentTag implements ObjectTag, FlaggableObject {
         // Returns the damage protection this enchantment applies against the given damage cause and optional attacker.
         // The input is a MapTag with a level value and a damage type specified, where the damage type must be from <@link language Damage Cause>.
         // For entity damage causes, optionally specify the entity attacker.
-        // For example, <[my_enchantment].damage_protection[level=3;type=undead]>
+        // @example
+        // # Narrates "With the Protection 3 enchantment, there is fall damage protection of 3!"
+        // - narrate "With the Protection 3 enchantment, there is fall damage protection of <enchantment[protection].damage_protection[level=3;type=fall]>!"
         // -->
         tagProcessor.registerTag(ElementTag.class, MapTag.class, "damage_protection", (attribute, object, map) -> {
             ElementTag level = map.getRequiredObjectAs("level", ElementTag.class, attribute);
