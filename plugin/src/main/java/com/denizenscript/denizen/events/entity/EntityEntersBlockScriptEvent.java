d386bab5cf77490235e6345f9f7d89e6b5534578
sudura <13711345+sudura@users.noreply.github.com>
2022-12-24 02:49:47 +0000

    remove <> in meta link

--- plugin/src/main/java/com/denizenscript/denizen/events/entity/EntityEntersBlockScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/entity/EntityEntersBlockScriptEvent.java
@@ -24,7 +24,7 @@ public class EntityEntersBlockScriptEvent extends BukkitScriptEvent implements L
     // @Cancellable true
     //
     // @Triggers when an entity enters, and is stored in a block (EG a bee enters a bee nest)
-    // Does not fire when a silverfish "enters" a stone block. Prefer <@link event <entity> changes <block> (into <block>)> for that.
+    // Does not fire when a silverfish "enters" a stone block. Prefer <@link event entity changes block (into block)> for that.
     //
     // @Context
     // <context.entity> returns the EntityTag.
