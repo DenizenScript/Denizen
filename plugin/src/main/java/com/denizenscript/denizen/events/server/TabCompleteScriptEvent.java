ada430040e5ec7d7459914931397ad618ac5b465
daniel <13711345+sudura@users.noreply.github.com>
2023-01-09 08:49:27 +0000

    Command Switch for Tab Complete (#2425)
    * Add command switch to TabCompleteScriptEvent
    
    * Meta docs
    
    * Attempting different way
    
    * Better way working
    
    * Change to runGenericSwitchCheck

--- plugin/src/main/java/com/denizenscript/denizen/events/server/TabCompleteScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/server/TabCompleteScriptEvent.java
@@ -21,6 +21,8 @@ public class TabCompleteScriptEvent extends BukkitScriptEvent implements Listene
     //
     // @Group Server
     //
+    // @Switch command:<command_name> to only process the event if the command matches the input name.
+    //
     // @Cancellable true
     //
     // @Triggers when a player or the console is sent a list of available tab completions.
@@ -41,9 +43,18 @@ public class TabCompleteScriptEvent extends BukkitScriptEvent implements Listene
 
     public TabCompleteScriptEvent() {
         registerCouldMatcher("tab complete");
+        registerSwitches("command");
     }
 
     public TabCompleteEvent event;
+
+    @Override
+    public boolean matches(ScriptPath path) {
+        if (!runGenericSwitchCheck(path, "command", getCommand())) {
+            return false;
+        }
+        return super.matches(path);
+    }
 
     public String getCommand() {
         String[] args = event.getBuffer().trim().split(" ");
