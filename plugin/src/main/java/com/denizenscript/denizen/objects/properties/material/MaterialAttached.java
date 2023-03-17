8a63c83421550986827dc1ce1454e4144d2ea8ce
SXRWahrheit <mattisonenloe@summaxr.com>
2023-03-17 15:55:29 -0500

    Avoid splitting a line in the middle of a sentance

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialAttached.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialAttached.java
@@ -148,8 +148,8 @@ public class MaterialAttached implements Property {
         // For a gate, this sets whether it is lowered to attach to a wall block.
         // For a mangrove_propagule, this sets whether it is hanging from the block above it.
         // For a tripwire, this sets whether a tripwire hook or string forms a complete tripwire circuit and is ready to trigger.
-        // Updating the property on a tripwire hook will change the texture to indicate a connected string, but will not have
-        // any effect when used on the tripwire string itself. It may however still be used to check whether the string forms a circuit.
+        // Updating the property on a tripwire hook will change the texture to indicate a connected string, but will not have any effect when used on the tripwire string itself.
+        // It may however still be used to check whether the string forms a circuit.
         // For hanging signs, this affects signs hanging below a block and changes whether the chains are vertical (false) or diagonal (true).
         // @tags
         // <MaterialTag.attached>
