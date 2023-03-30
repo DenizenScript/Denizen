597aff55e7274ac2e16a289a91e003791748b7bb
Brando! <63469489+BreadcrumbIsTaken@users.noreply.github.com>
2023-03-30 16:13:26 -0700

    `Instrument` property for goat horns (#2449)
    * `HornInstrument` property for goat horns
    
    * Update NSMVersion
    
    * Fix meta
    
    * Fix meta again
    
    Wow, my brain was not paying attention AT ALL when I was writing this huh?
    
    * Change property name to `ItemInstrument` + fixes

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/PropertyRegistry.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/PropertyRegistry.java
@@ -155,6 +155,9 @@ public class PropertyRegistry {
         PropertyParser.registerProperty(ItemFlags.class, ItemTag.class);
         PropertyParser.registerProperty(ItemFrameInvisible.class, ItemTag.class);
         PropertyParser.registerProperty(ItemHidden.class, ItemTag.class);
+        if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19)) {
+            PropertyParser.registerProperty(ItemInstrument.class, ItemTag.class);
+        }
         PropertyParser.registerProperty(ItemInventory.class, ItemTag.class);
         PropertyParser.registerProperty(ItemKnowledgeBookRecipes.class, ItemTag.class);
         PropertyParser.registerProperty(ItemLock.class, ItemTag.class);
