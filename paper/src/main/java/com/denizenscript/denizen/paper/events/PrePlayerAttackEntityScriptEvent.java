38364f3e5315eb7a26e7a4bb7668afc1b9ec1a80
hyper <mrminekqp@gmail.com>
2023-05-27 19:59:18 -0400

    Update PrePlayerAttackEntityScriptEvent.java
    Removed useless import, updated event line & updated matcher accordingly,  moved context for tags into triggers, and fixed formatting issue.

--- paper/src/main/java/com/denizenscript/denizen/paper/events/PrePlayerAttackEntityScriptEvent.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/events/PrePlayerAttackEntityScriptEvent.java
@@ -10,15 +10,12 @@ import com.denizenscript.denizencore.scripts.ScriptEntryData;
 import io.papermc.paper.event.player.PrePlayerAttackEntityEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 
 public class PrePlayerAttackEntityScriptEvent extends BukkitScriptEvent implements Listener {
 
     // <--[event]
     // @Events
-    // player attacks <entity>
-    //
-    // @Location true
+    // player pre attacks <entity>
     //
     // @Plugin Paper
     //
@@ -29,20 +26,19 @@ public class PrePlayerAttackEntityScriptEvent extends BukkitScriptEvent implemen
     // @Switch with:<item> to only process the event if the player attacks the entity with the specified item.
     //
     // @Triggers when the player tries to attack an entity. This occurs before any of the damage logic, so cancelling this event will prevent any sort of sounds from being played when attacking.
+    // Entities like falling sand would return false because their entity type does not allow them to be attacked.
+    // Note: there may be other factors (invulnerability, etc.) that will prevent this entity from being attacked that this event will not cover.
     //
     // @Context
     // <context.entity> returns the entity that was attacked in this event.
     // <context.attacked> returns whether this entity would be attacked normally.
-    // Entities like falling sand would return false because their entity type does not allow them to be attacked.
-    // Note: there may be other factors (invulnerability, etc.) that will prevent this entity from being attacked
-    // that this event will not cover.
     //
     // @Player when the attacked entity is a player.
     //
     // -->
 
     public PrePlayerAttackEntityScriptEvent() {
-        registerCouldMatcher("player attacks <entity>");
+        registerCouldMatcher("player pre attacks <entity>");
         registerSwitches("with");
     }
 
@@ -52,7 +48,7 @@ public class PrePlayerAttackEntityScriptEvent extends BukkitScriptEvent implemen
 
     @Override
     public boolean matches(ScriptPath path) {
-        if (!path.tryArgObject(2, entity)) {
+        if (!path.tryArgObject(3, entity)) {
             return false;
         }
         if (!path.tryObjectSwitch("with", new ItemTag(event.getPlayer().getEquipment().getItemInMainHand()))) {
