96ce69dcbbfb484e877bd0cafd7aaf90663231e6
Aya <31237389+tal5@users.noreply.github.com>
2023-06-26 14:16:53 +0100

    Interaction entity support

--- plugin/src/main/java/com/denizenscript/denizen/utilities/MultiVersionHelper1_19.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/MultiVersionHelper1_19.java
@@ -1,9 +1,19 @@
 package com.denizenscript.denizen.utilities;
 
+import com.denizenscript.denizen.objects.PlayerTag;
 import com.denizenscript.denizen.objects.properties.entity.EntityColor;
 import com.denizenscript.denizencore.objects.Mechanism;
+import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.core.ListTag;
-import org.bukkit.entity.*;
+import com.denizenscript.denizencore.objects.core.MapTag;
+import com.denizenscript.denizencore.objects.core.TimeTag;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Frog;
+import org.bukkit.entity.Interaction;
+
+import java.time.temporal.ChronoUnit;
 
 public class MultiVersionHelper1_19 {
 
@@ -12,8 +22,8 @@ public class MultiVersionHelper1_19 {
     }
 
     public static String getColor(Entity entity) {
-        if (entity instanceof Frog) {
-            return ((Frog) entity).getVariant().name();
+        if (entity instanceof Frog frog) {
+            return frog.getVariant().name();
         }
         return null;
     }
@@ -26,8 +36,20 @@ public class MultiVersionHelper1_19 {
     }
 
     public static void setColor(Entity entity, Mechanism mech) {
-        if (entity instanceof Frog && mech.requireEnum(Frog.Variant.class)) {
-            ((Frog) entity).setVariant(Frog.Variant.valueOf(mech.getValue().asString().toUpperCase()));
+        if (entity instanceof Frog frog && mech.requireEnum(Frog.Variant.class)) {
+            frog.setVariant(mech.getValue().asEnum(Frog.Variant.class));
         }
+    }
+
+    public static MapTag interactionToMap(Interaction.PreviousInteraction interaction, World world) {
+        if (interaction == null) {
+            return null;
+        }
+        MapTag result = new MapTag();
+        result.putObject("player", new PlayerTag(interaction.getPlayer()));
+        long millis = (world.getGameTime() - interaction.getTimestamp()) * 50L;
+        result.putObject("time", new TimeTag(TimeTag.now().instant.minus(millis, ChronoUnit.MILLIS)));
+        result.putObject("raw_game_time", new ElementTag(interaction.getTimestamp()));
+        return result;
     }
 }
