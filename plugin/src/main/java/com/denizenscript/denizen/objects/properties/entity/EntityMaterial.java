DenizenScript/Denizen

commit ba0aa4b2c096ae62482c7fa63de5503cfe463a27

EntityTag.material: block_display

plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityMaterial.java
@@ -1,5 +1,7 @@
 package com.denizenscript.denizen.objects.properties.entity;
 
+import com.denizenscript.denizen.nms.NMSHandler;
+import com.denizenscript.denizen.nms.NMSVersion;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.Mechanism;
@@ -8,6 +10,7 @@ import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.entity.BlockDisplay;
 import org.bukkit.entity.Enderman;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Minecart;
@@ -15,12 +18,13 @@ import org.bukkit.entity.Minecart;
 public class EntityMaterial implements Property {
 
     public static boolean describes(ObjectTag object) {
-        if (!(object instanceof EntityTag)) {
+        if (!(object instanceof EntityTag entityTag)) {
             return false;
         }
-        Entity entity = ((EntityTag) object).getBukkitEntity();
+        Entity entity = entityTag.getBukkitEntity();
         return entity instanceof Enderman
-                || entity instanceof Minecart;
+                || entity instanceof Minecart
+                || (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19) && entity instanceof BlockDisplay);
     }
 
     public static EntityMaterial getFrom(ObjectTag _entity) {
@@ -53,36 +57,28 @@ public class EntityMaterial implements Property {
         // Returns the block material associated with the entity.
         // For endermen, returns the material the enderman is holding.
         // For minecarts, returns the material the minecart is carrying.
+        // For block displays, returns the displayed block.
         // -->
         PropertyParser.registerTag(EntityMaterial.class, MaterialTag.class, "material", (attribute, object) -> {
             return object.getMaterial();
         });
     }
 
-    public boolean isEnderman() {
-        return entity.getBukkitEntity() instanceof Enderman;
-    }
-
-    public boolean isMinecart() {
-        return entity.getBukkitEntity() instanceof Minecart;
-    }
-
-    public Enderman getEnderman() {
-        return (Enderman) entity.getBukkitEntity();
-    }
-
-    public Minecart getMinecart() {
-        return (Minecart) entity.getBukkitEntity();
+    public BlockData getBlockData() {
+        if (entity instanceof Enderman enderman) {
+            return enderman.getCarriedBlock();
+        }
+        else if (entity instanceof Minecart minecart) {
+            return minecart.getDisplayBlockData();
+        }
+        else if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19) && entity instanceof BlockDisplay blockDisplay) {
+            return blockDisplay.getBlock();
+        }
+        return null;
     }
 
     public MaterialTag getMaterial() {
-        BlockData data = null;
-        if (isEnderman()) {
-            data = getEnderman().getCarriedBlock();
-        }
-        else if (isMinecart()) {
-            data = getMinecart().getDisplayBlockData();
-        }
+        BlockData data = getBlockData();
         if (data == null) {
             return new MaterialTag(Material.AIR);
         }
@@ -120,11 +116,15 @@ public class EntityMaterial implements Property {
         // -->
         if (mechanism.matches("material") && mechanism.requireObject(MaterialTag.class)) {
             BlockData data = mechanism.valueAsType(MaterialTag.class).getModernData();
-            if (isEnderman()) {
-                getEnderman().setCarriedBlock(data);
+            Entity entity = this.entity.getBukkitEntity();
+            if (entity instanceof Enderman enderman) {
+                enderman.setCarriedBlock(data);
             }
-            else if (isMinecart()) {
-                getMinecart().setDisplayBlockData(data);
+            else if (entity instanceof Minecart minecart) {
+                minecart.setDisplayBlockData(data);
+            }
+            else if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19) && entity instanceof BlockDisplay blockDisplay) {
+                blockDisplay.setBlock(data);
             }
         }
     }

plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityDisplayEntityData.java
@@ -84,7 +84,8 @@ public class EntityDisplayEntityData implements Property {
         map.putObject("transformation_translation", convertVector(trans.getTranslation()));
         map.putObject("view_range", new ElementTag(display.getViewRange()));
         if (display instanceof BlockDisplay block) {
-            map.putObject("block_display", new MaterialTag(block.getBlock()));
+            // handled by EntityTag.material
+            //map.putObject("block_display", new MaterialTag(block.getBlock()));
         }
         else if (display instanceof ItemDisplay item) {
             // "itemStack" handled by EntityTag.item
@@ -129,7 +130,7 @@ public class EntityDisplayEntityData implements Property {
         // Keys: billboard, brightness_block, brightness_sky, width, height, glow_color, interpolation_delay, interpolation_duration, shadow_radius, shadow_strength, view_range,
         // transformation_left_rotation, transformation_right_rotation, transformation_scale, transformation_translation
         // (Note: rotations use a temporary ListTag format, subject to replacement).
-        // For block displays: block_display
+        // For block displays: (N/A)
         // For item displays: item_transform
         // For text displays: text_alignment, text_background_color, text_line_width, text, text_opacity, text_is_default_background, text_is_see_through, text_is_shadowed
         // -->
@@ -177,7 +178,7 @@ public class EntityDisplayEntityData implements Property {
             }
             display.setViewRange(map.getElement("view_range", String.valueOf(display.getViewRange())).asFloat());
             if (display instanceof BlockDisplay block) {
-                block.setBlock(map.getObjectAs("block_display", MaterialTag.class, mechanism.context).getModernData());
+                //block.setBlock(map.getObjectAs("block_display", MaterialTag.class, mechanism.context).getModernData());
             }
             else if (display instanceof ItemDisplay item) {
                 item.setItemDisplayTransform(map.getElement("item_transform").asEnum(ItemDisplay.ItemDisplayTransform.class));

commit a7692452f26bf0c3da511cd18427529470861963

Fix + Update `EntityMaterial`
https://github.com/DenizenScript/Denizen/commit/ba0aa4b2c096ae62482c7fa63de5503cfe463a27 broke it by making the `instanceof` checks in `getBlockData` use the `EntityTag` object instead of the Bukkit entity object.
This fixes that, and updates it to modern property format.
Tested on `1.20.1` and `1.17.1`.

plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityMaterial.java
@@ -5,93 +5,62 @@ import com.denizenscript.denizen.nms.NMSVersion;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.properties.Property;
-import com.denizenscript.denizencore.objects.ObjectTag;
-import com.denizenscript.denizencore.objects.properties.PropertyParser;
 import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.BlockDisplay;
 import org.bukkit.entity.Enderman;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Minecart;
 
-public class EntityMaterial implements Property {
+public class EntityMaterial extends EntityProperty<MaterialTag> {
 
-    public static boolean describes(ObjectTag object) {
-        if (!(object instanceof EntityTag entityTag)) {
-            return false;
-        }
-        Entity entity = entityTag.getBukkitEntity();
-        return entity instanceof Enderman
-                || entity instanceof Minecart
-                || (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19) && entity instanceof BlockDisplay);
-    }
+    // <--[property]
+    // @object EntityTag
+    // @name material
+    // @input MaterialTag
+    // @description
+    // An entity's associated block material.
+    // For endermen, this is the block being held.
+    // For minecarts, this is the block being carried.
+    // For block displays, this is the block being displayed.
+    // -->
 
-    public static EntityMaterial getFrom(ObjectTag _entity) {
-        if (!describes(_entity)) {
-            return null;
-        }
-        else {
-            return new EntityMaterial((EntityTag) _entity);
-        }
-    }
-
-    public static final String[] handledMechs = new String[]{
-            "material"
-    };
-
-    public EntityMaterial(EntityTag _entity) {
-        entity = _entity;
-    }
-
-    EntityTag entity;
-
-    public static void register() {
-
-        // <--[tag]
-        // @attribute <EntityTag.material>
-        // @returns MaterialTag
-        // @mechanism EntityTag.material
-        // @group properties
-        // @description
-        // Returns the block material associated with the entity.
-        // For endermen, returns the material the enderman is holding.
-        // For minecarts, returns the material the minecart is carrying.
-        // For block displays, returns the displayed block.
-        // -->
-        PropertyParser.registerTag(EntityMaterial.class, MaterialTag.class, "material", (attribute, object) -> {
-            return object.getMaterial();
-        });
-    }
-
-    public BlockData getBlockData() {
-        if (entity instanceof Enderman enderman) {
-            return enderman.getCarriedBlock();
-        }
-        else if (entity instanceof Minecart minecart) {
-            return minecart.getDisplayBlockData();
-        }
-        else if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19) && entity instanceof BlockDisplay blockDisplay) {
-            return blockDisplay.getBlock();
-        }
-        return null;
-    }
-
-    public MaterialTag getMaterial() {
-        BlockData data = getBlockData();
-        if (data == null) {
-            return new MaterialTag(Material.AIR);
-        }
-        return new MaterialTag(data);
+    public static boolean describes(EntityTag entity) {
+        return entity.getBukkitEntity() instanceof Enderman
+                || entity.getBukkitEntity() instanceof Minecart
+                || (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19) && entity.getBukkitEntity() instanceof BlockDisplay);
     }
 
     @Override
-    public String getPropertyString() {
-        MaterialTag material = getMaterial();
-        if (material.getMaterial() != Material.AIR) {
-            return material.identify();
+    public MaterialTag getPropertyValue() {
+        BlockData blockData = null;
+        if (getEntity() instanceof Enderman enderman) {
+            blockData = enderman.getCarriedBlock();
         }
-        return null;
+        else if (getEntity() instanceof Minecart minecart) {
+            blockData = minecart.getDisplayBlockData();
+        }
+        else if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19)) {
+            blockData = as(BlockDisplay.class).getBlock();
+        }
+        return blockData != null ? new MaterialTag(blockData) : new MaterialTag(Material.AIR);
+    }
+
+    @Override
+    public boolean isDefaultValue(MaterialTag value) {
+        return value.getMaterial() == Material.AIR;
+    }
+
+    @Override
+    public void setPropertyValue(MaterialTag value, Mechanism mechanism) {
+        if (getEntity() instanceof Enderman enderman) {
+            enderman.setCarriedBlock(value.getModernData());
+        }
+        else if (getEntity() instanceof Minecart minecart) {
+            minecart.setDisplayBlockData(value.getModernData());
+        }
+        else if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19)) {
+            as(BlockDisplay.class).setBlock(value.getModernData());
+        }
     }
 
     @Override
@@ -99,33 +68,7 @@ public class EntityMaterial implements Property {
         return "material";
     }
 
-
-    @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object EntityTag
-        // @name material
-        // @input MaterialTag
-        // @description
-        // Sets the block material associated with the entity.
-        // For endermen, sets the material the enderman is holding.
-        // For minecarts, sets the material the minecart is carrying.
-        // @tags
-        // <EntityTag.material>
-        // -->
-        if (mechanism.matches("material") && mechanism.requireObject(MaterialTag.class)) {
-            BlockData data = mechanism.valueAsType(MaterialTag.class).getModernData();
-            Entity entity = this.entity.getBukkitEntity();
-            if (entity instanceof Enderman enderman) {
-                enderman.setCarriedBlock(data);
-            }
-            else if (entity instanceof Minecart minecart) {
-                minecart.setDisplayBlockData(data);
-            }
-            else if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_19) && entity instanceof BlockDisplay blockDisplay) {
-                blockDisplay.setBlock(data);
-            }
-        }
+    public static void register() {
+        autoRegister("material", EntityMaterial.class, MaterialTag.class, false);
     }
 }