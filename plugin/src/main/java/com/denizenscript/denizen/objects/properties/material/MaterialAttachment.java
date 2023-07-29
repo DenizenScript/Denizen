41482ccb5d9764567e286eca3389cc0fc65b6341
BreadcrumbIsTaken <63469489+BreadcrumbIsTaken@users.noreply.github.com>
2023-07-29 10:41:39 -0700

    Bell `attachment` mechanism beginnings

---
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialAttachment.java
@@ -0,0 +1,47 @@
+package com.denizenscript.denizen.objects.properties.material;
+
+import com.denizenscript.denizen.objects.MaterialTag;
+import com.denizenscript.denizencore.objects.Mechanism;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import org.bukkit.block.data.type.Bell;
+
+public class MaterialAttachment extends MaterialProperty<ElementTag> {
+
+    // <--[property]
+    // @object MaterialTag
+    // @name attachment
+    // @input ElementTag
+    // @description
+    // Controls the attachment of a bell.
+    // For types of attachment, see <@link url https://hub.spigotmc.org/javadocs/spigot/org/bukkit/block/data/type/Bell.Attachment.html>
+    // -->
+
+    public static boolean describes(MaterialTag material) {
+        return material.getModernData() instanceof Bell;
+    }
+
+    @Override
+    public ElementTag getPropertyValue() {
+        Bell bell = (Bell) getBlockData();
+        return new ElementTag(bell.getAttachment().toString());
+    }
+
+    @Override
+    public String getPropertyId() {
+        return "attachment";
+    }
+
+    // TODO: The bell does not set it's attachment in game. It stays the same. Make it so it actually changes lol.
+    @Override
+    public void setPropertyValue(ElementTag attachment, Mechanism mechanism) {
+        if (!mechanism.requireEnum(Bell.Attachment.class)) {
+            return;
+        }
+        Bell bell = (Bell) getBlockData();
+        bell.setAttachment(Bell.Attachment.valueOf(attachment.toString().toUpperCase()));
+    }
+
+    public static void register() {
+        autoRegister("attachment", MaterialAttachment.class, ElementTag.class, false);
+    }
+}
