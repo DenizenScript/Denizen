10b3ef870989d66742334cca51a7a6b6e5ca6c63
pobab <43501409+pobab@users.noreply.github.com>
2023-08-13 17:50:52 +0800

    add new context fuel_level & result on BrewingStand event

--- plugin/src/main/java/com/denizenscript/denizen/events/block/BrewsScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/block/BrewsScriptEvent.java
@@ -1,12 +1,17 @@
 package com.denizenscript.denizen.events.block;
 
 import com.denizenscript.denizen.objects.InventoryTag;
+import com.denizenscript.denizen.objects.ItemTag;
 import com.denizenscript.denizen.objects.LocationTag;
 import com.denizenscript.denizen.events.BukkitScriptEvent;
 import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.core.ListTag;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.BrewEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class BrewsScriptEvent extends BukkitScriptEvent implements Listener {
 
@@ -25,6 +30,8 @@ public class BrewsScriptEvent extends BukkitScriptEvent implements Listener {
     // @Context
     // <context.location> returns the LocationTag of the brewing stand.
     // <context.inventory> returns the InventoryTag of the brewing stand's contents.
+    // <context.fuel_level> returns an ElementTag of the brewing stand's fuel.
+    // <context.result> returns a ListTag(ItemTag) of the brewing stand's result.
     //
     // -->
 
@@ -48,6 +55,16 @@ public class BrewsScriptEvent extends BukkitScriptEvent implements Listener {
         switch (name) {
             case "location": return location;
             case "inventory": return InventoryTag.mirrorBukkitInventory(event.getContents());
+            case "fuel_level": return new ElementTag(event.getFuelLevel());
+            case "result":
+                ListTag results = new ListTag();
+                for (ItemStack itemStack : event.getResults()){
+                    if (itemStack == null){
+                        results.addObject(new ItemTag(Material.AIR));
+                    }
+                    results.addObject(new ItemTag(itemStack));
+                }
+                return results;
         }
         return super.getContext(name);
     }
