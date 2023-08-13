c3e8c29a6731f7ca74041d2204d03f4edf11f11d
pobab <43501409+pobab@users.noreply.github.com>
2023-08-13 20:59:25 +0800

    import cleanup

--- plugin/src/main/java/com/denizenscript/denizen/events/block/BrewsScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/block/BrewsScriptEvent.java
@@ -1,17 +1,15 @@
 package com.denizenscript.denizen.events.block;
 
+import com.denizenscript.denizen.events.BukkitScriptEvent;
 import com.denizenscript.denizen.objects.InventoryTag;
 import com.denizenscript.denizen.objects.ItemTag;
 import com.denizenscript.denizen.objects.LocationTag;
-import com.denizenscript.denizen.events.BukkitScriptEvent;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.core.ListTag;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.BrewEvent;
-import org.bukkit.inventory.ItemStack;
 
 public class BrewsScriptEvent extends BukkitScriptEvent implements Listener {
 
