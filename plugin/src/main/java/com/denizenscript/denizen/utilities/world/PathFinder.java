903e6de344c61617a69db410b5e51acc06f97acb
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-08-16 18:07:38 -0700

    add debug to pathfinder code

--- plugin/src/main/java/com/denizenscript/denizen/utilities/world/PathFinder.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/world/PathFinder.java
@@ -1,6 +1,7 @@
 package com.denizenscript.denizen.utilities.world;
 
 import com.denizenscript.denizen.objects.LocationTag;
+import com.denizenscript.denizencore.utilities.debugging.Debug;
 import net.citizensnpcs.api.astar.AStarMachine;
 import net.citizensnpcs.api.astar.pathfinder.*;
 import org.bukkit.Location;
@@ -18,10 +19,13 @@ public class PathFinder {
 
     public static List<LocationTag> getPath(Location start, Location dest) {
         VectorGoal goal = new VectorGoal(dest, 1);
-        Path plan = (Path) ASTAR.runFully(goal,
-                new VectorNode(goal, start, new ChunkBlockSource(start, 100), new MinecraftBlockExaminer()),
-                50000);
-        if (plan == null || plan.isComplete()) {
+        Path plan = (Path) ASTAR.runFully(goal, new VectorNode(goal, start, new ChunkBlockSource(start, 100), new MinecraftBlockExaminer()), 50000);
+        if (plan == null) {
+            Debug.verboseLog("PathFinder: No path found from " + start + " to " + dest + " (return null)");
+            return new ArrayList<>();
+        }
+        if (plan.isComplete()) {
+            Debug.verboseLog("PathFinder: path from " + start + " to " + dest + " is instantly completed");
             return new ArrayList<>();
         }
         else {
