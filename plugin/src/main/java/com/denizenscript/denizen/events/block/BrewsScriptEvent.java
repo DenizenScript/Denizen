874810183ebe5f756156530ac4f4473934c64691
pobab <43501409+pobab@users.noreply.github.com>
2023-08-22 19:51:20 +0800

    Brew event: fuel and result controls (#2519)
    * support color to filled map item
    
    * add condition to check filled map item doesn't have color
    
    * add mapMeta to getPropertyString
    
    * return null if filled_map doesn't have color
    
    * tidy up parentheses
    
    * clean up parentheses
    
    * add new context fuel_level & result on BrewingStand event
    
    * clarify documentation
    
    * using convertor constructor
    
    * import cleanup
    
    * register new event
    
    * register determination
    
    * Revert "register new event"
    
    This reverts commit 6be4a1d2b76806eb59a4c48995892cbca28cf2a8.
    
    * tidy up space on `registerDetermination`
    
    * alter `listTag` to `result`
    
    * add space between `)){`

--- plugin/src/main/java/com/denizenscript/denizen/events/block/BrewsScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/block/BrewsScriptEvent.java
@@ -1,9 +1,12 @@
 package com.denizenscript.denizen.events.block;
 
-import com.denizenscript.denizen.objects.InventoryTag;
-import com.denizenscript.denizen.objects.LocationTag;
 import com.denizenscript.denizen.events.BukkitScriptEvent;
+import com.denizenscript.denizen.objects.InventoryTag;
+import com.denizenscript.denizen.objects.ItemTag;
+import com.denizenscript.denizen.objects.LocationTag;
 import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.core.ListTag;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.BrewEvent;
@@ -25,11 +28,22 @@ public class BrewsScriptEvent extends BukkitScriptEvent implements Listener {
     // @Context
     // <context.location> returns the LocationTag of the brewing stand.
     // <context.inventory> returns the InventoryTag of the brewing stand's contents.
+    // <context.fuel_level> returns an ElementTag(Number) of the brewing stand's fuel level.
+    // <context.result> returns a ListTag(ItemTag) of the items that will be brewed.
+    //
+    // @Determine
+    // "RESULT:<ListTag(ItemTag)>" to change the items that are brewed.
     //
     // -->
 
     public BrewsScriptEvent() {
         registerCouldMatcher("brewing stand brews");
+        this.<BrewsScriptEvent, ListTag>registerDetermination("result", ListTag.class, (evt, context, result) -> {
+            evt.event.getResults().clear();
+            for (ItemTag item : result.filter(ItemTag.class, context)) {
+                evt.event.getResults().add(item.getItemStack());
+            }
+        });
     }
 
     public LocationTag location;
@@ -45,11 +59,13 @@ public class BrewsScriptEvent extends BukkitScriptEvent implements Listener {
 
     @Override
     public ObjectTag getContext(String name) {
-        switch (name) {
-            case "location": return location;
-            case "inventory": return InventoryTag.mirrorBukkitInventory(event.getContents());
-        }
-        return super.getContext(name);
+        return switch (name) {
+            case "location" -> location;
+            case "inventory" -> InventoryTag.mirrorBukkitInventory(event.getContents());
+            case "fuel_level" -> new ElementTag(event.getFuelLevel());
+            case "result" -> new ListTag(event.getResults(), ItemTag::new);
+            default -> super.getContext(name);
+        };
     }
 
     @EventHandler
