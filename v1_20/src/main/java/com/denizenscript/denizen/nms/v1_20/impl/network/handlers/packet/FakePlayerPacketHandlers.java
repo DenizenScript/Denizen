62bf7fc82fc26f7d91d943560b8653b88fef840f
Aya <31237389+tal5@users.noreply.github.com>
2023-09-02 23:21:59 +0100

    Simplify fake player packet handler

--- v1_20/src/main/java/com/denizenscript/denizen/nms/v1_20/impl/network/handlers/packet/FakePlayerPacketHandlers.java
+++ v1_20/src/main/java/com/denizenscript/denizen/nms/v1_20/impl/network/handlers/packet/FakePlayerPacketHandlers.java
@@ -6,7 +6,6 @@ import com.denizenscript.denizen.nms.v1_20.impl.network.handlers.DenizenNetworkM
 import net.minecraft.network.protocol.game.ClientboundAddPlayerPacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoRemovePacket;
 import net.minecraft.network.protocol.game.ClientboundPlayerInfoUpdatePacket;
-import net.minecraft.world.entity.Entity;
 import org.bukkit.Bukkit;
 
 import java.util.List;
@@ -19,13 +18,7 @@ public class FakePlayerPacketHandlers {
 
     public static void processAddPlayerPacket(DenizenNetworkManagerImpl networkManager, ClientboundAddPlayerPacket addPlayerPacket) {
         int id = addPlayerPacket.getEntityId();
-        if (id != -1) {
-            processFakePlayerSpawn(networkManager, networkManager.player.level().getEntity(id));
-        }
-    }
-
-    public static void processFakePlayerSpawn(DenizenNetworkManagerImpl networkManager, Entity entity) {
-        if (entity instanceof EntityFakePlayerImpl fakePlayer) {
+        if (id != -1 && networkManager.player.level().getEntity(id) instanceof EntityFakePlayerImpl fakePlayer) {
             networkManager.send(new ClientboundPlayerInfoUpdatePacket(ClientboundPlayerInfoUpdatePacket.Action.ADD_PLAYER, fakePlayer));
             Bukkit.getScheduler().runTaskLater(NMSHandler.getJavaPlugin(),
                     () -> networkManager.send(new ClientboundPlayerInfoRemovePacket(List.of(fakePlayer.getUUID()))), 5);
