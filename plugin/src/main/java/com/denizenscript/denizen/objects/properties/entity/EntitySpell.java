7aeab780df9113c19bfd99c4e6d4ae2a9f1649cb
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2023-12-03 09:09:12 -0800

    actually just update EntitySpell to modern syntax
    also fix a couple mistakes in prior commit

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntitySpell.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntitySpell.java
@@ -1,47 +1,38 @@
 package com.denizenscript.denizen.objects.properties.entity;
 
 import com.denizenscript.denizen.objects.EntityTag;
-import com.denizenscript.denizen.objects.NPCTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.ObjectTag;
-import com.denizenscript.denizencore.objects.properties.Property;
-import com.denizenscript.denizencore.tags.Attribute;
 import net.citizensnpcs.trait.versioned.SpellcasterTrait;
 import org.bukkit.entity.Spellcaster;
 
-public class EntitySpell implements Property {
+public class EntitySpell extends EntityProperty<ElementTag> {
 
-    public static boolean describes(ObjectTag entity) {
-        return entity instanceof EntityTag && ((EntityTag) entity).getBukkitEntity() instanceof Spellcaster;
+    // <--[property]
+    // @object EntityTag
+    // @name spell
+    // @input ElementTag
+    // @description
+    // Controls the spell that an Illager entity (such as an Evoker or Illusioner) should cast.
+    // Valid spells are: <@link url https://hub.spigotmc.org/javadocs/spigot/org/bukkit/entity/Spellcaster.Spell.html>
+    // -->
+
+    public static boolean describes(EntityTag entity) {
+        return entity.getBukkitEntity() instanceof Spellcaster;
     }
-
-    public static EntitySpell getFrom(ObjectTag entity) {
-        if (!describes(entity)) {
-            return null;
-        }
-        else {
-            return new EntitySpell((EntityTag) entity);
-        }
-    }
-
-    public static final String[] handledTags = new String[] {
-            "spell"
-    };
-
-    public static final String[] handledMechs = new String[] {
-            "spell"
-    };
-
-    public EntitySpell(EntityTag entity) {
-        dentity = entity;
-    }
-
-    EntityTag dentity;
 
     @Override
-    public String getPropertyString() {
-        return ((Spellcaster) dentity.getBukkitEntity()).getSpell().toString();
+    public ElementTag getPropertyValue() {
+        return new ElementTag(as(Spellcaster.class).getSpell());
+    }
+
+    @Override
+    public void setPropertyValue(ElementTag param, Mechanism mechanism) {
+        Spellcaster.Spell spell = param.asEnum(Spellcaster.Spell.class);
+        if (object.isCitizensNPC()) {
+            object.getDenizenNPC().getCitizen().getOrAddTrait(SpellcasterTrait.class).setSpell(spell);
+        }
+        as(Spellcaster.class).setSpell(spell);
     }
 
     @Override
@@ -49,52 +40,7 @@ public class EntitySpell implements Property {
         return "spell";
     }
 
-    @Override
-    public ObjectTag getObjectAttribute(Attribute attribute) {
-
-        if (attribute == null) {
-            return null;
-        }
-
-        // <--[tag]
-        // @attribute <EntityTag.spell>
-        // @returns ElementTag
-        // @mechanism EntityTag.spell
-        // @group properties
-        // @description
-        // Returns the spell the entity is currently casting.
-        // Can be: <@link url https://hub.spigotmc.org/javadocs/spigot/org/bukkit/entity/Spellcaster.Spell.html>
-        // -->
-        if (attribute.startsWith("spell")) {
-            return new ElementTag(((Spellcaster) dentity.getBukkitEntity()).getSpell())
-                    .getObjectAttribute(attribute.fulfill(1));
-        }
-
-        return null;
-    }
-
-    @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object EntityTag
-        // @name spell
-        // @input ElementTag
-        // @description
-        // Sets the spell the entity should cast. Valid spells are: <@link url https://hub.spigotmc.org/javadocs/spigot/org/bukkit/entity/Spellcaster.Spell.html>
-        // @tags
-        // <EntityTag.spell>
-        // -->
-        if (mechanism.matches("spell") && mechanism.requireEnum(Spellcaster.Spell.class)) {
-            Spellcaster.Spell spell = mechanism.value.asElement().asEnum(Spellcaster.Spell.class);
-            Spellcaster ent = (Spellcaster) dentity.getBukkitEntity();
-            if (EntityTag.isCitizensNPC(ent)) {
-                NPCTag npc = NPCTag.fromEntity(ent);
-                npc.getCitizen().getOrAddTrait(SpellcasterTrait.class).setSpell(spell);
-            }
-            else {
-                ent.setSpell(spell);
-            }
-        }
+    public static void register() {
+        autoRegister("spell", EntitySpell.class, ElementTag.class, false);
     }
 }
