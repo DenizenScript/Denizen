023bda77dae2a010e3fe023467c5a2da18091932
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2024-07-19 14:26:13 -0700

    patch for AttributeModifier UUID values

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/item/ItemAttributeModifiers.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/item/ItemAttributeModifiers.java
@@ -159,11 +159,10 @@ public class ItemAttributeModifiers extends ItemProperty<MapTag> {
                 }
             }
             for (String toRemove : inputList) {
-                UUID id = UUID.fromString(toRemove);
                 Multimap<org.bukkit.attribute.Attribute, AttributeModifier> metaMap = meta.getAttributeModifiers();
                 for (org.bukkit.attribute.Attribute attribute : metaMap.keys()) {
                     for (AttributeModifier modifer : metaMap.get(attribute)) {
-                        if (modifer.getUniqueId().equals(id)) {
+                        if (getIdString(modifer).equals(toRemove)) {
                             meta.removeAttributeModifier(attribute, modifer);
                             break;
                         }
@@ -172,5 +171,14 @@ public class ItemAttributeModifiers extends ItemProperty<MapTag> {
             }
             prop.setItemMeta(meta);
         });
+    }
+
+    public static String getIdString(AttributeModifier modifier) {
+        if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_21)) {
+            return modifier.getName();
+        }
+        else {
+            return modifier.getUniqueId().toString();
+        }
     }
 }
