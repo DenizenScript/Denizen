153a6acc722cef5b41d6bbe4e21a23d0ee63a648
Hydroxycobalamin <54799651+Hydroxycobalamin@users.noreply.github.com>
2024-08-20 14:17:17 +0200

    remove: redundant duplicates

--- paper/src/main/java/com/denizenscript/denizen/paper/events/PlayerQuitsScriptEventPaperImpl.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/events/PlayerQuitsScriptEventPaperImpl.java
@@ -4,20 +4,12 @@ import com.denizenscript.denizen.events.player.PlayerQuitsScriptEvent;
 import com.denizenscript.denizen.paper.PaperModule;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
-import net.md_5.bungee.api.ChatColor;
 
 
 public class PlayerQuitsScriptEventPaperImpl extends PlayerQuitsScriptEvent {
 
     public PlayerQuitsScriptEventPaperImpl() {
         registerSwitches("cause");
-        this.<PlayerQuitsScriptEventPaperImpl>registerTextDetermination("none", (evt) -> {
-            event.quitMessage(null);
-        });
-        this.<PlayerQuitsScriptEventPaperImpl, ElementTag>registerOptionalDetermination(null, ElementTag.class, (evt, context, determination) -> {
-            event.quitMessage(PaperModule.parseFormattedText(determination.toString(), ChatColor.WHITE));
-            return true;
-        });
     }
 
     @Override
@@ -31,7 +23,6 @@ public class PlayerQuitsScriptEventPaperImpl extends PlayerQuitsScriptEvent {
     @Override
     public ObjectTag getContext(String name) {
         return switch (name) {
-            case "message" -> new ElementTag(PaperModule.stringifyComponent(event.quitMessage()));
             case "cause" -> new ElementTag(event.getReason());
             default -> super.getContext(name);
         };
