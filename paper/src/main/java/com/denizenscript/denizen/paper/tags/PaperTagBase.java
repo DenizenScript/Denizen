42ca91b98c63d8bec88f9bbca458c0d6a6798a5a
david <89749147+davight@users.noreply.github.com>
2024-08-23 20:42:59 +0200

    update

--- paper/src/main/java/com/denizenscript/denizen/paper/tags/PaperTagBase.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/tags/PaperTagBase.java
@@ -2,29 +2,19 @@ package com.denizenscript.denizen.paper.tags;
 
 import com.denizenscript.denizencore.objects.core.DurationTag;
 import com.denizenscript.denizencore.objects.core.ListTag;
-import com.denizenscript.denizencore.tags.TagRunnable;
-import com.denizenscript.denizencore.tags.Attribute;
-import com.denizenscript.denizencore.tags.ReplaceableTagEvent;
-import com.denizenscript.denizencore.tags.TagManager;
+import com.denizenscript.denizencore.tags.*;
 import org.bukkit.Bukkit;
 
-public class PaperTagBase {
+public class PaperTagBase extends PseudoObjectTagBase<PaperTagBase> {
+
+    public static PaperTagBase instance;
 
     public PaperTagBase() {
-        TagManager.registerTagHandler(new TagRunnable.RootForm() {
-            @Override
-            public void run(ReplaceableTagEvent event) {
-                paperTag(event);
-            }
-        }, "paper");
+        instance = this;
+        TagManager.registerStaticTagBaseHandler(PaperTagBase.class, "paper", (t) -> instance);
     }
 
-    public void paperTag(ReplaceableTagEvent event) {
-        if (!event.matches("paper") || event.replaced()) {
-            return;
-        }
-
-        Attribute attribute = event.getAttributes().fulfill(1);
+    public void register() {
 
         // <--[tag]
         // @attribute <paper.tick_times>
@@ -34,13 +24,12 @@ public class PaperTagBase {
         // Returns a sample of the server's last 5s of tick times as a list of durations.
         // On average, a tick should take 50ms or less for a stable 20tps.
         // -->
-        if (attribute.startsWith("tick_times")) {
-            ListTag list = new ListTag();
+        tagProcessor.registerTag(ListTag.class, "tick_times", (attribute, object) -> {
+            ListTag list = new ListTag(3);
             for (long time : Bukkit.getServer().getTickTimes()) {
                 list.addObject(new DurationTag(time / 1000000000D));
             }
-            event.setReplacedObject(list.getObjectAttribute(attribute.fulfill(1)));
-            return;
-        }
+            return list;
+        });
     }
 }
