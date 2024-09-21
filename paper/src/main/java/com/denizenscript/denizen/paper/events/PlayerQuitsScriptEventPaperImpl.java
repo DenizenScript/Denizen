4d11c7a194310c8e48948100bae17fc77e093414
Hydroxycobalamin <54799651+Hydroxycobalamin@users.noreply.github.com>
2024-09-21 17:22:56 +0200

    monkey is dum and needs to read docs before replying to questions

--- paper/src/main/java/com/denizenscript/denizen/paper/events/PlayerQuitsScriptEventPaperImpl.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/events/PlayerQuitsScriptEventPaperImpl.java
@@ -1,14 +1,23 @@
 package com.denizenscript.denizen.paper.events;
 
 import com.denizenscript.denizen.events.player.PlayerQuitsScriptEvent;
+import com.denizenscript.denizen.paper.PaperModule;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
+import net.md_5.bungee.api.ChatColor;
 
 
 public class PlayerQuitsScriptEventPaperImpl extends PlayerQuitsScriptEvent {
 
     public PlayerQuitsScriptEventPaperImpl() {
         registerSwitches("cause");
+        this.<PlayerQuitsScriptEventPaperImpl>registerTextDetermination("none", (evt) -> {
+            event.quitMessage(null);
+        });
+        this.<PlayerQuitsScriptEventPaperImpl, ElementTag>registerOptionalDetermination(null, ElementTag.class, (evt, context, determination) -> {
+            event.quitMessage(PaperModule.parseFormattedText(determination.toString(), ChatColor.WHITE));
+            return true;
+        });
     }
 
     @Override
@@ -22,6 +31,7 @@ public class PlayerQuitsScriptEventPaperImpl extends PlayerQuitsScriptEvent {
     @Override
     public ObjectTag getContext(String name) {
         return switch (name) {
+            case "message" -> new ElementTag(PaperModule.stringifyComponent(event.quitMessage()));
             case "cause" -> new ElementTag(event.getReason());
             default -> super.getContext(name);
         };
