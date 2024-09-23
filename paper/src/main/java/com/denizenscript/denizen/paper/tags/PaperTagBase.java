e81c469d72939364d59e9ae9e2aa4a89d26a7289
david <89749147+davight@users.noreply.github.com>
2024-09-23 08:15:00 +0200

    fix

--- paper/src/main/java/com/denizenscript/denizen/paper/tags/PaperTagBase.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/tags/PaperTagBase.java
@@ -14,6 +14,7 @@ public class PaperTagBase extends PseudoObjectTagBase<PaperTagBase> {
         TagManager.registerStaticTagBaseHandler(PaperTagBase.class, "paper", (t) -> instance);
     }
 
+    @Override
     public void register() {
 
         // <--[tag]
