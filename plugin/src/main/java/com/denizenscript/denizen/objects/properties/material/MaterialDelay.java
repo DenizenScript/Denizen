61668f6f3f25124aed90f4f28fa8771101b6d975
hyper <honkhonk4456@gmail.com>
2024-10-31 18:36:30 -0400

    Modernized delay property

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialDelay.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialDelay.java
@@ -2,52 +2,55 @@ package com.denizenscript.denizen.objects.properties.material;
 
 import com.denizenscript.denizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
-import com.denizenscript.denizencore.objects.properties.Property;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Repeater;
 
-public class MaterialDelay implements Property {
+public class MaterialDelay extends MaterialProperty<ElementTag> {
 
-    public static boolean describes(ObjectTag material) {
-        return material instanceof MaterialTag
-                && ((MaterialTag) material).hasModernData()
-                && ((MaterialTag) material).getModernData() instanceof Repeater;
-    }
+    // <--[property]
+    // @object MaterialTag
+    // @name delay
+    // @input ElementTag(Number)
+    // @description
+    // Controls the delay of a redstone repeater material.
+    // To get the maximum delay of the repeater, see <MaterialTag.max_delay>.
+    // To get the minimum delay of the repeater, see <MaterialTag.min_delay>.
+    // -->
 
-    public static MaterialDelay getFrom(ObjectTag _material) {
-        if (!describes(_material)) {
-            return null;
-        }
-        else {
-            return new MaterialDelay((MaterialTag) _material);
-        }
-    }
-
-    public static final String[] handledMechs = new String[] {
-            "delay"
-    };
-
-    public MaterialDelay(MaterialTag _material) {
-        material = _material;
+    public static boolean describes(MaterialTag material) {
+        BlockData data = material.getModernData();
+        return data instanceof Repeater;
     }
 
     MaterialTag material;
 
-    public static void register() {
+    @Override
+    public String getPropertyId() {
+        return "delay";
+    }
 
-        // <--[tag]
-        // @attribute <MaterialTag.delay>
-        // @returns ElementTag(Number)
-        // @mechanism MaterialTag.delay
-        // @group properties
-        // @description
-        // Returns the current delay of a redstone repeater material.
-        // -->
-        PropertyParser.registerStaticTag(MaterialDelay.class, ElementTag.class, "delay", (attribute, material) -> {
-            return new ElementTag(material.getCurrent());
-        });
+    @Override
+    public ElementTag getPropertyValue() {
+        return new ElementTag(getCurrent());
+    }
+
+    @Override
+    public void setPropertyValue(ElementTag value, Mechanism mechanism) {
+        if (!mechanism.requireInteger()) {
+            return;
+        }
+        int delay = value.asInt();
+        if (delay < getMin() || delay > getMax()) {
+            mechanism.echoError("Delay value '" + delay + "' is not valid. Must be between " + getMin() + " and " + getMax() + ".");
+            return;
+        }
+        getRepeater().setDelay(delay);
+    }
+
+    public static void register() {
+        autoRegister("delay", MaterialDelay.class, ElementTag.class, true);
 
         // <--[tag]
         // @attribute <MaterialTag.max_delay>
@@ -89,39 +92,5 @@ public class MaterialDelay implements Property {
 
     public int getMin() {
         return getRepeater().getMinimumDelay();
-    }
-
-    @Override
-    public String getPropertyString() {
-        return String.valueOf(getCurrent());
-    }
-
-    @Override
-    public String getPropertyId() {
-        return "delay";
-    }
-
-    @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object MaterialTag
-        // @name delay
-        // @input ElementTag(Number)
-        // @description
-        // Sets the delay of a redstone repeater material.
-        // @tags
-        // <MaterialTag.delay>
-        // <MaterialTag.max_delay>
-        // <MaterialTag.min_delay>
-        // -->
-        if (mechanism.matches("delay") && mechanism.requireInteger()) {
-            int delay = mechanism.getValue().asInt();
-            if (delay < getMin() || delay > getMax()) {
-                mechanism.echoError("Delay value '" + delay + "' is not valid. Must be between " + getMin() + " and " + getMax() + ".");
-                return;
-            }
-            getRepeater().setDelay(delay);
-        }
     }
 }
