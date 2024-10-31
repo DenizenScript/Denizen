11160f8264ae06bf271d9742862bb71919bbd1fe
hyper <39167664+heypr@users.noreply.github.com>
2024-10-31 11:33:59 -0400

    Modernized snowable property

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialSnowable.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialSnowable.java
@@ -2,52 +2,29 @@ package com.denizenscript.denizen.objects.properties.material;
 
 import com.denizenscript.denizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
-import com.denizenscript.denizencore.objects.properties.Property;
-import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.Snowable;
 
-public class MaterialSnowable implements Property {
+public class MaterialSnowable extends MaterialProperty<ElementTag> {
 
-    public static boolean describes(ObjectTag material) {
-        return material instanceof MaterialTag
-                && ((MaterialTag) material).hasModernData()
-                && ((MaterialTag) material).getModernData() instanceof Snowable;
-    }
+    // <--[tag]
+    // @object MaterialTag
+    // @name snowy
+    // @input ElementTag(Boolean)
+    // @description
+    // Controls whether this material is covered in snow.
+    // -->
 
-    public static MaterialSnowable getFrom(ObjectTag _material) {
-        if (!describes(_material)) {
-            return null;
-        }
-        else {
-            return new MaterialSnowable((MaterialTag) _material);
-        }
-    }
-
-    public static final String[] handledMechs = new String[] {
-            "snowy"
-    };
-
-    public MaterialSnowable(MaterialTag _material) {
-        material = _material;
+    public static boolean describes(MaterialTag material) {
+        BlockData data = material.getModernData();
+        return data instanceof Snowable;
     }
 
     MaterialTag material;
 
     public static void register() {
-
-        // <--[tag]
-        // @attribute <MaterialTag.snowy>
-        // @returns ElementTag(Boolean)
-        // @mechanism MaterialTag.snowy
-        // @group properties
-        // @description
-        // Returns whether this material is covered in snow or not.
-        // -->
-        PropertyParser.registerStaticTag(MaterialSnowable.class, ElementTag.class, "snowy", (attribute, material) -> {
-            return new ElementTag(material.isSnowy());
-        });
+        autoRegister("snowy", MaterialSnowable.class, ElementTag.class, true);
     }
 
     public Snowable getSnowable() {
@@ -59,29 +36,20 @@ public class MaterialSnowable implements Property {
     }
 
     @Override
-    public String getPropertyString() {
-        return String.valueOf(isSnowy());
+    public ElementTag getPropertyValue() {
+        return new ElementTag(isSnowy());
+    }
+
+    @Override
+    public void setPropertyValue(ElementTag value, Mechanism mechanism) {
+        if (!mechanism.requireBoolean()) {
+            return;
+        }
+        getSnowable().setSnowy(value.asBoolean());
     }
 
     @Override
     public String getPropertyId() {
         return "snowy";
-    }
-
-    @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object MaterialTag
-        // @name snowy
-        // @input ElementTag(Boolean)
-        // @description
-        // Sets this material to be covered in snow, or not.
-        // @tags
-        // <MaterialTag.snowy>
-        // -->
-        if (mechanism.matches("snowy") && mechanism.requireBoolean()) {
-            getSnowable().setSnowy(mechanism.getValue().asBoolean());
-        }
     }
 }
