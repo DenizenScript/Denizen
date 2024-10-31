98f656aa6b39f713ece1fe80b27abda950f7cd44
hyper <honkhonk4456@gmail.com>
2024-10-31 18:07:44 -0400

    Modernized locked property

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialLocked.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialLocked.java
@@ -2,65 +2,30 @@ package com.denizenscript.denizen.objects.properties.material;
 
 import com.denizenscript.denizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
-import com.denizenscript.denizencore.objects.properties.Property;
-import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Repeater;
 
-public class MaterialLocked implements Property {
+public class MaterialLocked extends MaterialProperty<ElementTag> {
 
-    public static boolean describes(ObjectTag material) {
-        return material instanceof MaterialTag
-                && ((MaterialTag) material).hasModernData()
-                && ((MaterialTag) material).getModernData() instanceof Repeater;
-    }
+    // <--[property]
+    // @object MaterialTag
+    // @name is_locked
+    // @input ElementTag(Boolean)
+    // @description
+    // Controls whether this redstone repeater material is locked.
+    // -->
 
-    public static MaterialLocked getFrom(ObjectTag _material) {
-        if (!describes(_material)) {
-            return null;
-        }
-        else {
-            return new MaterialLocked((MaterialTag) _material);
-        }
-    }
-
-    public static final String[] handledMechs = new String[] {
-            "is_locked"
-    };
-
-    public MaterialLocked(MaterialTag _material) {
-        material = _material;
+    public static boolean describes(MaterialTag material) {
+        BlockData data = material.getModernData();
+        return data instanceof Repeater;
     }
 
     MaterialTag material;
 
-    public static void register() {
-
-        // <--[tag]
-        // @attribute <MaterialTag.is_locked>
-        // @returns ElementTag(Boolean)
-        // @mechanism MaterialTag.is_locked
-        // @group properties
-        // @description
-        // Returns whether this redstone repeater material is locked.
-        // -->
-        PropertyParser.registerStaticTag(MaterialLocked.class, ElementTag.class, "is_locked", (attribute, material) -> {
-            return new ElementTag(material.isLocked());
-        });
-    }
-
-    public Repeater getRepeater() {
-        return (Repeater) material.getModernData();
-    }
-
-    public boolean isLocked() {
-        return getRepeater().isLocked();
-    }
-
     @Override
-    public String getPropertyString() {
-        return String.valueOf(isLocked());
+    public ElementTag getPropertyValue() {
+        return new ElementTag(isLocked());
     }
 
     @Override
@@ -69,19 +34,21 @@ public class MaterialLocked implements Property {
     }
 
     @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object MaterialTag
-        // @name is_locked
-        // @input ElementTag(Boolean)
-        // @description
-        // Sets this redstone repeater material to be locked.
-        // @tags
-        // <MaterialTag.is_locked>
-        // -->
-        if (mechanism.matches("is_locked") && mechanism.requireBoolean()) {
-            getRepeater().setLocked(mechanism.getValue().asBoolean());
+    public void setPropertyValue(ElementTag value, Mechanism mechanism) {
+        if (mechanism.requireBoolean()) {
+            getRepeater().setLocked(value.asBoolean());
         }
+    }
+
+    public static void register() {
+        autoRegister("is_locked", MaterialLocked.class, ElementTag.class, true);
+    }
+
+    public Repeater getRepeater() {
+        return (Repeater) material.getModernData();
+    }
+
+    public boolean isLocked() {
+        return getRepeater().isLocked();
     }
 }
