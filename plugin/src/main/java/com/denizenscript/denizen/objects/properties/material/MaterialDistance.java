7ffc2e3cb191babda336421056fe4ab4aa72f662
hyper <honkhonk4456@gmail.com>
2024-10-31 18:09:53 -0400

    Modernized distance property

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialDistance.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialDistance.java
@@ -2,55 +2,61 @@ package com.denizenscript.denizen.objects.properties.material;
 
 import com.denizenscript.denizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
-import com.denizenscript.denizencore.objects.properties.Property;
-import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Leaves;
 import org.bukkit.block.data.type.Scaffolding;
 
-public class MaterialDistance implements Property {
+public class MaterialDistance extends MaterialProperty<ElementTag> {
 
-    public static boolean describes(Object material) {
-        return material instanceof MaterialTag
-                && ((MaterialTag) material).hasModernData()
-                && (((MaterialTag) material).getModernData() instanceof Scaffolding
-                || ((MaterialTag) material).getModernData() instanceof Leaves);
-    }
+    // <--[property]
+    // @object MaterialTag
+    // @name distance
+    // @input ElementTag(Number)
+    // @description
+    // Controls the horizontal distance between a scaffolding block and the nearest scaffolding block placed above a 'bottom' scaffold,
+    // or between a leaves block and the nearest log (a distance of 7 will cause a leaf to decay if 'persistent' is also false, less than 7 will prevent decay).
+    // -->
 
-    public static MaterialDistance getFrom(ObjectTag _material) {
-        if (!describes(_material)) {
-            return null;
-        }
-        else {
-            return new MaterialDistance((MaterialTag) _material);
-        }
-    }
-
-    public static final String[] handledMechs = new String[] {
-            "distance"
-    };
-
-    public MaterialDistance(MaterialTag _material) {
-        material = _material;
+    public static boolean describes(MaterialTag material) {
+        BlockData data = material.getModernData();
+        return data instanceof Scaffolding
+                || data instanceof Leaves;
     }
 
     MaterialTag material;
 
-    public static void register() {
+    @Override
+    public String getPropertyId() {
+        return "distance";
+    }
 
-        // <--[tag]
-        // @attribute <MaterialTag.distance>
-        // @returns ElementTag(Number)
-        // @mechanism MaterialTag.distance
-        // @group properties
-        // @description
-        // Returns the horizontal distance between a scaffolding block and the nearest scaffolding block placed above a 'bottom' scaffold,
-        // or between a leaves block and the nearest log (a distance of 7 will cause a leaf to decay if 'persistent' is also false, less than 7 will prevent decay).
-        // -->
-        PropertyParser.registerStaticTag(MaterialDistance.class, ElementTag.class, "distance", (attribute, material) -> {
-            return new ElementTag(material.getDistance());
-        });
+    @Override
+    public ElementTag getPropertyValue() {
+        return new ElementTag(getDistance());
+    }
+
+    @Override
+    public void setPropertyValue(ElementTag value, Mechanism mechanism) {
+        if (!mechanism.requireInteger()) {
+            return;
+        }
+        int distance = value.asInt();
+        if (isScaffolding()) {
+            if (distance >= 0 && distance <= getScaffolding().getMaximumDistance()) {
+                getScaffolding().setDistance(distance);
+            }
+            else {
+                mechanism.echoError("Distance must be between 0 and " + getScaffolding().getMaximumDistance());
+            }
+        }
+        else if (isLeaves()) {
+            getLeaves().setDistance(distance);
+        }
+    }
+
+    public static void register() {
+        autoRegister("distance", MaterialDistance.class, ElementTag.class, true);
     }
 
     public int getDistance() {
@@ -77,43 +83,5 @@ public class MaterialDistance implements Property {
 
     public boolean isLeaves() {
         return material.getModernData() instanceof Leaves;
-    }
-
-    @Override
-    public String getPropertyString() {
-        return String.valueOf(getDistance());
-    }
-
-    @Override
-    public String getPropertyId() {
-        return "distance";
-    }
-
-    @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object MaterialTag
-        // @name distance
-        // @input ElementTag(Number)
-        // @description
-        // Sets the horizontal distance between a scaffolding block and the nearest scaffolding block placed above a 'bottom' scaffold, or between a leaves block and the nearest log.
-        // @tags
-        // <MaterialTag.distance>
-        // -->
-        if (mechanism.matches("distance") && mechanism.requireInteger()) {
-            int distance = mechanism.getValue().asInt();
-            if (isScaffolding()) {
-                if (distance >= 0 && distance <= getScaffolding().getMaximumDistance()) {
-                    getScaffolding().setDistance(distance);
-                }
-                else {
-                    mechanism.echoError("Distance must be between 0 and " + getScaffolding().getMaximumDistance());
-                }
-            }
-            else if (isLeaves()) {
-                getLeaves().setDistance(distance);
-            }
-        }
     }
 }
