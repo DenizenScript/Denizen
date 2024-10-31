c674f7adbed5030231405111215e05c2bf2a6851
hyper <honkhonk4456@gmail.com>
2024-10-31 18:07:32 -0400

    Modernized note property

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialNote.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/material/MaterialNote.java
@@ -2,41 +2,49 @@ package com.denizenscript.denizen.objects.properties.material;
 
 import com.denizenscript.denizen.objects.MaterialTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
-import com.denizenscript.denizencore.objects.properties.Property;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
 import org.bukkit.Note;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.NoteBlock;
 
-public class MaterialNote implements Property {
+public class MaterialNote extends MaterialProperty<ElementTag> {
 
-    public static boolean describes(ObjectTag material) {
-        return material instanceof MaterialTag
-                && ((MaterialTag) material).hasModernData()
-                && ((MaterialTag) material).getModernData() instanceof NoteBlock;
-    }
+    // <--[property]
+    // @object MaterialTag
+    // @name note
+    // @input ElementTag(Number)
+    // @description
+    // Controls the note played from this note block, as an ID number from 0 to 24.
+    // See also <@link tag MaterialTag.note_octave> and <@link tag MaterialTag.note_tone>
+    // -->
 
-    public static MaterialNote getFrom(ObjectTag _material) {
-        if (!describes(_material)) {
-            return null;
-        }
-        else {
-            return new MaterialNote((MaterialTag) _material);
-        }
-    }
-
-    public static final String[] handledMechs = new String[] {
-            "note"
-    };
-
-    public MaterialNote(MaterialTag _material) {
-        material = _material;
+    public static boolean describes(MaterialTag material) {
+        BlockData data = material.getModernData();
+        return data instanceof NoteBlock;
     }
 
     MaterialTag material;
 
+    @Override
+    public String getPropertyId() {
+        return "note";
+    }
+
+    @Override
+    public ElementTag getPropertyValue() {
+        return new ElementTag(getNoteBlock().getNote().getId());
+    }
+
+    @Override
+    public void setPropertyValue(ElementTag value, Mechanism mechanism) {
+        if (mechanism.requireInteger()) {
+            getNoteBlock().setNote(new Note(value.asInt()));
+        }
+    }
+
     public static void register() {
+        autoRegister("note", MaterialNote.class, ElementTag.class, true);
 
         // <--[tag]
         // @attribute <MaterialTag.note_octave>
@@ -63,50 +71,9 @@ public class MaterialNote implements Property {
             Note note = material.getNoteBlock().getNote();
             return new ElementTag(note.getTone().name() + (note.isSharped() ? "#" : ""));
         });
-
-        // <--[tag]
-        // @attribute <MaterialTag.note>
-        // @returns ElementTag(Number)
-        // @mechanism MaterialTag.note
-        // @group properties
-        // @description
-        // Returns the note played from this note block, as an ID number from 0 to 24.
-        // -->
-        PropertyParser.registerStaticTag(MaterialNote.class, ElementTag.class, "note", (attribute, material) -> {
-            return new ElementTag(material.getNoteBlock().getNote().getId());
-        });
     }
 
     public NoteBlock getNoteBlock() {
         return (NoteBlock) material.getModernData();
-    }
-
-    @Override
-    public String getPropertyString() {
-        return String.valueOf(getNoteBlock().getNote().getId());
-    }
-
-    @Override
-    public String getPropertyId() {
-        return "note";
-    }
-
-    @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object MaterialTag
-        // @name note
-        // @input ElementTag(Number)
-        // @description
-        // Sets the note played from this note block, as an ID number from 0 to 24.
-        // @tags
-        // <MaterialTag.note>
-        // <MaterialTag.note_tone>
-        // <MaterialTag.note_octave>
-        // -->
-        if (mechanism.matches("note") && mechanism.requireInteger()) {
-            getNoteBlock().setNote(new Note(mechanism.getValue().asInt()));
-        }
     }
 }
