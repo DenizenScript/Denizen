09db7f8e053858ce4f315e7dc7f22077c0764fd8
MCSamuel <morrissamuelw@gmail.com>
2024-11-21 18:02:36 -0800

    - Added EntityAwake property for bats (Ref. https://discord.com/channels/315163488085475337/1301429345172193332) - Adjusted Wolf.color to include variants above 1.20 using the "Variant|Collar color" format (Ref. https://discord.com/channels/315163488085475337/1296881427618463828)

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityColor.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityColor.java
@@ -92,8 +92,31 @@ public class EntityColor extends EntityProperty<ElementTag> {
         else if (type == EntityType.SHEEP && mechanism.requireEnum(DyeColor.class)) {
             as(Sheep.class).setColor(color.asEnum(DyeColor.class));
         }
-        else if (type == EntityType.WOLF && mechanism.requireEnum(DyeColor.class)) {
-            as(Wolf.class).setCollarColor(color.asEnum(DyeColor.class));
+        else if (type == EntityType.WOLF && mechanism.requireObject(ListTag.class)) {
+            if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_20)) { // TODO: Remove when 1.20 is the minimum
+                Wolf wolf = as(Wolf.class);
+                ListTag list = mechanism.valueAsType(ListTag.class);
+                ElementTag input = list.getObject(0).asElement();
+                Wolf.Variant wolfVariety = Utilities.elementToEnumlike(input, Wolf.Variant.class);
+                if (wolfVariety != null) {
+                    wolf.setVariant(wolfVariety);
+                }
+                else {
+                    mechanism.echoError("Invalid wolf variant specified: " + input);
+                }
+                if (list.size() > 1) {
+                    ElementTag collarColor = list.getObject(1).asElement();
+                    if (collarColor.matchesEnum(DyeColor.class)) {
+                        wolf.setCollarColor(collarColor.asEnum(DyeColor.class));
+                    }
+                    else {
+                        mechanism.echoError("Invalid color specified: " + collarColor);
+                    }
+                }
+            else {
+                as(Wolf.class).setCollarColor(color.asEnum(DyeColor.class));
+                }
+            }
         }
         else if (type == EntityType.OCELOT && mechanism.requireEnum(Ocelot.Type.class)) { // TODO: Deprecate?
             as(Ocelot.class).setCatType(color.asEnum(Ocelot.Type.class));
@@ -318,6 +341,9 @@ public class EntityColor extends EntityProperty<ElementTag> {
     // For rabbits, the types are BROWN, WHITE, BLACK, BLACK_AND_WHITE, GOLD, SALT_AND_PEPPER, or THE_KILLER_BUNNY.
     // For cats (not ocelots), the format is TYPE|COLOR (see below).
     //          The types are TABBY, BLACK, RED, SIAMESE, BRITISH_SHORTHAIR, CALICO, PERSIAN, RAGDOLL, WHITE, JELLIE, and ALL_BLACK.
+    // For wolves on 1.20 and above, the format is VARIANT|COLOR (see below).
+    //          The variants are ASHEN, BLACK, CHESTNUT, PALE, RUSTY, SNOWY, SPOTTED, STRIPED, and WOODS.
+    // For wolves on 1.19 and below, the input is a Dye Color.
     // For parrots, the types are BLUE, CYAN, GRAY, GREEN, or RED.
     // For llamas, the types are CREAMY, WHITE, BROWN, and GRAY.
     // For mushroom_cows, the types are RED and BROWN.
@@ -327,7 +353,7 @@ public class EntityColor extends EntityProperty<ElementTag> {
     // For villagers and zombie_villagers, the types are DESERT, JUNGLE, PLAINS, SAVANNA, SNOW, SWAMP, and TAIGA.
     // For tropical_fish, the input is PATTERN|BODYCOLOR|PATTERNCOLOR, where BodyColor and PatterenColor are both DyeColor (see below),
     //          and PATTERN is KOB, SUNSTREAK, SNOOPER, DASHER, BRINELY, SPOTTY, FLOPPER, STRIPEY, GLITTER, BLOCKFISH, BETTY, is CLAYFISH.
-    // For sheep, wolf, and shulker entities, the input is a Dye Color.
+    // For sheep and shulker entities, the input is a Dye Color.
     // For Tipped Arrow entities, the input is a ColorTag.
     // For goats, the input is SCREAMING or NORMAL.
     // For axolotl, the types are BLUE, CYAN, GOLD, LUCY, or WILD.
