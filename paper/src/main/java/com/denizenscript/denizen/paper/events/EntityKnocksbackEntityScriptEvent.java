de6bf76bc5bbaa6660193cdaf4480945919c6ce0
MCSamuel <morrissamuelw@gmail.com>
2025-01-15 18:51:35 -0800

    version clarification

--- paper/src/main/java/com/denizenscript/denizen/paper/events/EntityKnocksbackEntityScriptEvent.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/events/EntityKnocksbackEntityScriptEvent.java
@@ -34,7 +34,8 @@ public class EntityKnocksbackEntityScriptEvent extends BukkitScriptEvent impleme
     // <context.entity> returns the EntityTag that was knocked back.
     // <context.damager> returns the EntityTag of the one who knocked.
     // <context.acceleration> returns the knockback applied as a vector.
-    // <context.cause> returns the cause of the knockback. Causes list: <@link url https://jd.papermc.io/paper/1.21.1/io/papermc/paper/event/entity/EntityKnockbackEvent.Cause.html>
+    // <context.cause> returns the cause of the knockback (only on 1.20 and above).
+    // Causes list: <@link url https://jd.papermc.io/paper/1.21.1/io/papermc/paper/event/entity/EntityKnockbackEvent.Cause.html>
     //
     // @Determine
     // LocationTag as a vector to change the acceleration applied.
