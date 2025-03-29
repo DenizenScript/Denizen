ade2fd5ca6579161707c256255c91082d8d33216
MCSamuel <morrissamuelw@gmail.com>
2025-03-29 05:15:40 -0700

    Update 6 entity properties (#2703)
    Properties: `EntityAnger`, `EntityAngry`, `EntityArms`, `EntityBasePlate`, `EntityCharged`, `EntityChestCarrier`
    
    * Modernized 6 properties
    
    * Aya update
    
    * Removed `as` calls when necessary
    Minor format fixes
    
    * meta updates & redundant `else` removed
    
    * () in meta notes removed
    
    * EntityCharged meta changes
    
    * ghast visual effect clarified
    
    * Ghast removed
    
    * defaults

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityAnger.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityAnger.java
@@ -3,46 +3,48 @@ package com.denizenscript.denizen.objects.properties.entity;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizencore.objects.core.DurationTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import com.denizenscript.denizencore.objects.ObjectTag;
-import com.denizenscript.denizencore.objects.properties.Property;
-import com.denizenscript.denizencore.objects.properties.PropertyParser;
 import org.bukkit.entity.Bee;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.PigZombie;
 
-public class EntityAnger implements Property {
+public class EntityAnger extends EntityProperty<DurationTag> {
 
-    public static boolean describes(ObjectTag entity) {
-        if (!(entity instanceof EntityTag)) {
-            return false;
-        }
-        Entity bukkitEntity = ((EntityTag) entity).getBukkitEntity();
-        return bukkitEntity instanceof PigZombie
-                || bukkitEntity instanceof Bee;
+    // <--[property]
+    // @object EntityTag
+    // @name anger
+    // @input DurationTag
+    // @description
+    // Controls the remaining anger time of a PigZombie or Bee.
+    // -->
+
+    public static boolean describes(EntityTag entity) {
+        return entity.getBukkitEntity() instanceof PigZombie
+                || entity.getBukkitEntity() instanceof Bee;
     }
-
-    public static EntityAnger getFrom(ObjectTag entity) {
-        if (!describes(entity)) {
-            return null;
-        }
-        else {
-            return new EntityAnger((EntityTag) entity);
-        }
-    }
-
-    public static final String[] handledMechs = new String[] {
-            "anger"
-    };
-
-    public EntityAnger(EntityTag entity) {
-        this.entity = entity;
-    }
-
-    EntityTag entity;
 
     @Override
-    public String getPropertyString() {
-        return new DurationTag((long) getAnger()).identify();
+    public DurationTag getPropertyValue() {
+        if (getEntity() instanceof PigZombie entity) {
+            return new DurationTag((long) entity.getAnger());
+        }
+        else if (getEntity() instanceof Bee entity) {
+            return new DurationTag((long) entity.getAnger());
+        }
+        else {
+            return null;
+        }
+    }
+
+    @Override
+    public void setPropertyValue(DurationTag param, Mechanism mechanism) {
+        if (mechanism.getValue().isInt()) { // Soft-deprecated - backwards compatibility, as this used to use a tick count
+            param = new DurationTag(mechanism.getValue().asLong());
+        }
+        if (getEntity() instanceof PigZombie entity) {
+            entity.setAnger(param.getTicksAsInt());
+        }
+        else {
+            as(Bee.class).setAnger(param.getTicksAsInt());
+        }
     }
 
     @Override
@@ -50,68 +52,7 @@ public class EntityAnger implements Property {
         return "anger";
     }
 
-    public int getAnger() {
-        if (isPigZombie()) {
-            return getPigZombie().getAnger();
-        }
-        else {
-            return getBee().getAnger();
-        }
-    }
-
-    public boolean isPigZombie() {
-        return entity.getBukkitEntity() instanceof PigZombie;
-    }
-
-    public PigZombie getPigZombie() {
-        return (PigZombie) entity.getBukkitEntity();
-    }
-
-    public Bee getBee() {
-        return (Bee) entity.getBukkitEntity();
-    }
-
     public static void register() {
-
-        // <--[tag]
-        // @attribute <EntityTag.anger>
-        // @returns DurationTag
-        // @mechanism EntityTag.anger
-        // @group properties
-        // @description
-        // Returns the remaining anger time of a PigZombie or Bee.
-        // -->
-        PropertyParser.registerTag(EntityAnger.class, DurationTag.class, "anger", (attribute, object) -> {
-            return new DurationTag((long) object.getAnger());
-        });
-    }
-
-    @Override
-    public void adjust(Mechanism mechanism) {
-
-        // <--[mechanism]
-        // @object EntityTag
-        // @name anger
-        // @input DurationTag
-        // @description
-        // Changes the remaining anger time of a PigZombie or Bee.
-        // @tags
-        // <EntityTag.anger>
-        // -->
-        if (mechanism.matches("anger") && mechanism.requireObject(DurationTag.class)) {
-            DurationTag duration;
-            if (mechanism.getValue().isInt()) { // Soft-deprecated - backwards compatibility, as this used to use a tick count
-                duration = new DurationTag(mechanism.getValue().asLong());
-            }
-            else {
-                duration = mechanism.valueAsType(DurationTag.class);
-            }
-            if (isPigZombie()) {
-                getPigZombie().setAnger(duration.getTicksAsInt());
-            }
-            else {
-                getBee().setAnger(duration.getTicksAsInt());
-            }
-        }
+        autoRegister("anger", EntityAnger.class, DurationTag.class, false);
     }
 }
