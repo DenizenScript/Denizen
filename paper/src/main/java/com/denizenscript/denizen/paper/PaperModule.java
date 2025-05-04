39078d3a4a4bca4cebcf0e1e299a67b3af1e54fa
hyper <39167664+heypr@users.noreply.github.com>
2025-05-04 07:41:14 -0400

    Add Paper-only `dragon egg forms` event (#2669)
    * Added event
    
    * Update meta
    
    * Add new meta entry + context tag
    
    * Enum constructor
    
    * remember

--- paper/src/main/java/com/denizenscript/denizen/paper/PaperModule.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/PaperModule.java
@@ -34,6 +34,7 @@ public class PaperModule {
         }
         ScriptEvent.registerScriptEvent(BlockPreDispenseScriptEvent.class);
         ScriptEvent.registerScriptEvent(CreeperIgnitesScriptEvent.class);
+        ScriptEvent.registerScriptEvent(DragonEggFormScriptEvent.class);
         ScriptEvent.registerScriptEvent(EntityAddToWorldScriptEvent.class);
         ScriptEvent.registerScriptEvent(EntityKnocksbackEntityScriptEvent.class);
         ScriptEvent.registerScriptEvent(EntityLoadCrossbowScriptEvent.class);
