0ecfd8746d7dd9d9001bb6427c283b88e5230d87
Aya <31237389+tal5@users.noreply.github.com>
2025-05-22 12:25:18 +0100

    Add TODO

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityVariant.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityVariant.java
@@ -17,6 +17,7 @@ import java.lang.invoke.MethodHandle;
 
 public class EntityVariant extends EntityProperty<ElementTag> {
 
+    // TODO: once 1.21 is the minimum supported version, replace with direct method calls
     public static final MethodHandle COW_GET_VARIANT, COW_SET_VARIANT;
 
     static {
