1648ef2b5f284d673616db7002f82142807a0294
Darwin1546 <turoktest@gmail.com>
2025-05-31 02:57:02 +0800

    property added

--- paper/src/main/java/com/denizenscript/denizen/paper/PaperModule.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/PaperModule.java
@@ -118,6 +118,9 @@ public class PaperModule {
             PropertyParser.registerProperty(EntityEggLayTime.class, EntityTag.class);
             PropertyParser.registerProperty(EntityFriction.class, EntityTag.class);
         }
+        if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_18)) {
+            PropertyParser.registerProperty(EntityFreezeTickingLocked.class, EntityTag.class);
+        }
         PropertyParser.registerProperty(EntityLeftHanded.class, EntityTag.class);
         PropertyParser.registerProperty(EntityReputation.class, EntityTag.class);
         PropertyParser.registerProperty(EntityShouldBurn.class, EntityTag.class);
---
+++ paper/src/main/java/com/denizenscript/denizen/paper/properties/EntityFreezeTickingLocked.java
@@ -0,0 +1,42 @@
+package com.denizenscript.denizen.paper.properties;
+
+import com.denizenscript.denizen.objects.EntityTag;
+import com.denizenscript.denizen.objects.properties.entity.EntityProperty;
+import com.denizenscript.denizencore.objects.Mechanism;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+
+public class EntityFreezeTickingLocked extends EntityProperty<ElementTag> {
+
+    // <--[property]
+    // @object EntityTag
+    // @name freeze_locked
+    // @input ElementTag(Boolean)
+    // @plugin Paper
+    // @description
+    // Controls whether an entity’s freeze ticks are locked to a fixed value, preventing vanilla freeze tick updates.
+    // NOTE: Clients may still show frost effects when in powder snow.
+    // -->
+
+    public static boolean describes(EntityTag entity) { return true; }
+
+    @Override
+    public ElementTag getPropertyValue() {
+        return new ElementTag(getEntity().isFreezeTickingLocked());
+    }
+
+    @Override
+    public String getPropertyId() {
+        return "freeze_locked";
+    }
+
+    @Override
+    public void setPropertyValue(ElementTag param, Mechanism mechanism) {
+        if (mechanism.requireBoolean()) {
+            getEntity().lockFreezeTicks(param.asBoolean());
+        }
+    }
+
+    public static void register() {
+        autoRegister("freeze_locked", EntityFreezeTickingLocked.class, ElementTag.class, false);
+    }
+}
