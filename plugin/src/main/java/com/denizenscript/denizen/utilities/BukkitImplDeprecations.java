03091bdd2211e31a669242326620eba7db95bf13
hyper <39167664+heypr@users.noreply.github.com>
2025-06-22 23:04:13 -0400

    Merge pull request #2659 from heypr/modernization-1
    Modernized WorldTag

--- plugin/src/main/java/com/denizenscript/denizen/utilities/BukkitImplDeprecations.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/BukkitImplDeprecations.java
@@ -465,6 +465,9 @@ public class BukkitImplDeprecations {
     // Added 2025/04/27
     public static Warning playerChangesWorldSwitches = new FutureWarning("playerChangesWorldSwitches", "The 'from' and 'to' arguments in the 'player changes world' script event have been deprecated in favor of the 'from' and 'to' switches.");
 
+    // Added 2025/05/02
+    public static Warning timeSubTags = new FutureWarning("timeSubTags", pointlessSubtagPrefix + "'time.*' tags are now just 'time_*'.");
+
     // ==================== PAST deprecations of things that are already gone but still have a warning left behind ====================
 
     // Removed upstream 2023/10/29 without warning.
