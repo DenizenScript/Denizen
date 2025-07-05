77f3b7bcd04c3ccd2f13ac7f4d3df9d46c7a93ab
Aya <31237389+tal5@users.noreply.github.com>
2025-07-05 19:09:07 +0100

    Only refresh if not respawning

--- v1_21/src/main/java/com/denizenscript/denizen/nms/v1_21/impl/ProfileEditorImpl.java
+++ v1_21/src/main/java/com/denizenscript/denizen/nms/v1_21/impl/ProfileEditorImpl.java
@@ -53,7 +53,9 @@ public class ProfileEditorImpl extends ProfileEditor {
         if (isSkinChanging) {
             ((CraftServer) Bukkit.getServer()).getHandle().respawn(nmsPlayer, true, Entity.RemovalReason.CHANGED_DIMENSION, PlayerRespawnEvent.RespawnReason.PLUGIN);
         }
-        NMSHandler.playerHelper.refreshPlayer(player);
+        else {
+            NMSHandler.playerHelper.refreshPlayer(player);
+        }
         player.updateInventory();
     }
 
