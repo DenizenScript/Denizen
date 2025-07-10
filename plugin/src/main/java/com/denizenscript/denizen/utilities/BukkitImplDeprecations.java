5a22b7be02d0f33d842c130d883841694e82101f
david <89749147+davight@users.noreply.github.com>
2025-07-10 18:16:40 +0200

    add backsupport for entity knockback property

--- plugin/src/main/java/com/denizenscript/denizen/utilities/BukkitImplDeprecations.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/BukkitImplDeprecations.java
@@ -268,6 +268,9 @@ public class BukkitImplDeprecations {
     // Added 2025/03/14
     public static Warning settingBoatType = new Warning("settingBoatType", "As of MC 1.21, separate boat wood types are separate entity types, meaning the wood type of an existing boat entity cannot be changed without spawning a new one.");
 
+    // Added 2025/07/10
+    public static Warning entityKnockback = new Warning("entityKnockback", "The 'entity.knockback' property is deprecated. You should adjust knockback enchantment on the weapon itself.");
+
     // ==================== SLOW deprecations ====================
     // These aren't spammed, but will show up repeatedly until fixed. Server owners will probably notice them.
 
