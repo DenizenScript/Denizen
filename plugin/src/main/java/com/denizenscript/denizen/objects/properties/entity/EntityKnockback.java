50659233200cb9284c7c7a66b38b139d2603fd77
david <89749147+davight@users.noreply.github.com>
2025-08-24 05:00:36 +0200

    Deprecation of EntityTag.knockback (#2751)
    * add backsupport for entity knockback property
    
    * fix property string
    
    * more fixes
    
    * meta
    
    * Update meta
    
    Co-authored-by: Aya <31237389+tal5@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Aya <31237389+tal5@users.noreply.github.com>

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityKnockback.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityKnockback.java
@@ -1,12 +1,19 @@
 package com.denizenscript.denizen.objects.properties.entity;
 
+import com.denizenscript.denizen.nms.NMSHandler;
+import com.denizenscript.denizen.nms.NMSVersion;
 import com.denizenscript.denizen.objects.EntityTag;
+import com.denizenscript.denizen.utilities.BukkitImplDeprecations;
 import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.properties.Property;
 import com.denizenscript.denizencore.objects.properties.PropertyParser;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.AbstractArrow;
+import org.bukkit.inventory.ItemStack;
 
+@Deprecated
 public class EntityKnockback implements Property {
 
     public static boolean describes(ObjectTag object) {
@@ -30,7 +37,7 @@ public class EntityKnockback implements Property {
 
     @Override
     public String getPropertyString() {
-        return String.valueOf(getAbstractArrow().getKnockbackStrength());
+        return NMSHandler.getVersion().isAtLeast(NMSVersion.v1_21) ? null : String.valueOf(getAbstractArrow().getKnockbackStrength());
     }
 
     @Override
@@ -46,10 +53,18 @@ public class EntityKnockback implements Property {
         // @mechanism EntityTag.knockback
         // @group properties
         // @description
-        // Returns the knockback strength of an arrow or trident.
+        // Deprecated in favor of setting the knockback enchantment on the weapon itself on MC 1.21+.
+        // @deprecated use the knockback enchantment on the weapon itself on MC 1.21+.
         // -->
         PropertyParser.registerTag(EntityKnockback.class, ElementTag.class, "knockback", (attribute, object) -> {
-            return new ElementTag(object.getAbstractArrow().getKnockbackStrength());
+            BukkitImplDeprecations.entityKnockback.warn(attribute.context);
+            if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_21)) {
+                ItemStack is = object.getAbstractArrow().getWeapon();
+                return new ElementTag(is == null ? 0 : is.getEnchantmentLevel(Enchantment.KNOCKBACK)); // it is nullable even when it is marked as @NotNull, for example when you spawn an arrow
+            }
+            else {
+                return new ElementTag(object.getAbstractArrow().getKnockbackStrength());
+            }
         });
 
         // <--[mechanism]
@@ -57,13 +72,25 @@ public class EntityKnockback implements Property {
         // @name knockback
         // @input ElementTag(Number)
         // @description
-        // Sets the knockback strength of an arrow or trident.
+        // Deprecated in favor of setting the knockback enchantment on the weapon itself on MC 1.21+.
+        // @deprecated use the knockback enchantment on the weapon itself on MC 1.21+.
         // @tags
         // <EntityTag.knockback>
         // -->
         PropertyParser.registerMechanism(EntityKnockback.class, ElementTag.class, "knockback", (object, mechanism, input) -> {
+            BukkitImplDeprecations.entityKnockback.warn(mechanism.context);
             if (mechanism.requireInteger()) {
-                object.getAbstractArrow().setKnockbackStrength(input.asInt());
+                if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_21)) {
+                    ItemStack is = object.getAbstractArrow().getWeapon();
+                    if (is == null) { // it is nullable even when it is marked as @NotNull, for example when you spawn an arrow
+                        is = new ItemStack(Material.BOW);
+                    }
+                    is.addUnsafeEnchantment(Enchantment.KNOCKBACK, input.asInt());
+                    object.getAbstractArrow().setWeapon(is);
+                }
+                else {
+                    object.getAbstractArrow().setKnockbackStrength(input.asInt());
+                }
             }
         });
     }
