aff0f89f2624345c46ff873c3e39ec8f5a759ec7
Aya <31237389+tal5@users.noreply.github.com>
2025-08-24 04:07:40 +0100

    Update for format scripts moving to core (#2757)
    * Update for format scripts to core move
    
    * Minor cleanups

--- plugin/src/main/java/com/denizenscript/denizen/events/player/ChatScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/player/ChatScriptEvent.java
@@ -1,18 +1,21 @@
 package com.denizenscript.denizen.events.player;
 
-import com.denizenscript.denizen.objects.PlayerTag;
-import com.denizenscript.denizen.scripts.containers.core.FormatScriptContainer;
-import com.denizenscript.denizencore.utilities.CoreConfiguration;
-import com.denizenscript.denizencore.utilities.debugging.Debug;
-import com.denizenscript.denizen.utilities.implementation.BukkitScriptEntryData;
-import com.denizenscript.denizen.utilities.Settings;
 import com.denizenscript.denizen.events.BukkitScriptEvent;
+import com.denizenscript.denizen.objects.PlayerTag;
+import com.denizenscript.denizen.tags.BukkitTagContext;
+import com.denizenscript.denizen.utilities.Settings;
+import com.denizenscript.denizen.utilities.implementation.BukkitScriptEntryData;
+import com.denizenscript.denizencore.objects.ObjectTag;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.core.ListTag;
-import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.objects.core.ScriptTag;
 import com.denizenscript.denizencore.scripts.ScriptEntryData;
 import com.denizenscript.denizencore.scripts.ScriptRegistry;
+import com.denizenscript.denizencore.scripts.containers.core.FormatScriptContainer;
+import com.denizenscript.denizencore.tags.TagManager;
+import com.denizenscript.denizencore.utilities.CoreConfiguration;
 import com.denizenscript.denizencore.utilities.CoreUtilities;
+import com.denizenscript.denizencore.utilities.debugging.Debug;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -107,6 +110,14 @@ public class ChatScriptEvent extends BukkitScriptEvent implements Listener {
         return event;
     }
 
+    public static final String TEXT_CHAR = String.valueOf((char) 0x00), NAME_CHAR = String.valueOf((char) 0x04);
+
+    public String getFormatText(FormatScriptContainer formatScript) {
+        String text = formatScript.getRawFormat().replace("<[text]>", TEXT_CHAR).replace("<[name]>", NAME_CHAR);
+        return TagManager.tag(text, new BukkitTagContext(player, null, new ScriptTag(formatScript)))
+                .replace("%", "%%").replace(TEXT_CHAR, "%2$s").replace(NAME_CHAR, "%1$s");
+    }
+
     @Override
     public boolean applyDetermination(ScriptPath path, ObjectTag determinationObj) {
         if (determinationObj instanceof ElementTag) {
@@ -119,7 +130,7 @@ public class ChatScriptEvent extends BukkitScriptEvent implements Listener {
                     Debug.echoError("Could not find format script matching '" + name + '\'');
                 }
                 else {
-                    String formatstr = formatscr.getFormatText(null, player);
+                    String formatstr = getFormatText(formatscr);
                     if (CoreConfiguration.debugVerbose) {
                         Debug.log("Setting format to " + formatstr);
                     }
