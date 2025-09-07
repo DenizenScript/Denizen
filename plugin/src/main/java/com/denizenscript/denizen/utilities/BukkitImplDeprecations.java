5703b8ebe0168f1649fad494697de79f013c29a1
MCSamuel <morrissamuelw@gmail.com>
2025-09-07 13:18:49 -0700

    deprecation date updated

--- plugin/src/main/java/com/denizenscript/denizen/utilities/BukkitImplDeprecations.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/BukkitImplDeprecations.java
@@ -479,10 +479,10 @@ public class BukkitImplDeprecations {
     // Added 2025/06/29
     public static Warning blockDispensesItemDetermination = new FutureWarning("blockDispensesItemDetermination", "The determination to control the item in the 'block dispenses' script event has been changed into the 'ITEM:<ItemTag>' format.");
 
-    // Added 2025/08/23
+    // Added 2025/09/07
     public static Warning advancementBackgroundFormat = new FutureWarning("advancementBackgroundFormat", "The 'background:' input in the advancement command no longer uses the 'textures/' path or '.png' suffix, so for example 'minecraft:textures/gui/advancements/backgrounds/stone.png' would be 'minecraft:gui/advancements/backgrounds/stone'.");
 
-    // Added 2025/08/25
+    // Added 2025/09/07
     public static Warning brewingStandConsumeDetermination = new FutureWarning("brewingStandConsumeDetermination", "The 'consuming' and 'not_consuming' determinations in the 'brewing stand fueled' event have been deprecated in favor of 'CONSUME:<ElementTag(Boolean)>'.");
 
     // ==================== PAST deprecations of things that are already gone but still have a warning left behind ====================
