7e6ccaa92b0a6d25c514894e802f79199626604f
MCSamuel <morrissamuelw@gmail.com>
2025-11-01 11:53:29 -0700

    EntityDropsItemScriptEvent, EntityEntersPortalScriptEvent, EntityEntersVehicleScriptEvent, and EntityExitsPortalScriptEvent

--- plugin/src/main/java/com/denizenscript/denizen/events/entity/EntityEntersPortalScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/entity/EntityEntersPortalScriptEvent.java
@@ -59,13 +59,11 @@ public class EntityEntersPortalScriptEvent extends BukkitScriptEvent implements 
 
     @Override
     public ObjectTag getContext(String name) {
-        if (name.equals("entity")) {
-            return entity;
-        }
-        else if (name.equals("location")) {
-            return location;
-        }
-        return super.getContext(name);
+        return switch (name) {
+            case "entity" -> entity.getDenizenObject();
+            case "location" -> location;
+            default -> super.getContext(name);
+        };
     }
 
     @EventHandler
