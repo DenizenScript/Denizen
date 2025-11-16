a607a64f53baf4f83c9f812e72d09ab7e2ede86d
MCSamuel <morrissamuelw@gmail.com>
2025-11-16 09:27:30 -0800

    minor changes

--- plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityProfession.java
+++ plugin/src/main/java/com/denizenscript/denizen/objects/properties/entity/EntityProfession.java
@@ -1,12 +1,9 @@
 package com.denizenscript.denizen.objects.properties.entity;
 
-import com.denizenscript.denizen.nms.NMSHandler;
-import com.denizenscript.denizen.nms.NMSVersion;
 import com.denizenscript.denizen.objects.EntityTag;
 import com.denizenscript.denizen.utilities.Utilities;
 import com.denizenscript.denizencore.objects.core.ElementTag;
 import com.denizenscript.denizencore.objects.Mechanism;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Villager;
 import org.bukkit.entity.ZombieVillager;
 
@@ -26,25 +23,15 @@ public class EntityProfession extends EntityProperty<ElementTag> {
                 || entity.getBukkitEntity() instanceof ZombieVillager;
     }
 
-    public EntityProfession(EntityTag entity) {
-        professional = entity;
-    }
-
-    EntityTag professional;
-
-    public Villager.Profession getProfession() {
-        if (professional.getBukkitEntityType() == EntityType.ZOMBIE_VILLAGER) {
-            return ((ZombieVillager) professional.getBukkitEntity()).getVillagerProfession();
-        }
-        return ((Villager) professional.getBukkitEntity()).getProfession();
-    }
-
     @Override
     public ElementTag getPropertyValue() {
-        if (NMSHandler.getVersion().isAtMost(NMSVersion.v1_20)) {
-            return new ElementTag(String.valueOf(getProfession()), true);
+        if (getEntity() instanceof Villager villager) {
+            return Utilities.enumlikeToElement(villager.getProfession());
         }
-        return new ElementTag(Utilities.namespacedKeyToString(getProfession().getKey()).toUpperCase(), true);
+        else if (getEntity() instanceof ZombieVillager zvillager) {
+            return Utilities.enumlikeToElement(zvillager.getVillagerProfession());
+        }
+        return null;
     }
 
     @Override
