8fb7c808b3f0bf1ea8b6d0cd03aa2728fcf13e2b
Remasuri3 <benicek.seb@gmail.com>
2026-01-13 00:22:21 +0100

    Added a space at if-statement for formatting purposes

--- plugin/src/main/java/com/denizenscript/denizen/events/player/PlayerPreparesEnchantScriptEvent.java
+++ plugin/src/main/java/com/denizenscript/denizen/events/player/PlayerPreparesEnchantScriptEvent.java
@@ -77,7 +77,7 @@ public class PlayerPreparesEnchantScriptEvent extends BukkitScriptEvent implemen
                 }
                 for (int i = 0; i < offers.size(); i++) {
                     MapTag map = MapTag.getMapFor(offers.getObject(i), getTagContext(path));
-                    if (map.isEmpty()){
+                    if (map.isEmpty()) {
                         event.getOffers()[i] = null;
                         continue;
                     }
