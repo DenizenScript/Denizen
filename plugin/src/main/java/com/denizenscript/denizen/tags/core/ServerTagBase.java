19c7dfe3dbe266b1ff33413c78c8df6bfde55240
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2026-02-11 20:27:08 -0800

    new tag: server.bukkit_name

--- plugin/src/main/java/com/denizenscript/denizen/tags/core/ServerTagBase.java
+++ plugin/src/main/java/com/denizenscript/denizen/tags/core/ServerTagBase.java
@@ -1121,6 +1121,16 @@ public class ServerTagBase extends PseudoObjectTagBase<ServerTagBase> {
         });
 
         // <--[tag]
+        // @attribute <server.bukkit_name>
+        // @returns ElementTag
+        // @description
+        // Returns the name of the Bukkit platform, such as "Paper".
+        // -->
+        tagProcessor.registerStaticTag(ElementTag.class, "bukkit_name", (attribute, object) -> {
+            return new ElementTag(Bukkit.getName());
+        });
+
+        // <--[tag]
         // @attribute <server.bukkit_version>
         // @returns ElementTag
         // @description
