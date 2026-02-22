4323e7d16f088ff8bcb5ab98a9f914e6912e0fc6
hyper <honkhonk4456@gmail.com>
2026-02-22 12:02:49 -0500

    New item component registry system

---
+++ paper/src/main/java/com/denizenscript/denizen/paper/datacomponents/ItemModelAdapter.java
@@ -0,0 +1,35 @@
+package com.denizenscript.denizen.paper.datacomponents;
+
+import com.denizenscript.denizen.utilities.Utilities;
+import com.denizenscript.denizencore.objects.Mechanism;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import net.kyori.adventure.key.Key;
+
+public class ItemModelAdapter extends DataComponentAdapter.Valued<ElementTag, Key> {
+
+    // <--[property]
+    // @object ItemTag
+    // @name item_model
+    // @input ElementTag
+    // @description
+    // Controls an item's model <@link language Item Components> in namespaced key format.
+    // The default namespace is "minecraft", so for example an input of "stone" becomes "minecraft:stone", and will set the item model to a stone block.
+    // @mechanism
+    // Provide no input to reset the item to its default value.
+    // -->
+
+    public ItemModelAdapter() {
+        super(ElementTag.class, DataComponentTypes.ITEM_MODEL, "item_model");
+    }
+
+    @Override
+    public ElementTag toDenizen(Key value) {
+        return new ElementTag(value.toString());
+    }
+
+    @Override
+    public Key fromDenizen(ElementTag value, Mechanism mechanism) {
+        return Utilities.parseNamespacedKey(value.asString());
+    }
+}
