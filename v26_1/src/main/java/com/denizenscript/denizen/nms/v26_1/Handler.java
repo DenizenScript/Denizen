3b4eb3438e40a63d7c1095aa3bab982209f04038
Aya <31237389+tal5@users.noreply.github.com>
2026-04-03 14:40:34 +0100

    26.1.1 bump

--- v26_1/src/main/java/com/denizenscript/denizen/nms/v26_1/Handler.java
+++ v26_1/src/main/java/com/denizenscript/denizen/nms/v26_1/Handler.java
@@ -150,7 +150,7 @@ public class Handler extends NMSHandler {
 
     @Override
     public boolean isExactServerVersionMatch() {
-        return Denizen.supportsPaper ? SharedConstants.getCurrentVersion().id().equals("26.1") : CraftMagicNumbers.INSTANCE.getMappingsVersion().equals("e8ece90188c951d866bd2fffc52c803e");
+        return Denizen.supportsPaper ? SharedConstants.getCurrentVersion().id().equals("26.1.1") : CraftMagicNumbers.INSTANCE.getMappingsVersion().equals("e8ece90188c951d866bd2fffc52c803e");
     }
 
     @Override
