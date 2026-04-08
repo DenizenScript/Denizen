7f6475f516e3b0ad6d9593a336a48dc790607844
hyper <39167664+heypr@users.noreply.github.com>
2026-04-08 17:50:35 -0400

    `ItemTag.item_model/max_durability/max_stack_size/rarity` (#2734)
    * Components :o
    
    - Added support for the `item_model`, `max_durability`, `food`, `max_stack_size`, and `rarity` item components.
    
    - Additionally made sure that the paper `ItemTag.rarity` didn't conflict with the new >1.20 rarity component mech.
    
    * Updated name + corrected registration
    
    * Addressed comments
    
    * Addressed comments.
    
    * Max durability tag for <=1.19
    
    * Update ItemDurability.java
    
    * Reverted changes.
    
    * Forgot to revert item extensions file.
    
    * New item component registry system
    
    * toString --> asMinimalString
    
    * Addressed comments
    
    * tern
    
    * Update PaperModule.java
    
    * moved version check into paperitemextension file
    
    * Removed legacy tag meta

--- paper/src/main/java/com/denizenscript/denizen/paper/datacomponents/ComponentAdaptersRegistry.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/datacomponents/ComponentAdaptersRegistry.java
@@ -5,5 +5,9 @@ public class ComponentAdaptersRegistry {
     public static void register() {
         DataComponentAdapter.register(new FoodAdapter());
         DataComponentAdapter.register(new GliderAdapter());
+        DataComponentAdapter.register(new ItemModelAdapter());
+        DataComponentAdapter.register(new MaxDurabilityAdapter());
+        DataComponentAdapter.register(new MaxStackSizeAdapter());
+        DataComponentAdapter.register(new RarityAdapter());
     }
 }
