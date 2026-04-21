a5f202f60c031cad7a19594d59df9c24f3db5387
MCSamuel <morrissamuelw@gmail.com>
2026-04-21 07:47:17 -0700

    remove todo

--- plugin/src/main/java/com/denizenscript/denizen/utilities/MultiVersionHelper1_19.java
+++ plugin/src/main/java/com/denizenscript/denizen/utilities/MultiVersionHelper1_19.java
@@ -20,7 +20,6 @@ public class MultiVersionHelper1_19 {
     // TODO Frog variants technically have registries on all supported versions
     public static String getColor(Entity entity, boolean includeDeprecated) {
         if (entity instanceof Frog frog) {
-            // TODO once 1.21 is the minimum supported version, replace with direct registry-based handling
             if (NMSHandler.getVersion().isAtLeast(NMSVersion.v1_21)) {
                 return Utilities.namespacedKeyToString(frog.getVariant().getKey());
             }
