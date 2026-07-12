0ed6f7adfc4d2886c1d32d0920262403a35f858e
MCSamuel <187220916+MC-Samuel@users.noreply.github.com>
2026-07-12 13:44:08 -0700

    moved to a separate determination

--- paper/src/main/java/com/denizenscript/denizen/paper/events/ServerListPingScriptEventPaperImpl.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/events/ServerListPingScriptEventPaperImpl.java
@@ -15,7 +15,6 @@ import com.destroystokyo.paper.event.server.PaperServerListPingEvent;
 import com.destroystokyo.paper.profile.PlayerProfile;
 import com.destroystokyo.paper.profile.ProfileProperty;
 import net.md_5.bungee.api.ChatColor;
-import org.apache.commons.lang3.mutable.MutableInt;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.profile.PlayerTextures;
@@ -71,6 +70,17 @@ public class ServerListPingScriptEventPaperImpl extends ListPingScriptEvent {
             ListedPlayersEditor.setListedPlayerInfo(evt.getEvent(), text);
             return true;
         });
+        this.<ServerListPingScriptEventPaperImpl, ElementTag>registerOptionalDetermination("player_count", ElementTag.class, (evt, context, input) -> {
+            if (!CoreConfiguration.allowRestrictedActions) {
+                Debug.echoError("Cannot use 'player_count' in list ping event: 'Allow restricted actions' is disabled in Denizen config.yml.");
+                return false;
+            }
+            if (input.isInt()) {
+                evt.getEvent().setNumPlayers(input.asInt());
+                return true;
+            }
+            return false;
+        });
     }
 
     public PaperServerListPingEvent getEvent() {
@@ -87,15 +97,7 @@ public class ServerListPingScriptEventPaperImpl extends ListPingScriptEvent {
         }
 
         public static void excludeListedPlayers(PaperServerListPingEvent event, Set<UUID> exclude) {
-            MutableInt counter = new MutableInt(0);
-            event.getListedPlayers().removeIf(listedPlayerInfo -> {
-                if (exclude.contains(listedPlayerInfo.id())) {
-                    counter.increment();
-                    return true;
-                }
-                return false;
-            });
-            event.setNumPlayers(event.getNumPlayers() - counter.intValue());
+            event.getListedPlayers().removeIf(listedPlayerInfo -> exclude.contains(listedPlayerInfo.id()));
         }
     }
 
