893f96cec1038cb68a54315765c99353cfd88a64
Inquisitor <62276696+creaturesofhabit@users.noreply.github.com>
2026-07-24 11:52:48 -0700

    item_name meta
    pending change to itemtag.display meta

--- paper/src/main/java/com/denizenscript/denizen/paper/datacomponents/ItemNameAdapter.java
+++ paper/src/main/java/com/denizenscript/denizen/paper/datacomponents/ItemNameAdapter.java
@@ -14,7 +14,7 @@ public class ItemNameAdapter extends DataComponentAdapter.Valued<ElementTag, Com
     // @name item_name
     // @input ElementTag
     // @description
-    // Controls the item's item name component. This exists under the item's <@link mechanism ItemTag.display> and persists if it is renamed in an anvil.
+    // Controls the item's item_name component. In effect, this changes its display name. Unlike <@link mechanism ItemTag.display>, this name cannot be changed by an anvil, but will be hidden when the display is set, such as with an anvil. That is, it will remain under the display name and be shown again if the display is removed.
     // See also <@link language Item Components>.
     // @mechanism
     // Provide no input to reset the item to its default value.
